b344965f82c8c36ebccb8b46b0558d1ccc05b2cd
Kevin MacWhinnie <km@sayhello.com>
2014-10-07 16:59:53 -0700

    use the same scale factor as iOS for timeline items

--- app/src/main/java/is/hello/sense/ui/adapter/TimelineSegmentAdapter.java
+++ app/src/main/java/is/hello/sense/ui/adapter/TimelineSegmentAdapter.java
@@ -3,8 +3,13 @@ package is.hello.sense.ui.adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
 
 import java.util.List;
@@ -14,8 +19,19 @@ import is.hello.sense.api.model.TimelineSegment;
 import is.hello.sense.ui.widget.TimelineSegmentView;
 
 public class TimelineSegmentAdapter extends ArrayAdapter<TimelineSegment> {
+    private static final int NUMBER_HOURS_ON_SCREEN = 4;
+
+    private final int itemHeight;
+
     public TimelineSegmentAdapter(@NonNull Context context) {
         super(context, android.R.layout.simple_list_item_1);
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+
+        this.itemHeight = metrics.heightPixels / NUMBER_HOURS_ON_SCREEN;
 
         SenseApplication.getInstance().inject(this);
     }
@@ -32,6 +48,10 @@ public class TimelineSegmentAdapter extends ArrayAdapter<TimelineSegment> {
         clear();
     }
 
+    private int calculateHeight(@NonNull TimelineSegment segment) {
+        return (int) (segment.getDuration() / 3600) * itemHeight;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         TimelineSegmentView view = (TimelineSegmentView) convertView;
@@ -41,6 +61,7 @@ public class TimelineSegmentAdapter extends ArrayAdapter<TimelineSegment> {
 
         TimelineSegment segment = getItem(position);
         view.displaySegment(segment);
+        view.setLayoutParams(new AbsListView.LayoutParams(AbsListView.LayoutParams.MATCH_PARENT, calculateHeight(segment)));
 
         return view;
     }
