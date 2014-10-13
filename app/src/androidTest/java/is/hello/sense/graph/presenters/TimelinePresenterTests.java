1080151ea2d2b4436dbf23f92909b88aadcc1d42
Kevin MacWhinnie <km@sayhello.com>
2014-10-13 12:03:14 -0700

    adapter for async-to-sync testing

--- app/src/androidTest/java/is/hello/sense/graph/presenters/TimelinePresenterTests.java
+++ app/src/androidTest/java/is/hello/sense/graph/presenters/TimelinePresenterTests.java
@@ -1,13 +1,27 @@
 package is.hello.sense.graph.presenters;
 
+import org.joda.time.DateTime;
+
+import java.util.List;
+
 import javax.inject.Inject;
 
+import is.hello.sense.api.model.Timeline;
 import is.hello.sense.graph.InjectionTestCase;
+import is.hello.sense.util.SyncObserver;
+import rx.Observable;
 
 public class TimelinePresenterTests extends InjectionTestCase {
     @Inject TimelinePresenter timelinePresenter;
 
-    public void testUpdate() {
-        fail();
+    public void testUpdate() throws Exception {
+        Observable<List<Timeline>> fullTimeline = timelinePresenter.timeline;
+        timelinePresenter.setDate(DateTime.now());
+
+        SyncObserver<List<Timeline>> observer = SyncObserver.waitOn(SyncObserver.WaitingFor.NEXT, fullTimeline);
+        observer.await();
+
+        assertNull(observer.getError());
+        assertFalse(observer.getResults().isEmpty());
     }
 }
---
+++ app/src/androidTest/java/is/hello/sense/util/SyncObserver.java
@@ -0,0 +1,78 @@
+package is.hello.sense.util;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.Observer;
+import rx.schedulers.Schedulers;
+
+public final class SyncObserver<T> implements Observer<T> {
+    public static final long STANDARD_TIMEOUT = 500;
+
+    private final CountDownLatch latch = new CountDownLatch(1);
+    private final WaitingFor waitingFor;
+
+    private Throwable error;
+    private ArrayList<T> results = new ArrayList<>();
+
+    public SyncObserver(@NonNull WaitingFor waitingFor) {
+        this.waitingFor = waitingFor;
+    }
+
+    public static <T> SyncObserver<T> waitOn(@NonNull WaitingFor waitingFor, @NonNull Observable<T> observable) {
+        SyncObserver<T> observer = new SyncObserver<>(waitingFor);
+        observable.subscribeOn(Schedulers.io()).subscribe(observer);
+        return observer;
+    }
+
+
+    @Override
+    public void onCompleted() {
+        if (waitingFor == WaitingFor.COMPLETED)
+            latch.countDown();
+    }
+
+    @Override
+    public void onError(Throwable e) {
+        this.error = e;
+        latch.countDown();
+    }
+
+    @Override
+    public void onNext(T t) {
+        results.add(t);
+        if (waitingFor == WaitingFor.NEXT)
+            latch.countDown();
+    }
+
+
+    public boolean await() throws InterruptedException {
+        latch.await();
+        return true;
+        //return await(STANDARD_TIMEOUT, TimeUnit.MILLISECONDS);
+    }
+
+    public boolean await(long timeout, TimeUnit unit) throws InterruptedException {
+        return latch.await(timeout, unit);
+    }
+
+
+    public @Nullable Throwable getError() {
+        return error;
+    }
+
+    public @NonNull ArrayList<T> getResults() {
+        return results;
+    }
+
+
+    public static enum WaitingFor {
+        NEXT,
+        COMPLETED,
+    }
+}
