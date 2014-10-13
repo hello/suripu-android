028b2d77cacf24fbffc7a6eae2caeff825701e6c
Kevin MacWhinnie <km@sayhello.com>
2014-10-13 11:20:42 -0700

    tests compile again

--- app/src/androidTest/java/is/hello/sense/util/DateFormatterTests.java
+++ app/src/androidTest/java/is/hello/sense/util/DateFormatterTests.java
@@ -3,8 +3,12 @@ package is.hello.sense.util;
 import android.test.InstrumentationTestCase;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDateTime;
+import org.joda.time.ReadableInstant;
+import org.joda.time.ReadablePartial;
 
 public class DateFormatterTests extends InstrumentationTestCase {
+    private static final LocalDateTime LOCAL_TEST_TIME = new LocalDateTime(2014, 10, 1, 10, 30, 0);
     private static final DateTime TEST_TIME = new DateTime(2014, 10, 1, 10, 30, 0);
     private DateFormatter formatter;
 
@@ -20,12 +24,15 @@ public class DateFormatterTests extends InstrumentationTestCase {
     }
 
     public void testFormatAsDate() {
-        assertNotNull(formatter.formatAsDate(null));
+        assertNotNull(formatter.formatAsDate((ReadableInstant) null));
+        assertNotNull(formatter.formatAsDate((ReadablePartial) null));
         assertEquals("October 1", formatter.formatAsDate(TEST_TIME));
+        assertEquals("October 1", formatter.formatAsDate(LOCAL_TEST_TIME));
     }
 
     public void testFormatAsTime() {
-        assertNotNull(formatter.formatAsTime(null));
+        assertNotNull(formatter.formatAsTime((ReadableInstant) null));
+        assertNotNull(formatter.formatAsTime((ReadablePartial) null));
         assertEquals("10:30 AM", formatter.formatAsTime(TEST_TIME));
     }
 }
