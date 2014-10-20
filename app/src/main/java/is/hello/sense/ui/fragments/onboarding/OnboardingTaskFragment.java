0cc2ce1926fc92418cc7e129ae3073115d7ee15d
Kevin MacWhinnie <km@sayhello.com>
2014-10-20 15:18:07 -0700

    generic onboarding task fragmnet [ref #81058600]

---
+++ app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingTaskFragment.java
@@ -0,0 +1,48 @@
+package is.hello.sense.ui.fragments.onboarding;
+
+import android.app.Fragment;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import is.hello.sense.R;
+
+public class OnboardingTaskFragment extends Fragment {
+    private static final String ARG_TITLE_RES_ID = OnboardingTaskFragment.class.getName() + ".ARG_TITLE_RES_ID";
+
+    public static OnboardingTaskFragment newInstance(@StringRes int titleResId) {
+        OnboardingTaskFragment fragment = new OnboardingTaskFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_TITLE_RES_ID, titleResId);
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setRetainInstance(true);
+    }
+
+    @Override
+    public @Nullable View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_onboarding_task, container, false);
+
+        ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.fragment_onboarding_task_progress_bar);
+        progressBar.getIndeterminateDrawable().setColorFilter(getResources().getColor(R.color.light_accent), PorterDuff.Mode.SRC_IN);
+
+        TextView title = (TextView) view.findViewById(R.id.fragment_onboarding_task_title);
+        title.setText(getArguments().getInt(ARG_TITLE_RES_ID));
+
+        return view;
+    }
+}
