d5298cd99c22e5890a55ca43aeb8ddec447739fb
Kevin MacWhinnie <km@sayhello.com>
2014-11-04 10:53:44 -0800

    fix smart alarm save crash [ref #82055332]

--- app/src/main/java/is/hello/sense/ui/common/InjectionFragment.java
+++ app/src/main/java/is/hello/sense/ui/common/InjectionFragment.java
@@ -1,7 +1,11 @@
 package is.hello.sense.ui.common;
 
+import android.app.Activity;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Parcelable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -177,4 +181,22 @@ public class InjectionFragment extends Fragment implements ObservableContainer, 
     }
 
 
+    /**
+     * Safely pops the fragment from the back stack, propagating a result value
+     * and response Intent to the receiver's target fragment.
+     * <p/>
+     * This method requires a target fragment and the receiver be attached to an activity.
+     * @param resultCode    The result code to propagate back.
+     * @param response      The result of the fragment.
+     * @return  true if the fragment was popped and the target fragment informed; false otherwise.
+     */
+    protected boolean popFromBackStack(int resultCode, @Nullable Intent response) {
+        if (getTargetFragment() != null && getFragmentManager() != null) {
+            new Handler().post(() -> getTargetFragment().onActivityResult(getTargetRequestCode(), resultCode, response));
+            getFragmentManager().popBackStackImmediate();
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
