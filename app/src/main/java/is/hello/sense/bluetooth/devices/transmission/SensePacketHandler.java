99efe740faf825c311e6bf922e9ca2d20e804a81
Kevin MacWhinnie <km@sayhello.com>
2014-11-11 15:44:13 -0800

    add comments to clarify arraycopy usage

--- app/src/main/java/is/hello/sense/bluetooth/devices/transmission/SensePacketHandler.java
+++ app/src/main/java/is/hello/sense/bluetooth/devices/transmission/SensePacketHandler.java
@@ -36,7 +36,13 @@ public class SensePacketHandler extends PacketHandler {
         if (totalPacketCount == 1) {
             final byte[] headPacket = new byte[2 + applicationData.length];
             headPacket[1] = 1;
-            System.arraycopy(applicationData, 0, headPacket, 2, applicationData.length);
+            System.arraycopy(
+                    /* src */ applicationData,
+                    /* srcStart */ 0,
+                    /* dest */ headPacket,
+                    /* destStart */ 2,
+                    /* length */ applicationData.length
+            );
             packets.add(headPacket);
             return packets;
         }
@@ -45,13 +51,20 @@ public class SensePacketHandler extends PacketHandler {
         int bytesRemaining = applicationData.length;
         for (int packetIndex = 0; packetIndex < totalPacketCount; packetIndex++) {
             if (packetIndex == 0) {
-                final byte[] headPacket = new byte[PacketHandler.BLE_PACKET_LEN];
-                headPacket[1] = (byte) totalPacketCount;
+                final byte[] headerPacket = new byte[PacketHandler.BLE_PACKET_LEN];
+                headerPacket[0] = (byte) packetIndex;
+                headerPacket[1] = (byte) totalPacketCount;
 
-                System.arraycopy(applicationData, 0, headPacket, 2, PacketHandler.HEADER_PACKET_PAYLOAD_LEN);
+                System.arraycopy(
+                        /* src */ applicationData,
+                        /* srcStart */ 0,
+                        /* dest */ headerPacket,
+                        /* destStart */ 2,
+                        /* length */ PacketHandler.HEADER_PACKET_PAYLOAD_LEN
+                );
                 bytesRemaining -= PacketHandler.HEADER_PACKET_PAYLOAD_LEN;
 
-                packets.add(headPacket);
+                packets.add(headerPacket);
             } else {
                 final int packetLength = (packetIndex == totalPacketCount - 1) ? (bytesRemaining + 1) : PacketHandler.BLE_PACKET_LEN;
                 final byte[] packet = new byte[packetLength];
@@ -59,7 +72,13 @@ public class SensePacketHandler extends PacketHandler {
 
                 int dataStart = PacketHandler.HEADER_PACKET_PAYLOAD_LEN + (packetIndex - 1) * PacketHandler.PACKET_PAYLOAD_LEN;
                 int dataAmount = packetLength - 1;
-                System.arraycopy(applicationData, dataStart, packet, 1, dataAmount);
+                System.arraycopy(
+                        /* src */ applicationData,
+                        /* srcStart */ dataStart,
+                        /* dest */ packet,
+                        /* destStart */ 1,
+                        /* length */ dataAmount
+                );
                 bytesRemaining -= dataAmount;
 
                 packets.add(packet);
