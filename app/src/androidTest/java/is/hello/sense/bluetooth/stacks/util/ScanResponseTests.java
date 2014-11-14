450c07ffb99a858d5f3b35bb817ff3ef89f1b904
Kevin MacWhinnie <km@sayhello.com>
2014-11-13 16:44:01 -0800

    ScanResponse tests

--- app/src/androidTest/java/is/hello/sense/bluetooth/stacks/util/ScanResponseTests.java
+++ app/src/androidTest/java/is/hello/sense/bluetooth/stacks/util/ScanResponseTests.java
@@ -2,8 +2,27 @@ package is.hello.sense.bluetooth.stacks.util;
 
 import junit.framework.TestCase;
 
+import java.util.Set;
+
 public class ScanResponseTests extends TestCase {
+    private static final byte[] TEST_PAYLOAD = {
+            // Advertisement payload size
+            (byte) 0x02,
+
+            // Advertisement data type
+            (byte) ScanResponse.TYPE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS,
+
+            // Begin data
+            (byte) 0xE1,
+            (byte) 0xFE,
+    };
+
     public void testParse() throws Exception {
-        fail();
+        Set<ScanResponse> response = ScanResponse.parse(TEST_PAYLOAD);
+        assertEquals(1, response.size());
+
+        ScanResponse first = response.iterator().next();
+        assertEquals(first.type, ScanResponse.TYPE_LIST_OF_16_BIT_SERVICE_CLASS_UUIDS);
+        assertEquals(BluetoothUtils.bytesToString(first.payload), "E1FE");
     }
 }
