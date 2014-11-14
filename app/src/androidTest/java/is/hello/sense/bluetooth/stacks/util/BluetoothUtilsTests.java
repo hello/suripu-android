a53c8d0d50404b25ccfbc727da7c6ffcff810344
Kevin MacWhinnie <km@sayhello.com>
2014-11-13 16:27:01 -0800

    tests for BluetoothUtils

--- app/src/androidTest/java/is/hello/sense/bluetooth/stacks/util/BluetoothUtilsTests.java
+++ app/src/androidTest/java/is/hello/sense/bluetooth/stacks/util/BluetoothUtilsTests.java
@@ -2,12 +2,29 @@ package is.hello.sense.bluetooth.stacks.util;
 
 import junit.framework.TestCase;
 
+import java.util.Arrays;
+
+import static is.hello.sense.AssertExtensions.assertThrows;
+
 public class BluetoothUtilsTests extends TestCase {
     public void testBytesToString() throws Exception {
-        fail();
+        byte[] emptyBytes = {};
+        String emptyString = BluetoothUtils.bytesToString(emptyBytes);
+        assertEquals("", emptyString);
+
+
+        byte[] testBytes = { 0x12, 0x14, 0x0f, 0x12 };
+        String testString = BluetoothUtils.bytesToString(testBytes);
+        assertEquals("12140F12", testString);
     }
 
     public void testStringToBytes() throws Exception {
-        fail();
+        byte[] emptyBytes = {};
+        assertTrue(Arrays.equals(emptyBytes, BluetoothUtils.stringToBytes("")));
+
+        byte[] testBytes = { 0x12, 0x14, 0x0f, 0x12 };
+        assertTrue(Arrays.equals(testBytes, BluetoothUtils.stringToBytes("12140F12")));
+
+        assertThrows(() -> BluetoothUtils.stringToBytes("121"));
     }
 }
