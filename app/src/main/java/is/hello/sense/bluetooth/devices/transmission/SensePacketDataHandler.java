5e373d9fc29187dc40e1943ea655aeeb1140e5d2
Kevin MacWhinnie <km@sayhello.com>
2014-11-17 13:48:45 -0800

    remove unneeded variable to help with memory pressure

--- app/src/main/java/is/hello/sense/bluetooth/devices/transmission/SensePacketDataHandler.java
+++ app/src/main/java/is/hello/sense/bluetooth/devices/transmission/SensePacketDataHandler.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import com.google.protobuf.InvalidProtocolBufferException;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.UUID;
 
 import is.hello.sense.bluetooth.devices.SenseIdentifiers;
@@ -18,12 +17,12 @@ import is.hello.sense.util.Logger;
 
 public class SensePacketDataHandler extends PacketDataHandler<MorpheusBle.MorpheusCommand> {
     private int totalPackets = 0;
-    private byte[] buffer;
+    private int packetsProcessed = 0;
     private int expectedIndex = 0;
-    private int actualDataLength = 0;
-    private int bufferOffsetIndex = 0;
+    private byte[] buffer;
+    private int bufferOffset = 0;
+    private int bufferDataLength = 0;
 
-    private LinkedList<SequencedPacket> packets = new LinkedList<>();
 
 
     @Override
@@ -34,7 +33,7 @@ public class SensePacketDataHandler extends PacketDataHandler<MorpheusBle.Morphe
     @Override
     public void processPacket(final @NonNull SequencedPacket blePacket) {
         if (this.expectedIndex != blePacket.sequenceNumber) {
-            this.packets.clear();
+            this.packetsProcessed = 0;
             this.expectedIndex = 0;
             this.buffer = null;
             onError(new ProtobufProcessingError(ProtobufProcessingError.Reason.DATA_LOST_OR_OUT_OF_ORDER));
@@ -44,49 +43,50 @@ public class SensePacketDataHandler extends PacketDataHandler<MorpheusBle.Morphe
         }
 
 
+        SequencedPacket lastPacket;
         if (blePacket.sequenceNumber == 0) {
             // Assume the packets arrive in order.
-            this.packets.clear();
+            this.packetsProcessed = 0;
             this.totalPackets = blePacket.payload[0];
-            this.bufferOffsetIndex = 0;
+            this.bufferOffset = 0;
 
             final SequencedPacket headPacket = new SequencedPacket(0, Arrays.copyOfRange(blePacket.payload, 1, blePacket.payload.length));
-            this.packets.add(headPacket);
-            this.actualDataLength = headPacket.payload.length;
+            this.bufferDataLength = headPacket.payload.length;
             this.buffer = new byte[PacketHandler.HEADER_PACKET_PAYLOAD_LEN + PacketHandler.PACKET_PAYLOAD_LEN * (this.totalPackets - 1)];
-        } else {
-            this.packets.add(blePacket);
 
-            this.actualDataLength += blePacket.payload.length;
+            lastPacket = headPacket;
+        } else {
+            lastPacket = blePacket;
+            this.bufferDataLength += blePacket.payload.length;
         }
 
         // copy data in packets to a continues payload buffer.
-        if (this.bufferOffsetIndex < this.buffer.length) {
-            final SequencedPacket lastPacket = this.packets.getLast();
+        if (this.bufferOffset < this.buffer.length) {
             System.arraycopy(
                 /* src */ lastPacket.payload,
                 /* srcStart */ 0,
                 /* dest */ buffer,
-                /* destStart */ bufferOffsetIndex,
+                /* destStart */ bufferOffset,
                 /* length */ lastPacket.payload.length
             );
 
-            bufferOffsetIndex += lastPacket.payload.length;
+            bufferOffset += lastPacket.payload.length;
         }
+        packetsProcessed++;
 
-        if (this.packets.size() == this.totalPackets) {
+        if (this.packetsProcessed == this.totalPackets) {
             final MorpheusBle.MorpheusCommand data;
             try {
-                data = MorpheusBle.MorpheusCommand.parseFrom(Arrays.copyOfRange(this.buffer, 0, actualDataLength));
+                data = MorpheusBle.MorpheusCommand.parseFrom(Arrays.copyOfRange(this.buffer, 0, bufferDataLength));
                 this.onResponse(data);
             } catch (InvalidProtocolBufferException e) {
                 Logger.error(SensePacketDataHandler.class.getSimpleName(), "Could not parse command.", e);
                 onError(new ProtobufProcessingError(ProtobufProcessingError.Reason.INVALID_PROTOBUF));
             }
 
-            this.packets.clear();
             this.expectedIndex = 0;
             this.buffer = null;
+            this.packetsProcessed = 0;
         }
     }
 }
