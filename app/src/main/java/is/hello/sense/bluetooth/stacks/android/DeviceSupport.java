703b1047646bf4cd004ba7cdc79083b595a37d9f
Kevin MacWhinnie <km@sayhello.com>
2014-12-09 17:30:39 -0800

    update supported device list

--- app/src/main/java/is/hello/sense/bluetooth/stacks/android/DeviceSupport.java
+++ app/src/main/java/is/hello/sense/bluetooth/stacks/android/DeviceSupport.java
@@ -9,6 +9,9 @@ final class DeviceSupport {
     static @NonNull BluetoothStack.SupportLevel getSupportLevelForModel(@NonNull String model) {
         switch (model) {
             case "XT1032" /* Moto G first generation */:
+            case "GT-I9500" /* Samsung Galaxy S4 (Intl) */:
+            case "HTC One_M8" /* HTC One M8 */:
+            case "Nexus 4" /* Nexus 4 */:
                 return BluetoothStack.SupportLevel.SUPPORTED;
 
             default:
