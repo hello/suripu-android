bc67c271c8b1aa75521a807395f7a5992620b196
Kevin MacWhinnie <km@sayhello.com>
2014-12-17 15:46:17 -0800

    guts of navigator fragment

--- app/src/main/java/is/hello/sense/ui/fragments/TimelineNavigatorFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/TimelineNavigatorFragment.java
@@ -3,9 +3,13 @@ package is.hello.sense.ui.fragments;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.joda.time.DateTime;
 
@@ -18,6 +22,10 @@ public class TimelineNavigatorFragment extends InjectionFragment {
     private static final String ARG_START_DATE = TimelineNavigatorFragment.class.getName() + ".ARG_START_DATE";
 
     private DateTime startTime;
+
+    private TextView monthText;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     public static TimelineNavigatorFragment newInstance(@NonNull DateTime startTime) {
         TimelineNavigatorFragment fragment = new TimelineNavigatorFragment();
@@ -44,6 +52,24 @@ public class TimelineNavigatorFragment extends InjectionFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_timeline_navigator, container, false);
 
+        this.monthText = (TextView) view.findViewById(R.id.fragment_timeline_navigator_month);
+        monthText.setText(startTime.toString("MMMM"));
+
+
+        this.recyclerView = (RecyclerView) view.findViewById(R.id.fragment_timeline_navigator_recycler_view);
+
+        this.linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        Button todayButton = (Button) view.findViewById(R.id.fragment_timeline_navigator_today);
+        todayButton.setOnClickListener(this::jumpToToday);
+
         return view;
+    }
+
+
+    public void jumpToToday(@NonNull View sender) {
+
     }
 }
