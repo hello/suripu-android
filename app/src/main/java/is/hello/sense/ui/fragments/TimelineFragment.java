4d9217ebd42fce3804d094828517807054217418
Kevin MacWhinnie <km@sayhello.com>
2014-12-22 14:46:29 -0800

    move before sleep header to top + rewrite timestamp scrolling code

--- app/src/main/java/is/hello/sense/ui/fragments/TimelineFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/TimelineFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -59,12 +60,9 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
     private TimelineSegmentAdapter segmentAdapter;
 
     private TimestampTextView timeScrubber;
-    private int timeScrubberTopMargin = 0;
-    private boolean headerTallerThanList = false;
-    private int totalHeaderHeight = 0;
-    private int listViewContentHeight = 0;
+    private float scrollContentAperture;
+    private float timeScrubberTrackHeight;
 
-    private View headerView;
     private ImageButton menuButton;
     private ImageButton shareButton;
     private TextView dateText;
@@ -73,7 +71,8 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
     private TextView messageText;
 
     private TextView timelineEventsHeader;
-    private LinearLayout insightsContainer;
+    private LinearLayout beforeSleepHeader;
+    private LinearLayout beforeSleepItemContainer;
 
     private final Handler fadeOutHandler = new Handler(message -> {
         fadeOutTimeScrubber();
@@ -98,7 +97,6 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
         addPresenter(timelinePresenter);
 
         this.segmentAdapter = new TimelineSegmentAdapter(getActivity());
-        this.timeScrubberTopMargin = getResources().getDimensionPixelSize(R.dimen.gap_medium);
 
         setRetainInstance(true);
     }
@@ -114,7 +112,8 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
         listView.setOnScrollListener(new TimelineScrollListener());
         listView.setOnItemClickListener(this);
 
-        this.headerView = inflater.inflate(R.layout.sub_fragment_timeline_header, listView, false);
+
+        View headerView = inflater.inflate(R.layout.sub_fragment_timeline_header, listView, false);
 
         this.scoreGraph = (PieGraphView) headerView.findViewById(R.id.fragment_timeline_sleep_score_chart);
         this.scoreText = (TextView) headerView.findViewById(R.id.fragment_timeline_sleep_score);
@@ -126,6 +125,10 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
 
         listView.addHeaderView(headerView, null, false);
 
+        this.beforeSleepHeader = (LinearLayout) inflater.inflate(R.layout.sub_fragment_before_sleep, listView, false);
+        this.beforeSleepItemContainer = (LinearLayout) beforeSleepHeader.findViewById(R.id.fragment_timeline_before_sleep_container);
+        beforeSleepHeader.setVisibility(View.GONE);
+        listView.addHeaderView(beforeSleepHeader, null, false);
 
         this.timelineEventsHeader = (TextView) inflater.inflate(R.layout.item_section_header, listView, false);
         timelineEventsHeader.setText(R.string.title_events_timeline);
@@ -150,11 +153,6 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
             shareIntent.putExtra(Intent.EXTRA_TEXT, "http://hello.is");
             startActivity(Intent.createChooser(shareIntent, getString(R.string.action_share)));
         });
-
-
-        this.insightsContainer = (LinearLayout) inflater.inflate(R.layout.sub_fragment_before_sleep, listView, false);
-        insightsContainer.setVisibility(View.GONE);
-        listView.addFooterView(insightsContainer, null, false);
 
         // Always do this after adding headers and footer views,
         // we have to support Android versions under 4.4 KitKat.
@@ -218,24 +216,20 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
 
     public void showInsights(@NonNull List<PreSleepInsight> preSleepInsights) {
         if (preSleepInsights.isEmpty()) {
-            insightsContainer.setVisibility(View.GONE);
+            beforeSleepHeader.setVisibility(View.GONE);
         } else {
+            beforeSleepItemContainer.removeViews(0, beforeSleepItemContainer.getChildCount());
+
             LayoutInflater inflater = LayoutInflater.from(getActivity());
-
-            int childCount = insightsContainer.getChildCount();
-            if (childCount > 2) {
-                insightsContainer.removeViews(2, childCount - 2);
-            }
-
             for (PreSleepInsight preSleepInsight : preSleepInsights) {
-                TextView insightText = (TextView) inflater.inflate(R.layout.item_before_sleep, insightsContainer, false);
-                insightText.setCompoundDrawablesRelativeWithIntrinsicBounds(preSleepInsight.getIconResource(), 0, 0, 0);
-                insightText.setText(preSleepInsight.getMessage());
-                insightsContainer.addView(insightText);
+                ImageView insightImage = (ImageView) inflater.inflate(R.layout.item_before_sleep, beforeSleepItemContainer, false);
+                insightImage.setImageResource(preSleepInsight.getIconResource());
+                insightImage.setTag(R.id.fragment_timeline_before_sleep_item_tag_insight, preSleepInsight);
+                beforeSleepItemContainer.addView(insightImage);
             }
 
-            insightsContainer.setVisibility(View.VISIBLE);
-            insightsContainer.forceLayout();
+            beforeSleepHeader.setVisibility(View.VISIBLE);
+            beforeSleepHeader.forceLayout();
         }
     }
 
@@ -254,13 +248,8 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
                 timelineEventsHeader.setVisibility(View.VISIBLE);
 
                 bindAndSubscribe(Views.observeNextLayout(listView), ignored -> {
-                    this.totalHeaderHeight = headerView.getMeasuredHeight() + timelineEventsHeader.getMeasuredHeight();
-                    this.headerTallerThanList = headerView.getMeasuredHeight() > listView.getMeasuredHeight();
-                    if (headerTallerThanList) {
-                        this.listViewContentHeight = (listView.getMeasuredHeight() - timelineEventsHeader.getMeasuredHeight());
-                    } else {
-                        this.listViewContentHeight = (listView.getMeasuredHeight() - totalHeaderHeight);
-                    }
+                    this.scrollContentAperture = segmentAdapter.getTotalItemHeight() - listView.getMeasuredHeight();
+                    this.timeScrubberTrackHeight = listView.getMeasuredHeight() - timeScrubber.getMeasuredHeight();
 
                     updateTimeScrubber();
                     timeScrubber.forceLayout(); // Does not happen implicitly
@@ -354,39 +343,30 @@ public class TimelineFragment extends InjectionFragment implements SlidingLayers
     }
 
     private void updateTimeScrubber() {
-        View topView = listView.getChildAt(0);
-
         // OnScrollListener will fire immediately after rotation before
         // the list view has laid itself out, have to guard against that.
-        if (topView == null) {
+        if (listView.getChildCount() == 0) {
             return;
         }
 
         int firstVisiblePosition = listView.getFirstVisiblePosition();
         int firstVisibleSegment = ListViews.getAdapterPosition(listView, firstVisiblePosition);
 
-        float scrolledAmount;
-        if (firstVisiblePosition == 0) {
-            scrolledAmount = headerTallerThanList ? 0 : -topView.getTop();
-        } else if (firstVisiblePosition == 1) {
-            scrolledAmount = headerTallerThanList ? 0 : (headerView.getMeasuredHeight() + -topView.getTop());
+        float headerInset;
+        float scrollTop;
+        if (firstVisiblePosition < listView.getHeaderViewsCount()) {
+            headerInset = timelineEventsHeader.getBottom();
+            scrollTop = 0f;
         } else {
+            headerInset = 0f;
+
+            View topView = listView.getChildAt(0);
             float scaleFactor = -topView.getTop() / (float) topView.getMeasuredHeight();
-            float itemsHeight = segmentAdapter.getHeightOfItems(0, firstVisibleSegment, scaleFactor);
-            scrolledAmount = headerTallerThanList ? itemsHeight : (totalHeaderHeight + itemsHeight);
+            scrollTop = segmentAdapter.getHeightOfItems(0, firstVisibleSegment, scaleFactor);
         }
 
-        float multiple = (scrolledAmount / segmentAdapter.getTotalItemHeight());
-        float headerInset = headerTallerThanList ? headerView.getBottom() : headerView.getMeasuredHeight();
-        float timestampY = (timeScrubberTopMargin + headerInset) + (listViewContentHeight * multiple);
-
-        if (insightsContainer.getParent() != null) {
-            int insightsVisibleHeight = listView.getMeasuredHeight() - insightsContainer.getTop();
-            int amountVisible = insightsVisibleHeight / insightsContainer.getMeasuredHeight();
-            float extraMarginFraction = timeScrubberTopMargin * amountVisible;
-            timestampY -= insightsVisibleHeight + extraMarginFraction;
-        }
-
+        float multiple = Math.min(1f, scrollTop / scrollContentAperture);
+        float timestampY = headerInset + (timeScrubberTrackHeight * multiple);
 
         int itemPosition = ListViews.getPositionForY(listView, timestampY);
         TimelineSegment segment = segmentAdapter.getItem(itemPosition);
