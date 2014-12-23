8a80b5863281cf333b05107f809568aa55fe4061
Kevin MacWhinnie <km@sayhello.com>
2014-12-22 17:36:46 -0800

    unbreak insight deserialization

--- app/src/main/java/is/hello/sense/api/model/Insight.java
+++ app/src/main/java/is/hello/sense/api/model/Insight.java
@@ -1,5 +1,10 @@
 package is.hello.sense.api.model;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import org.joda.time.DateTime;
@@ -16,6 +21,9 @@ public class Insight extends ApiResponse {
 
     @JsonProperty("created_utc")
     private DateTime created;
+
+    @JsonProperty("category")
+    private Category category;
 
 
     public long getId() {
@@ -34,6 +42,9 @@ public class Insight extends ApiResponse {
         return created;
     }
 
+    public Category getCategory() {
+        return category;
+    }
 
     @Override
     public String toString() {
@@ -42,6 +53,33 @@ public class Insight extends ApiResponse {
                 ", title='" + title + '\'' +
                 ", message='" + message + '\'' +
                 ", created=" + created +
+                ", category=" + category +
                 '}';
+    }
+
+
+    public static enum Category {
+        GENERIC,
+        SLEEP_HYGIENE,
+        LIGHT,
+        SOUND,
+        TEMPERATURE,
+        HUMIDITY,
+        AIR_QUALITY,
+        SLEEP_DURATION,
+        SLEEP_TIME,
+        WAKEUP_TIME,
+        WORKOUT,
+        CAFFEINE,
+        ALCOHOL,
+        DAYTIME_SLEEPINESS,
+        DIET,
+        SLEEP_QUALITY;
+
+        @JsonCreator
+        @SuppressWarnings("UnusedDeclaration")
+        public static Category fromString(@NonNull String value) {
+                return Enums.fromString(value, values(), GENERIC);
+        }
     }
 }
