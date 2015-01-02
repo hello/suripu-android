f5f70fe5e476abfdfd57e25e082e0fb52703f595
Pang Wu <pang@sayhello.com>
2015-01-02 11:34:48 -0800

    Fix the query time

--- app/src/main/java/is/hello/sense/api/model/SensorHistory.java
+++ app/src/main/java/is/hello/sense/api/model/SensorHistory.java
@@ -61,20 +61,10 @@ public class SensorHistory extends ApiResponse {
     //region Time Zone Fun
 
     /**
-     * Returns the user's current time, in the UTC timezone. For use with sensor history.
+     * Returns the user's current time. For use with sensor history.
      */
     public static long timeForLatest() {
-        DateTime now = DateTime.now();
-        DateTime nowUTC = new DateTime(
-                now.getYear(),
-                now.getMonthOfYear(),
-                now.getDayOfMonth(),
-                now.getHourOfDay(),
-                now.getMinuteOfHour(),
-                now.getSecondOfMinute(),
-                DateTimeZone.UTC
-        );
-        return nowUTC.getMillis();
+        return DateTime.now().getMillis();
     }
 
     //endregion
