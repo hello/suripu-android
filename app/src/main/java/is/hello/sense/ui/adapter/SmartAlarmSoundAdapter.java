d7e19e2e9c17a60275a4e66e189e483f6ee3ae67
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2015-01-08 14:45:18 -0800

    playback smart alarm sounds on click [ref #85727566]

--- app/src/main/java/is/hello/sense/ui/adapter/SmartAlarmSoundAdapter.java
+++ app/src/main/java/is/hello/sense/ui/adapter/SmartAlarmSoundAdapter.java
@@ -1,19 +1,34 @@
 package is.hello.sense.ui.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import is.hello.sense.R;
 import is.hello.sense.api.model.SmartAlarm;
 
 public class SmartAlarmSoundAdapter extends ArrayAdapter<SmartAlarm.Sound> {
-    private long selectedSoundId = -1;
+    public static final int NONE = -1;
 
-    public SmartAlarmSoundAdapter(Context context) {
-        super(context, R.layout.item_simple_text);
+    private final LayoutInflater inflater;
+    private final Resources resources;
+
+    private long selectedSoundId = NONE;
+    private long playingSoundId = NONE;
+    private boolean playingSoundLoading = false;
+
+    public SmartAlarmSoundAdapter(@NonNull Context context) {
+        super(context, R.layout.item_smart_alarm_sound);
+
+        this.inflater = LayoutInflater.from(context);
+        this.resources = context.getResources();
     }
 
     public void setSelectedSoundId(long selectedSoundId) {
@@ -21,19 +36,64 @@ public class SmartAlarmSoundAdapter extends ArrayAdapter<SmartAlarm.Sound> {
         notifyDataSetChanged();
     }
 
+    public void setPlayingSoundId(long id, boolean loading) {
+        this.playingSoundId = id;
+        this.playingSoundLoading = loading;
+
+        notifyDataSetChanged();
+    }
+
+    public long getPlayingSoundId() {
+        return playingSoundId;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView text = (TextView) super.getView(position, convertView, parent);
-
-        SmartAlarm.Sound sound = getItem(position);
-        text.setText(sound.name);
-
-        if (selectedSoundId == sound.id) {
-            text.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.radio_on, 0);
-        } else {
-            text.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.radio_off, 0);
+        View view = convertView;
+        if (view == null) {
+            view = inflater.inflate(R.layout.item_smart_alarm_sound, parent, false);
+            view.setTag(new ViewHolder(view));
         }
 
-        return text;
+        ViewHolder holder = (ViewHolder) view.getTag();
+
+        SmartAlarm.Sound sound = getItem(position);
+        holder.name.setText(sound.name);
+
+        if (selectedSoundId == sound.id) {
+            holder.checked.setImageResource(R.drawable.radio_on);
+        } else {
+            holder.checked.setImageResource(R.drawable.radio_off);
+        }
+
+        if (playingSoundId == sound.id) {
+            holder.name.setTextColor(resources.getColor(R.color.light_accent));
+            if (playingSoundLoading) {
+                holder.checked.setVisibility(View.INVISIBLE);
+                holder.busy.setVisibility(View.VISIBLE);
+            } else {
+                holder.checked.setVisibility(View.VISIBLE);
+                holder.busy.setVisibility(View.GONE);
+            }
+        } else {
+            holder.name.setTextColor(resources.getColor(R.color.text_dark));
+            holder.checked.setVisibility(View.VISIBLE);
+            holder.busy.setVisibility(View.GONE);
+        }
+
+        return view;
+    }
+
+
+    class ViewHolder {
+        final TextView name;
+        final ImageView checked;
+        final ProgressBar busy;
+
+        ViewHolder(@NonNull View view) {
+            this.name = (TextView) view.findViewById(R.id.item_smart_alarm_sound_name);
+            this.checked = (ImageView) view.findViewById(R.id.item_smart_alarm_sound_checked);
+            this.busy = (ProgressBar) view.findViewById(R.id.item_smart_alarm_sound_busy);
+        }
     }
 }
--- app/src/main/java/is/hello/sense/ui/dialogs/SmartAlarmSoundDialogFragment.java
+++ app/src/main/java/is/hello/sense/ui/dialogs/SmartAlarmSoundDialogFragment.java
@@ -3,10 +3,16 @@ package is.hello.sense.ui.dialogs;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.inject.Inject;
@@ -17,7 +23,7 @@ import is.hello.sense.ui.adapter.SmartAlarmSoundAdapter;
 import is.hello.sense.ui.common.InjectionDialogFragment;
 import is.hello.sense.ui.widget.SenseSelectorDialog;
 
-public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment implements SenseSelectorDialog.OnSelectionListener<SmartAlarm.Sound> {
+public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment implements SenseSelectorDialog.OnSelectionListener<SmartAlarm.Sound>, MediaPlayer.OnPreparedListener, MediaPlayer.OnCompletionListener, MediaPlayer.OnErrorListener {
     public static final String ARG_SELECTED_SOUND = SmartAlarmSoundDialogFragment.class.getName() + ".ARG_SELECTED_SOUND";
 
     public static final String TAG = SmartAlarmSoundDialogFragment.class.getSimpleName();
@@ -28,6 +34,8 @@ public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment imple
     private SmartAlarmSoundAdapter adapter;
     private SenseSelectorDialog<SmartAlarm.Sound> dialog;
 
+    private MediaPlayer soundPlayer;
+
     public static SmartAlarmSoundDialogFragment newInstance(@Nullable SmartAlarm.Sound sound) {
         SmartAlarmSoundDialogFragment dialogFragment = new SmartAlarmSoundDialogFragment();
 
@@ -36,6 +44,18 @@ public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment imple
         dialogFragment.setArguments(arguments);
 
         return dialogFragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.soundPlayer = new MediaPlayer();
+
+        soundPlayer.setOnPreparedListener(this);
+        soundPlayer.setOnCompletionListener(this);
+        soundPlayer.setOnErrorListener(this);
+        soundPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
     }
 
     @Override
@@ -59,6 +79,13 @@ public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment imple
         return dialog;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        stopPlayback();
+        soundPlayer.release();
+    }
 
     public void bindSounds(@NonNull ArrayList<SmartAlarm.Sound> sounds) {
         dialog.setActivityIndicatorVisible(false);
@@ -73,11 +100,13 @@ public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment imple
 
 
     @Override
-    public void onItemSelected(@NonNull SenseSelectorDialog<SmartAlarm.Sound> dialog, int position, @NonNull SmartAlarm.Sound item) {
-        this.selectedSound = item;
-        adapter.setSelectedSoundId(item.id);
+    public void onItemSelected(@NonNull SenseSelectorDialog<SmartAlarm.Sound> dialog, int position, @NonNull SmartAlarm.Sound sound) {
+        this.selectedSound = sound;
+        adapter.setSelectedSoundId(sound.id);
         getArguments().putSerializable(ARG_SELECTED_SOUND, selectedSound);
         dialog.setDoneButtonEnabled(true);
+
+        playSound(sound);
     }
 
     @Override
@@ -86,4 +115,58 @@ public class SmartAlarmSoundDialogFragment extends InjectionDialogFragment imple
         response.putExtra(ARG_SELECTED_SOUND, selectedSound);
         getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, response);
     }
+
+
+    //region Playback
+
+    public void playSound(@NonNull SmartAlarm.Sound sound) {
+        stopPlayback();
+
+        try {
+            soundPlayer.setDataSource(getActivity(), Uri.parse(sound.url));
+            soundPlayer.prepareAsync();
+            adapter.setPlayingSoundId(sound.id, true);
+        } catch (IOException e) {
+            presentError(e);
+        }
+    }
+
+    public void stopPlayback() {
+        if (soundPlayer.isPlaying()) {
+            soundPlayer.stop();
+        }
+
+        soundPlayer.reset();
+
+        adapter.setPlayingSoundId(SmartAlarmSoundAdapter.NONE, false);
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        Log.i(getClass().getSimpleName(), "onPrepared");
+
+        adapter.setPlayingSoundId(adapter.getPlayingSoundId(), false);
+        soundPlayer.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        Log.i(getClass().getSimpleName(), "onCompletion");
+
+        stopPlayback();
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int what, int extra) {
+        if (what != -38) {
+            stopPlayback();
+
+            ErrorDialogFragment dialogFragment = ErrorDialogFragment.newInstance("Could not play alarm sound");
+            dialogFragment.show(getFragmentManager(), ErrorDialogFragment.TAG);
+        }
+
+        return true;
+    }
+
+    //endregion
 }
