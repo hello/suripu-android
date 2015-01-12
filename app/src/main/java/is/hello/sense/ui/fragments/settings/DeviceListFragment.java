e9ce8d4bcc1e19d4a1fa9ac57ebf02e55e6b46cf
Kevin MacWhinnie <kevin.macwhinnie@me.com>
2015-01-12 12:50:14 -0800

    first pass on restyling devices list

--- app/src/main/java/is/hello/sense/ui/fragments/settings/DeviceListFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/settings/DeviceListFragment.java
@@ -2,6 +2,7 @@ package is.hello.sense.ui.fragments.settings;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,11 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import net.danlew.android.joda.DateUtils;
+
+import org.joda.time.DateTime;
+import org.joda.time.Minutes;
 
 import java.util.List;
 
@@ -23,13 +27,10 @@ import javax.inject.Inject;
 import is.hello.sense.R;
 import is.hello.sense.api.model.Device;
 import is.hello.sense.graph.presenters.DevicesPresenter;
-import is.hello.sense.ui.activities.OnboardingActivity;
 import is.hello.sense.ui.common.FragmentNavigation;
 import is.hello.sense.ui.common.InjectionFragment;
 import is.hello.sense.ui.dialogs.ErrorDialogFragment;
-import is.hello.sense.ui.widget.util.Views;
-import is.hello.sense.util.Constants;
-import is.hello.sense.util.DateFormatter;
+import is.hello.sense.ui.widget.util.Styles;
 
 import static is.hello.sense.ui.animation.PropertyAnimatorProxy.animate;
 
@@ -37,7 +38,6 @@ public class DeviceListFragment extends InjectionFragment implements AdapterView
     private static final int DEVICE_REQUEST_CODE = 0x14;
 
     @Inject DevicesPresenter devicesPresenter;
-    @Inject DateFormatter dateFormatter;
 
     private ProgressBar loadingIndicator;
     private DevicesAdapter adapter;
@@ -60,18 +60,12 @@ public class DeviceListFragment extends InjectionFragment implements AdapterView
         ListView listView = (ListView) view.findViewById(android.R.id.list);
         listView.setOnItemClickListener(this);
 
-        this.adapter = new DevicesAdapter(getActivity(), dateFormatter);
+        this.adapter = new DevicesAdapter(getActivity());
         listView.setAdapter(adapter);
 
-        this.loadingIndicator = (ProgressBar) view.findViewById(R.id.fragment_settings_device_list_progress);
+        Styles.addCardSpacingHeaderAndFooter(listView);
 
-        ImageButton addDevice = (ImageButton) view.findViewById(R.id.fragment_settings_device_list_add);
-        Views.setSafeOnClickListener(addDevice, ignored -> {
-            Intent onboarding = new Intent(getActivity(), OnboardingActivity.class);
-            onboarding.putExtra(OnboardingActivity.EXTRA_START_CHECKPOINT, Constants.ONBOARDING_CHECKPOINT_QUESTIONS);
-            onboarding.putExtra(OnboardingActivity.EXTRA_PAIR_ONLY, true);
-            startActivity(onboarding);
-        });
+        this.loadingIndicator = (ProgressBar) view.findViewById(R.id.fragment_settings_device_list_progress);
 
         return view;
     }
@@ -129,13 +123,13 @@ public class DeviceListFragment extends InjectionFragment implements AdapterView
 
     private static class DevicesAdapter extends ArrayAdapter<Device> {
         private final LayoutInflater inflater;
-        private final DateFormatter dateFormatter;
+        private final Resources resources;
 
-        private DevicesAdapter(@NonNull Context context, @NonNull DateFormatter dateFormatter) {
+        private DevicesAdapter(@NonNull Context context) {
             super(context, R.layout.item_device);
 
             this.inflater = LayoutInflater.from(context);
-            this.dateFormatter = dateFormatter;
+            this.resources = context.getResources();
         }
 
         @Override
@@ -147,30 +141,63 @@ public class DeviceListFragment extends InjectionFragment implements AdapterView
             }
 
             ViewHolder holder = (ViewHolder) view.getTag();
-
             Device device = getItem(position);
-            holder.icon.setImageResource(device.getType().iconRes);
-            holder.title.setText(device.getType().nameRes);
-            if (device.getState() == Device.State.LOW_BATTERY) {
-                holder.status.setText(device.getState().nameRes);
-            } else {
-                String formattedDate = dateFormatter.formatAsDate(device.getLastUpdated());
-                holder.status.setText(getContext().getString(R.string.device_last_seen_fmt, formattedDate));
-            }
+            holder.display(device);
 
             return view;
         }
 
 
-        private class ViewHolder {
-            private final ImageView icon;
-            private final TextView title;
-            private final TextView status;
+        class ViewHolder {
+            final TextView title;
+            final TextView lastSeen;
+            final TextView status1;
+            final TextView status1Label;
+            final TextView status2;
+            final TextView status2Label;
 
-            private ViewHolder(@NonNull View view) {
-                this.icon = (ImageView) view.findViewById(R.id.item_device_icon);
+            ViewHolder(@NonNull View view) {
                 this.title = (TextView) view.findViewById(R.id.item_device_name);
-                this.status = (TextView) view.findViewById(R.id.item_device_status);
+                this.lastSeen = (TextView) view.findViewById(R.id.item_device_last_seen);
+                this.status1 = (TextView) view.findViewById(R.id.item_device_status);
+                this.status1Label = (TextView) view.findViewById(R.id.item_device_status_label);
+                this.status2 = (TextView) view.findViewById(R.id.item_device_status2);
+                this.status2Label = (TextView) view.findViewById(R.id.item_device_status2_label);
+            }
+
+            void display(@NonNull Device device) {
+                title.setText(device.getType().nameRes);
+                title.setCompoundDrawablesRelativeWithIntrinsicBounds(device.getType().iconRes, 0, 0, 0);
+
+                lastSeen.setText(DateUtils.getRelativeTimeSpanString(getContext(), device.getLastUpdated()));
+                if (Minutes.minutesBetween(device.getLastUpdated(), DateTime.now()).getMinutes() > 15) {
+                    lastSeen.setTextColor(resources.getColor(R.color.destructuve_accent));
+                } else {
+                    lastSeen.setTextColor(resources.getColor(R.color.text_dark));
+                }
+
+                switch (device.getType()) {
+                    case SENSE: {
+                        status1Label.setText(R.string.label_wifi);
+                        status1.setText(R.string.missing_data_placeholder);
+
+                        status2Label.setText(R.string.label_firmware_version);
+                        status2.setText(device.getFirmwareVersion());
+
+                        break;
+                    }
+
+                    default:
+                    case OTHER:
+                    case PILL: {
+                        status1Label.setText(R.string.label_battery_level);
+                        status1Label.setText(device.getState().nameRes);
+
+                        status2Label.setText(R.string.label_color);
+
+                        break;
+                    }
+                }
             }
         }
     }
