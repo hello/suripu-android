69c310e58e2c7e104dbed590c6fecea56169b6ee
Kevin MacWhinnie <km@sayhello.com>
2015-01-27 11:28:41 -0800

    finish basic dialog implementation

--- app/src/main/java/is/hello/sense/ui/dialogs/WelcomeDialog.java
+++ app/src/main/java/is/hello/sense/ui/dialogs/WelcomeDialog.java
@@ -9,19 +9,33 @@ import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import is.hello.sense.R;
+import is.hello.sense.ui.widget.util.Views;
 
 public class WelcomeDialog extends DialogFragment {
-    private static final String ARG_ITEMS = WelcomeDialog.class.getSimpleName() + ".ARG_ITEMS";
+    public static final String TAG = WelcomeDialog.class.getSimpleName();
+
+    private static final String ARG_ITEMS = WelcomeDialog.class.getName() + ".ARG_ITEMS";
 
     private Item[] items;
+    private ItemAdapter adapter;
+    private ViewPager viewPager;
 
 
-    public static WelcomeDialog newInstance(@NonNull Item... items) {
+    //region Lifecycle
+
+    public static WelcomeDialog newInstance(@NonNull Item[] items) {
         WelcomeDialog fragment = new WelcomeDialog();
 
         Bundle arguments = new Bundle();
@@ -44,10 +58,22 @@ public class WelcomeDialog extends DialogFragment {
         Dialog dialog = new Dialog(getActivity(), R.style.AppTheme_Dialog_FullScreen);
         dialog.setContentView(R.layout.dialog_welcome);
 
-        ViewPager viewPager = (ViewPager) dialog.findViewById(R.id.dialog_welcome_view_pager);
-        viewPager.setAdapter(new ItemAdapter(getChildFragmentManager()));
+        this.viewPager = (ViewPager) dialog.findViewById(R.id.dialog_welcome_view_pager);
+        this.adapter = new ItemAdapter(getChildFragmentManager());
+        viewPager.setAdapter(adapter);
 
         return dialog;
+    }
+
+    //endregion
+
+
+    public void next() {
+        if (viewPager.getCurrentItem() < adapter.getCount() - 1) {
+            viewPager.setCurrentItem(viewPager.getCurrentItem() + 1, true);
+        } else {
+            dismiss();
+        }
     }
 
 
@@ -63,22 +89,59 @@ public class WelcomeDialog extends DialogFragment {
 
         @Override
         public Fragment getItem(int position) {
-            return ItemFragment.newInstance(items[position]);
+            return ItemFragment.newInstance(WelcomeDialog.this, items[position]);
         }
     }
 
     public static class ItemFragment extends Fragment {
-        private static final String ARG_ITEM = ItemFragment.class.getName() + ".ARG_ITEMS";
+        private static final String ARG_ITEM = ItemFragment.class.getName() + ".ARG_ITEM";
 
-        public static ItemFragment newInstance(@NonNull Item item) {
+        private Item item;
+        private WelcomeDialog parentFragment;
+
+
+        //region Lifecycle
+
+        public static ItemFragment newInstance(@NonNull WelcomeDialog targetFragment, @NonNull Item item) {
             ItemFragment fragment = new ItemFragment();
 
             Bundle arguments = new Bundle();
             arguments.putParcelable(ARG_ITEM, item);
             fragment.setArguments(arguments);
+            fragment.setTargetFragment(targetFragment, 0x00);
 
             return fragment;
         }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            this.item = getArguments().getParcelable(ARG_ITEMS);
+            this.parentFragment = (WelcomeDialog) getTargetFragment();
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_dialog_welcome_item, container, false);
+
+            ImageView diagram = (ImageView) view.findViewById(R.id.fragment_dialog_welcome_item_diagram);
+            diagram.setImageResource(item.diagramRes);
+
+            TextView title = (TextView) view.findViewById(R.id.fragment_dialog_welcome_item_title);
+            title.setText(item.titleRes);
+
+            TextView message = (TextView) view.findViewById(R.id.fragment_dialog_welcome_item_message);
+            message.setText(item.messageRes);
+
+            Button next = (Button) view.findViewById(R.id.fragment_dialog_welcome_item_next);
+            Views.setSafeOnClickListener(next, ignored -> parentFragment.next());
+
+            return view;
+        }
+
+        //endregion
     }
 
     public static class Item implements Parcelable {
