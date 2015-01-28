d7a94b0b155930a5bdd405e8a8e07cdfe33b0a41
Kevin MacWhinnie <km@sayhello.com>
2015-01-28 14:43:09 -0800

    clamp smart alarm times to nearest 5 minutes [ref #86334676]

--- app/src/main/java/is/hello/sense/ui/dialogs/TimePickerDialogFragment.java
+++ app/src/main/java/is/hello/sense/ui/dialogs/TimePickerDialogFragment.java
@@ -16,6 +16,7 @@ public class TimePickerDialogFragment extends DialogFragment implements TimePick
 
     private static final String ARG_DATE = TimePickerDialogFragment.class.getName() + ".ARG_DATE";
     private static final String ARG_USE_24_TIME = TimePickerDialogFragment.class.getName() + ".ARG_USE_24_TIME";
+
     public static final String RESULT_HOUR = TimePickerDialogFragment.class.getName() + ".RESULT_HOUR";
     public static final String RESULT_MINUTE = TimePickerDialogFragment.class.getName() + ".RESULT_MINUTE";
 
@@ -43,7 +44,29 @@ public class TimePickerDialogFragment extends DialogFragment implements TimePick
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        return new TimePickerDialog(getActivity(), this, time.getHourOfDay(), time.getMinuteOfHour(), use24Time);
+        return new TimePickerDialog(getActivity(), this, time.getHourOfDay(), time.getMinuteOfHour(), use24Time) {
+            boolean ignoreEvent = false;
+
+            @Override
+            public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+                // This only works in KitKat and below. Lollipop introduced
+                // a new appearance for the picker that uses a radial clock,
+                // and this new appearance only sends `onTimeChanged`
+                // notifications after the user has finished editing all
+                // of the fields in the picker. For Lollipop+, we just clamp
+                // the time before sending it back to the invoker of this class.
+
+                if (ignoreEvent) {
+                    return;
+                }
+
+                if (shouldClampMinute(minute)) {
+                    ignoreEvent = true;
+                    view.setCurrentMinute(clampMinute(minute));
+                    ignoreEvent = false;
+                }
+            }
+        };
     }
 
     @Override
@@ -51,8 +74,34 @@ public class TimePickerDialogFragment extends DialogFragment implements TimePick
         if (getTargetFragment() != null) {
             Intent response = new Intent();
             response.putExtra(RESULT_HOUR, hour);
-            response.putExtra(RESULT_MINUTE, minute);
+            response.putExtra(RESULT_MINUTE, clampMinute(minute));
             getTargetFragment().onActivityResult(getTargetRequestCode(), Activity.RESULT_OK, response);
         }
     }
+
+
+    //region Clamping Times
+
+    private static final int MINUTE_INTERVAL = 5;
+
+    private static boolean shouldClampMinute(int minute) {
+        return ((minute % MINUTE_INTERVAL) != 0);
+    }
+
+    private static int clampMinute(int minute) {
+        if (shouldClampMinute(minute)) {
+            // From <http://stackoverflow.com/questions/2580216/android-timepicker-minutes-to-15>
+            int remainder = minute % MINUTE_INTERVAL;
+            int minuteFloor = minute - remainder;
+            int clampedMinute = minuteFloor + ((minute == minuteFloor + 1) ? MINUTE_INTERVAL : 0);
+            if (clampedMinute >= 60) {
+                clampedMinute = 0;
+            }
+            return clampedMinute;
+        } else {
+            return minute;
+        }
+    }
+
+    //endregion
 }
