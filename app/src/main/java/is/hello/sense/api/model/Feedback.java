cd0adc354c326db1f659c9e56c9942c2ef7be5dc
Kevin MacWhinnie <km@sayhello.com>
2015-02-09 16:53:46 -0800

    wrap feedback API

---
+++ app/src/main/java/is/hello/sense/api/model/Feedback.java
@@ -0,0 +1,69 @@
+package is.hello.sense.api.model;
+
+import android.support.annotation.NonNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import is.hello.sense.api.ApiService;
+
+public class Feedback extends ApiResponse {
+    @JsonProperty("event_type")
+    private TimelineSegment.EventType eventType;
+
+    @JsonProperty("day")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = ApiService.DATE_FORMAT)
+    private LocalDate day;
+
+    @JsonProperty("ts")
+    private DateTime timestamp;
+
+
+    public Feedback() {
+
+    }
+
+    public Feedback(@NonNull DateTime timestamp, @NonNull TimelineSegment.EventType eventType) {
+        this.day = timestamp.toLocalDate();
+        this.timestamp = timestamp;
+        this.eventType = eventType;
+    }
+
+
+    public TimelineSegment.EventType getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(TimelineSegment.EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    public LocalDate getDay() {
+        return day;
+    }
+
+    public void setDay(LocalDate day) {
+        this.day = day;
+    }
+
+    public DateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(DateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Feedback{" +
+                "eventType=" + eventType +
+                ", day=" + day +
+                ", timestamp=" + timestamp +
+                '}';
+    }
+}
