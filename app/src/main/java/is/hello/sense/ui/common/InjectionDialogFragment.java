f17d996abe11776d90a18e27785053040fd41b01
Kevin MacWhinnie <km@sayhello.com>
2015-02-10 10:10:04 -0800

    fix NPE [ref #88066714]

--- app/src/main/java/is/hello/sense/ui/common/InjectionDialogFragment.java
+++ app/src/main/java/is/hello/sense/ui/common/InjectionDialogFragment.java
@@ -1,7 +1,6 @@
 package is.hello.sense.ui.common;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -9,19 +8,8 @@ import android.support.annotation.Nullable;
 import is.hello.sense.SenseApplication;
 import is.hello.sense.graph.presenters.Presenter;
 import is.hello.sense.graph.presenters.PresenterContainer;
-import is.hello.sense.util.ResumeScheduler;
-import rx.Observable;
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.functions.Func1;
 
-public class InjectionDialogFragment extends DialogFragment implements ObservableContainer {
-    protected final ResumeScheduler.Coordinator coordinator = new ResumeScheduler.Coordinator(this::isResumed);
-    protected final ResumeScheduler observeScheduler = new ResumeScheduler(coordinator);
-
-    protected static final Func1<DialogFragment, Boolean> FRAGMENT_VALIDATOR = (fragment) -> fragment.isAdded() && !fragment.getActivity().isFinishing();
-    protected final DelegateObservableContainer<DialogFragment> observableContainer = new DelegateObservableContainer<>(observeScheduler, this, FRAGMENT_VALIDATOR);
-
+public class InjectionDialogFragment extends SenseDialogFragment implements ObservableContainer {
     protected final PresenterContainer presenterContainer = new PresenterContainer();
 
     public InjectionDialogFragment() {
@@ -49,7 +37,6 @@ public class InjectionDialogFragment extends DialogFragment implements Observabl
     public void onResume() {
         super.onResume();
 
-        coordinator.resume();
         presenterContainer.onContainerResumed();
     }
 
@@ -78,36 +65,6 @@ public class InjectionDialogFragment extends DialogFragment implements Observabl
         super.onDestroy();
 
         presenterContainer.onContainerDestroyed();
-    }
-
-
-    @Override
-    public boolean hasSubscriptions() {
-        return observableContainer.hasSubscriptions();
-    }
-
-    @Override
-    @NonNull
-    public Subscription track(@NonNull Subscription subscription) {
-        return observableContainer.track(subscription);
-    }
-
-    @Override
-    @NonNull
-    public <T> Observable<T> bind(@NonNull Observable<T> toBind) {
-        return observableContainer.bind(toBind);
-    }
-
-    @Override
-    @NonNull
-    public <T> Subscription subscribe(@NonNull Observable<T> toSubscribe, Action1<? super T> onNext, Action1<Throwable> onError) {
-        return observableContainer.subscribe(toSubscribe, onNext, onError);
-    }
-
-    @Override
-    @NonNull
-    public <T> Subscription bindAndSubscribe(@NonNull Observable<T> toSubscribe, Action1<? super T> onNext, Action1<Throwable> onError) {
-        return observableContainer.bindAndSubscribe(toSubscribe, onNext, onError);
     }
 
 
