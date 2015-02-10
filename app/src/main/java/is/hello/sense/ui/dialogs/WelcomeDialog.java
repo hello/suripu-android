31c8491aebcfebb8b9eab678d81a0e4dfaf1ae50
Kevin MacWhinnie <km@sayhello.com>
2015-02-10 14:30:55 -0800

    create a proper PageDots view

--- app/src/main/java/is/hello/sense/ui/dialogs/WelcomeDialog.java
+++ app/src/main/java/is/hello/sense/ui/dialogs/WelcomeDialog.java
@@ -19,7 +19,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParserException;
@@ -27,19 +26,16 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 
 import is.hello.sense.R;
+import is.hello.sense.ui.widget.PageDots;
 import is.hello.sense.ui.widget.util.Views;
 import is.hello.sense.util.Constants;
 import is.hello.sense.util.Logger;
 import is.hello.sense.util.WelcomeDialogParser;
 
-import static android.widget.LinearLayout.LayoutParams;
-
-public class WelcomeDialog extends DialogFragment implements ViewPager.OnPageChangeListener {
+public class WelcomeDialog extends DialogFragment {
     public static final String TAG = WelcomeDialog.class.getSimpleName();
 
     private static final String ARG_ITEMS = WelcomeDialog.class.getName() + ".ARG_ITEMS";
-
-    private LinearLayout pageDots;
 
     private Item[] items;
     private ItemAdapter adapter;
@@ -98,11 +94,8 @@ public class WelcomeDialog extends DialogFragment implements ViewPager.OnPageCha
     }
 
     public static boolean showIfNeeded(@NonNull Activity activity, @XmlRes int welcomeRes) {
-        if (!shouldShow(activity, welcomeRes)) {
-            return false;
-        }
-
-        return show(activity, welcomeRes);
+        return (shouldShow(activity, welcomeRes) &&
+                show(activity, welcomeRes));
     }
 
     public static WelcomeDialog newInstance(@NonNull Item[] items) {
@@ -130,31 +123,20 @@ public class WelcomeDialog extends DialogFragment implements ViewPager.OnPageCha
         dialog.setCanceledOnTouchOutside(false);
         dialog.setCancelable(true);
 
-        this.pageDots = (LinearLayout) dialog.findViewById(R.id.dialog_welcome_page_dots);
-        if (items.length > 1) {
-            int dotSize = getResources().getDimensionPixelSize(R.dimen.page_dot_size);
-            int dotMargin = getResources().getDimensionPixelSize(R.dimen.page_dot_margin);
-            LayoutParams dotLayout = new LayoutParams(dotSize, dotSize);
-            dotLayout.setMargins(dotMargin, dotMargin, dotMargin, dotMargin);
-            for (Item ignored : items) {
-                ImageView dot = new ImageView(getActivity());
-                dot.setScaleType(ImageView.ScaleType.CENTER);
-                dot.setImageResource(R.drawable.page_dot_selected);
-                pageDots.addView(dot, dotLayout);
-            }
-            setSelectedPageDot(0);
-        }
-
         this.viewPager = (ViewPager) dialog.findViewById(R.id.dialog_welcome_view_pager);
 
         int pageMargin = getResources().getDimensionPixelSize(R.dimen.gap_outer);
         viewPager.setClipToPadding(false);
         viewPager.setPadding(pageMargin, 0, pageMargin, 0);
         viewPager.setPageMargin(pageMargin);
-        viewPager.setOnPageChangeListener(this);
 
         this.adapter = new ItemAdapter();
         viewPager.setAdapter(adapter);
+
+        if (items.length > 1) {
+            PageDots pageDots = (PageDots) dialog.findViewById(R.id.dialog_welcome_page_dots);
+            pageDots.attach(viewPager);
+        }
 
         int maxWidth = getResources().getDimensionPixelSize(R.dimen.dialog_max_width);
         Views.observeNextLayout(dialog.getWindow().getDecorView())
@@ -173,30 +155,6 @@ public class WelcomeDialog extends DialogFragment implements ViewPager.OnPageCha
 
     //endregion
 
-
-    public void setSelectedPageDot(int selection) {
-        for (int i = 0, count = pageDots.getChildCount(); i < count; i++) {
-            ImageView dot = (ImageView) pageDots.getChildAt(i);
-            if (i == selection) {
-                dot.setImageResource(R.drawable.page_dot_selected);
-            } else {
-                dot.setImageResource(R.drawable.page_dot_unselected);
-            }
-        }
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        setSelectedPageDot(position);
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-    }
 
     public void next() {
         if (viewPager.getCurrentItem() < adapter.getCount() - 1) {
---
+++ app/src/main/java/is/hello/sense/ui/widget/PageDots.java
@@ -0,0 +1,193 @@
+package is.hello.sense.ui.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IntDef;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import is.hello.sense.R;
+
+public final class PageDots extends LinearLayout implements ViewPager.OnPageChangeListener {
+    //region Styles
+
+    public static final int STYLE_WHITE = 0;
+    public static final int STYLE_BLUE = 1;
+
+    @IntDef({STYLE_WHITE, STYLE_BLUE})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface DotStyle {}
+
+    //endregion
+
+    private final LayoutParams dotLayout;
+
+    private Drawable selectedDrawable;
+    private Drawable unselectedDrawable;
+    private int count = 0;
+    private int selection = 0;
+
+    public PageDots(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public PageDots(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public PageDots(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+
+        Resources resources = getResources();
+        int padding = resources.getDimensionPixelSize(R.dimen.gap_medium);
+        setPadding(padding, padding, padding, padding);
+        setOrientation(HORIZONTAL);
+        setGravity(Gravity.CENTER);
+
+
+        int dotSize = resources.getDimensionPixelSize(R.dimen.page_dot_size);
+        this.dotLayout = new LayoutParams(dotSize, dotSize);
+
+        int dotMargin = resources.getDimensionPixelSize(R.dimen.page_dot_margin);
+        dotLayout.setMargins(dotMargin, dotMargin, dotMargin, dotMargin);
+
+
+        if (attrs != null) {
+            TypedArray styles = getContext().obtainStyledAttributes(attrs, R.styleable.PageDots, defStyleAttr, 0);
+
+            @DotStyle int dotStyle = styles.getInt(R.styleable.PageDots_dotStyle, STYLE_WHITE);
+            setDotStyle(dotStyle);
+
+            styles.recycle();
+        }
+    }
+
+
+    //region Internal
+
+    private ImageView createDotView() {
+        ImageView dotView = new ImageView(getContext());
+        dotView.setScaleType(ImageView.ScaleType.CENTER);
+        dotView.setImageDrawable(unselectedDrawable);
+        return dotView;
+    }
+
+    private void updateCount(int newCount) {
+        if (newCount > getChildCount()) {
+            int delta = newCount - getChildCount();
+            for (int i = 0; i < delta; i++) {
+                addView(createDotView(), dotLayout);
+            }
+        } else if (newCount < getChildCount()) {
+            removeViews(newCount - 1, getChildCount());
+        }
+    }
+
+    private void syncSelection() {
+        for (int i = 0; i < count; i++) {
+            ImageView dotView = (ImageView) getChildAt(i);
+            if (i == selection) {
+                dotView.setImageDrawable(selectedDrawable);
+            } else {
+                dotView.setImageDrawable(unselectedDrawable);
+            }
+        }
+    }
+
+    //endregion
+
+
+    //region Properties
+
+    public void setSelectedDrawable(@NonNull Drawable selectedDrawable) {
+        this.selectedDrawable = selectedDrawable;
+        syncSelection();
+    }
+
+    public void setSelectedResource(@DrawableRes int activeRes) {
+        setSelectedDrawable(getResources().getDrawable(activeRes));
+    }
+
+    public void setUnselectedDrawable(@NonNull Drawable unselectedDrawable) {
+        this.unselectedDrawable = unselectedDrawable;
+        syncSelection();
+    }
+
+    public void setUnselectedResource(@DrawableRes int inactiveRes) {
+        setUnselectedDrawable(getResources().getDrawable(inactiveRes));
+    }
+
+    public void setDotStyle(@DotStyle int dotStyle) {
+        switch (dotStyle) {
+            case STYLE_WHITE: {
+                setSelectedResource(R.drawable.page_dot_white_selected);
+                setUnselectedResource(R.drawable.page_dot_white_unselected);
+                break;
+            }
+
+            case STYLE_BLUE: {
+                break;
+            }
+
+            default: {
+                throw new IllegalArgumentException("Unknown style " + dotStyle);
+            }
+        }
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+
+        updateCount(count);
+        syncSelection();
+    }
+
+    public void setSelection(int selection) {
+        this.selection = selection;
+
+        syncSelection();
+    }
+
+    public void attach(@NonNull ViewPager viewPager) {
+        viewPager.setOnPageChangeListener(this);
+        PagerAdapter adapter = viewPager.getAdapter();
+        if (adapter != null) {
+            setCount(adapter.getCount());
+        }
+    }
+
+    //endregion
+
+
+    //region OnPageChangeListener
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        setSelection(position);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    //endregion
+}
