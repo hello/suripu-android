851484294ce42d1c61ff916ee67aad1d51a0645d
Kevin MacWhinnie <km@sayhello.com>
2015-02-17 14:39:02 -0800

    add test stubs

--- app/src/androidTest/java/is/hello/sense/graph/presenters/HardwarePresenterTests.java
+++ app/src/androidTest/java/is/hello/sense/graph/presenters/HardwarePresenterTests.java
@@ -6,4 +6,28 @@ import is.hello.sense.graph.InjectionTestCase;
 
 public class HardwarePresenterTests extends InjectionTestCase {
     @Inject HardwarePresenter hardwarePresenter;
+
+    public void testErrorsResetPeripheral() throws Exception {
+        fail();
+    }
+
+    public void testClearsPeripheralOnBluetoothDisable() throws Exception {
+        fail();
+    }
+
+    public void testConnectivityGetters() throws Exception {
+        fail();
+    }
+
+    public void testNoDeviceErrors() throws Exception {
+        fail();
+    }
+
+    public void testWifiNetworksSorted() throws Exception {
+        fail();
+    }
+
+    public void testClearPeripheral() throws Exception {
+        fail();
+    }
 }
