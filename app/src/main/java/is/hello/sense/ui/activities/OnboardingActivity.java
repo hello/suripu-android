c50943e0f9ccd1a8836602176d9c185daf912b8d
Kevin MacWhinnie <km@sayhello.com>
2015-02-23 14:44:42 -0800

    fix incorrect URL reference [ref #88932962]

--- app/src/main/java/is/hello/sense/ui/activities/OnboardingActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/OnboardingActivity.java
@@ -356,7 +356,7 @@ public class OnboardingActivity extends InjectionActivity implements FragmentNav
                 builder.setDiagramImage(R.drawable.onboarding_sense_intro);
                 builder.setNextFragmentClass(OnboardingPairSenseFragment.class);
                 builder.setAnalyticsEvent(Analytics.Onboarding.EVENT_SENSE_SETUP);
-                builder.setHelpStep(UserSupport.OnboardingStep.PAIRING_SENSE_BLE);
+                builder.setHelpStep(UserSupport.OnboardingStep.SETTING_UP_SENSE);
                 pushFragment(builder.toFragment(), null, false);
             }
         } else {
