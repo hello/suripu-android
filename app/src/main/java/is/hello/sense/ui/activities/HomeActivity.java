ade2b517418f6b97fdca4a30d996c42e7b3f126c
Kevin MacWhinnie <km@sayhello.com>
2015-02-23 15:01:24 -0800

    make pill pair error vaguer [ref #88834928]

--- app/src/main/java/is/hello/sense/ui/activities/HomeActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/HomeActivity.java
@@ -20,7 +20,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.seismic.ShakeDetector;
 
