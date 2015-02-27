d436517f759e78d14d3600b50b5c92e6d00033b0
Kevin MacWhinnie <km@sayhello.com>
2015-02-27 15:30:49 -0800

    most of the core handholding crap

--- app/src/main/java/is/hello/sense/ui/handholding/Tutorial.java
+++ app/src/main/java/is/hello/sense/ui/handholding/Tutorial.java
@@ -1,25 +1,52 @@
 package is.hello.sense.ui.handholding;
 
+import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
+import android.view.Gravity;
+import android.widget.RelativeLayout;
 
 import java.io.Serializable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import static android.widget.RelativeLayout.ALIGN_PARENT_BOTTOM;
+import static android.widget.RelativeLayout.ALIGN_PARENT_TOP;
 
 public class Tutorial implements Serializable {
-    public final String description;
+    @IntDef({Gravity.TOP, Gravity.BOTTOM})
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface DescriptionGravity {}
+
+    public final CharSequence description;
+    public final @DescriptionGravity int descriptionGravity;
     public final Interaction interaction;
 
-    public Tutorial(@NonNull String description,
+    public Tutorial(@NonNull CharSequence description,
+                    @DescriptionGravity int descriptionGravity,
                     @NonNull Interaction interaction) {
         this.description = description;
+        this.descriptionGravity = descriptionGravity;
         this.interaction = interaction;
     }
 
 
-    @Override
-    public String toString() {
-        return "Tutorial{" +
-                "description='" + description + '\'' +
-                ", interaction=" + interaction +
-                '}';
+    public RelativeLayout.LayoutParams generateDescriptionLayoutParams() {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        switch (descriptionGravity) {
+            case Gravity.TOP: {
+                layoutParams.addRule(ALIGN_PARENT_TOP);
+                break;
+            }
+
+            case Gravity.BOTTOM: {
+                layoutParams.addRule(ALIGN_PARENT_BOTTOM);
+                break;
+            }
+
+            default: {
+                throw new IllegalArgumentException("Unknown gravity constant " + descriptionGravity);
+            }
+        }
+        return layoutParams;
     }
 }
--- app/src/main/java/is/hello/sense/ui/handholding/TutorialDialogFragment.java
+++ app/src/main/java/is/hello/sense/ui/handholding/TutorialDialogFragment.java
@@ -1,16 +1,29 @@
 package is.hello.sense.ui.handholding;
 
 import android.app.Dialog;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import is.hello.sense.R;
 import is.hello.sense.ui.common.SenseDialogFragment;
+import is.hello.sense.ui.widget.util.Views;
+
+import static android.widget.RelativeLayout.ALIGN_PARENT_LEFT;
+import static android.widget.RelativeLayout.ALIGN_PARENT_TOP;
+import static android.widget.RelativeLayout.LayoutParams;
 
 public class TutorialDialogFragment extends SenseDialogFragment {
+    public static final String TAG = TutorialDialogFragment.class.getSimpleName();
+
     private static final String ARG_TUTORIAL = TutorialDialogFragment.class.getName() + ".ARG_TUTORIAL";
 
     private Tutorial tutorial;
+    private RelativeLayout contentLayout;
 
     public static TutorialDialogFragment newInstance(@NonNull Tutorial tutorial) {
         TutorialDialogFragment dialogFragment = new TutorialDialogFragment();
@@ -31,8 +44,52 @@ public class TutorialDialogFragment extends SenseDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        Dialog overlay = new Dialog(getActivity(), R.style.AppTheme_Dialog_FullScreen);
+        Dialog dialog = new Dialog(getActivity(), R.style.AppTheme_Dialog_FullScreen);
 
-        return overlay;
+        this.contentLayout = new RelativeLayout(getActivity());
+        dialog.setContentView(contentLayout);
+
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        TextView descriptionText = (TextView) inflater.inflate(R.layout.sub_fragment_tutorial_description, contentLayout, false);
+        descriptionText.setText(tutorial.description);
+        descriptionText.setOnClickListener(ignored -> dismissSafely());
+        contentLayout.addView(descriptionText, tutorial.generateDescriptionLayoutParams());
+
+        Interaction interaction = tutorial.interaction;
+        View anchorView = getActivity().findViewById(interaction.anchorViewRes);
+        if (anchorView != null) {
+            if (anchorView.isLayoutRequested()) {
+                Views.observeNextLayout(anchorView)
+                     .subscribe(this::addInteractionIndicator);
+            } else {
+                addInteractionIndicator(anchorView);
+            }
+        }
+
+        return dialog;
+    }
+
+
+    private void addInteractionIndicator(@NonNull View anchorView) {
+        InteractionView interactionView = new InteractionView(getActivity());
+
+        int[] anchorCoordinates = {0, 0};
+        anchorView.getLocationInWindow(anchorCoordinates);
+        Rect windowFrame = new Rect();
+        anchorView.getWindowVisibleDisplayFrame(windowFrame);
+
+        int anchorMidX = anchorCoordinates[0] - windowFrame.left + anchorView.getMeasuredWidth() / 2;
+        int anchorMidY = anchorCoordinates[1] - windowFrame.top + anchorView.getMeasuredHeight() / 2;
+
+        int interactionMidX = interactionView.getMinimumWidth() / 2;
+        int interactionMidY = interactionView.getMinimumHeight() / 2;
+
+        LayoutParams layoutParams = new LayoutParams(interactionView.getMinimumWidth(), interactionView.getMinimumHeight());
+        layoutParams.addRule(ALIGN_PARENT_TOP);
+        layoutParams.addRule(ALIGN_PARENT_LEFT);
+        layoutParams.leftMargin = anchorMidX - interactionMidX;
+        layoutParams.topMargin = anchorMidY - interactionMidY;
+
+        contentLayout.addView(interactionView, layoutParams);
     }
 }
