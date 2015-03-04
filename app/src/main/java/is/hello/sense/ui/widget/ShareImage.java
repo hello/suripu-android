f683937abcbb6cdabb58cd42846b1700c6bd0e85
Kevin MacWhinnie <km@sayhello.com>
2015-03-04 15:55:45 -0800

    many improvements

--- app/src/main/java/is/hello/sense/ui/widget/ShareImage.java
+++ app/src/main/java/is/hello/sense/ui/widget/ShareImage.java
@@ -6,7 +6,6 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
@@ -24,20 +23,31 @@ import rx.schedulers.Schedulers;
 public final class ShareImage implements Observable.OnSubscribe<ShareImage.Result> {
     private final Context context;
     private final Timeline timeline;
+    private final Resources resources;
 
     private final int width;
     private final int height;
 
+    private final int leftInset;
+    private final int rightInset;
+
     private final int scoreWidth;
     private final int scoreHeight;
+    private final int scoreTextHeight;
 
     private final int baseSegmentHeight;
+    private final int footerHeight;
+    private final int footerTextHeight;
+    private final int footerInset;
+
+    private final String leftFooterText;
+    private final String rightFooterText;
 
     private final Paint fillPaint = new Paint();
-    private final Paint scoreFillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private final Paint shadowedFillPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private final Paint scoreTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.SUBPIXEL_TEXT_FLAG);
+    private final Paint footerTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.SUBPIXEL_TEXT_FLAG);
     private final RectF ovalRect = new RectF();
-    private final Rect textRect = new Rect();
 
     //region Creation
 
@@ -52,21 +62,39 @@ public final class ShareImage implements Observable.OnSubscribe<ShareImage.Resul
         this.context = context;
         this.timeline = timeline;
 
-        Resources resources = context.getResources();
+        this.resources = context.getResources();
+        Paint.FontMetricsInt fontMetrics = new Paint.FontMetricsInt();
         this.width = resources.getDimensionPixelSize(R.dimen.share_image_width);
         this.height = resources.getDimensionPixelSize(R.dimen.share_image_height);
+
+        this.leftInset = resources.getDimensionPixelSize(R.dimen.view_timeline_segment_left_inset);
+        this.rightInset = resources.getDimensionPixelSize(R.dimen.view_timeline_segment_right_inset);
 
         this.scoreWidth = resources.getDimensionPixelSize(R.dimen.grand_sleep_summary_width);
         this.scoreHeight = resources.getDimensionPixelSize(R.dimen.grand_sleep_summary_height);
 
-        scoreFillPaint.setColor(Color.WHITE);
-        scoreFillPaint.setShadowLayer(20, 0, 0, 0x80009CFF);
+        this.footerHeight = resources.getDimensionPixelSize(R.dimen.share_image_footer_height);
+        this.footerInset = resources.getDimensionPixelSize(R.dimen.share_image_footer_inset);
 
+        shadowedFillPaint.setColor(Color.WHITE);
+        shadowedFillPaint.setShadowLayer(20, 0, 0, resources.getColor(R.color.share_image_shadow));
+
+        scoreTextPaint.setTextAlign(Paint.Align.CENTER);
         scoreTextPaint.setTextSize(resources.getDimensionPixelOffset(R.dimen.text_size_big_score));
         scoreTextPaint.setTypeface(Typeface.create("sans-serif-thin", Typeface.NORMAL));
+        scoreTextPaint.getFontMetricsInt(fontMetrics);
+        this.scoreTextHeight = fontMetrics.top + fontMetrics.descent;
+
+        footerTextPaint.setTextSize(resources.getDimensionPixelOffset(R.dimen.text_size_body));
+        footerTextPaint.setColor(resources.getColor(R.color.text_light));
+        footerTextPaint.getFontMetricsInt(fontMetrics);
+        this.footerTextHeight = fontMetrics.top + fontMetrics.descent;
 
         int minItemHeight = resources.getDimensionPixelSize(R.dimen.timeline_segment_min_height);
-        this.baseSegmentHeight = Math.max(minItemHeight, height / Styles.TIMELINE_HOURS_ON_SCREEN);
+        this.baseSegmentHeight = Math.max(minItemHeight, height / Styles.TIMELINE_HOURS_ON_SCREEN) * 2;
+
+        this.leftFooterText = timeline.getDate().toString("MMMM dd, yyyy");
+        this.rightFooterText = "https://hello.is";
     }
 
     //endregion
@@ -89,25 +117,35 @@ public final class ShareImage implements Observable.OnSubscribe<ShareImage.Resul
             maxX = width,
             midX = maxX / 2;
         int minY = 0,
-            maxY = height,
+            maxY = height - footerHeight,
             midY = maxY / 2;
+
+
+        // Background
 
         fillPaint.setColor(Color.WHITE);
         canvas.drawRect(minX, minY, maxX, maxY, fillPaint);
 
+
+        // Segments
+
         if (!Lists.isEmpty(timeline.getSegments())) {
+            int segmentMaxX = width - (leftInset + rightInset);
+            int segmentMidX = leftInset + segmentMaxX / 2;
             int segmentMinY = minY;
             for (TimelineSegment segment : timeline.getSegments()) {
                 int itemHeight = (int) ((segment.getDuration() / 3600f) * baseSegmentHeight);
                 int segmentMaxY = segmentMinY + itemHeight;
 
                 int sleepDepth = segment.getSleepDepth();
-                int colorRes = Styles.getSleepDepthColorRes(sleepDepth, segment.isBeforeSleep());
-                fillPaint.setColor(context.getResources().getColor(colorRes));
+                int colorRes = Styles.getSleepDepthColorRes(sleepDepth, false);
+                fillPaint.setColor(resources.getColor(colorRes));
 
                 float percentage = sleepDepth / 100f;
-                float halfWidth = Math.round(maxX * percentage) / 2f;
-                canvas.drawRect(midX - halfWidth, segmentMinY, midX + halfWidth, segmentMaxY, fillPaint);
+                float halfWidth = Math.round(segmentMaxX * percentage) / 2f;
+                canvas.drawRect(segmentMidX - halfWidth, segmentMinY,
+                                segmentMidX + halfWidth, segmentMaxY,
+                                fillPaint);
 
                 segmentMinY = segmentMaxY;
 
@@ -117,18 +155,34 @@ public final class ShareImage implements Observable.OnSubscribe<ShareImage.Resul
             }
         }
 
+        fillPaint.setColor(resources.getColor(R.color.share_image_tint));
+        canvas.drawRect(minX, minY, maxX, maxY, fillPaint);
+
+
+        // Score
+
         ovalRect.set(midX - scoreWidth / 2f, midY - scoreHeight / 2f,
-                midX + scoreWidth / 2f, midY + scoreHeight / 2f);
-        canvas.drawOval(ovalRect, scoreFillPaint);
+                     midX + scoreWidth / 2f, midY + scoreHeight / 2f);
+        canvas.drawOval(ovalRect, shadowedFillPaint);
 
         int score = timeline.getScore();
-        scoreTextPaint.setColor(Styles.getSleepScoreColor(context, score));
         String scoreText = Integer.toString(score);
-        scoreTextPaint.getTextBounds(scoreText, 0, scoreText.length(), textRect);
+        scoreTextPaint.setColor(Styles.getSleepScoreColor(context, score));
+        float scoreTextY = midY - scoreTextHeight / 2f;
+        canvas.drawText(scoreText, midX, scoreTextY, scoreTextPaint);
 
-        float scoreX = midX - textRect.centerX();
-        float scoreY = midY - textRect.centerY();
-        canvas.drawText(scoreText, scoreX, scoreY, scoreTextPaint);
+
+        //region Footer
+
+        canvas.drawRect(minX, maxY, maxX, maxY + footerHeight, shadowedFillPaint);
+
+        float footerTextY = maxY + ((footerHeight / 2f) - footerTextHeight / 2f);
+
+        footerTextPaint.setTextAlign(Paint.Align.LEFT);
+        canvas.drawText(leftFooterText, minX + footerInset, footerTextY, footerTextPaint);
+
+        footerTextPaint.setTextAlign(Paint.Align.RIGHT);
+        canvas.drawText(rightFooterText, maxX - footerInset, footerTextY, footerTextPaint);
     }
 
     //endregion
