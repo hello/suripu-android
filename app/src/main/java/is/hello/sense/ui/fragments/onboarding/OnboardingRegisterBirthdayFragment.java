c63cf81852000e83ed6a9b30b538428b5304c4f5
km <km@sayhello.com>
2015-05-06 17:04:55 -0700

    work around some phones having a broken impl of DateFormat#getDateFormatOrder(Context)

--- app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingRegisterBirthdayFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingRegisterBirthdayFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.text.format.DateFormat;
 import android.view.HapticFeedbackConstants;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -24,6 +23,7 @@ import is.hello.sense.ui.activities.OnboardingActivity;
 import is.hello.sense.ui.common.AccountEditingFragment;
 import is.hello.sense.ui.widget.util.Views;
 import is.hello.sense.util.Analytics;
+import is.hello.sense.util.DateFormatter;
 
 public class OnboardingRegisterBirthdayFragment extends AccountEditingFragment {
     private static final int NUM_FIELDS = 3;
@@ -59,7 +59,7 @@ public class OnboardingRegisterBirthdayFragment extends AccountEditingFragment {
         LinearLayout fieldContainer = (LinearLayout) view.findViewById(R.id.fragment_onboarding_birthday_fields);
         int hintColor = getResources().getColor(R.color.text_dim_placeholder);
 
-        char[] dateFormat = DateFormat.getDateFormatOrder(getActivity());
+        char[] dateFormat = DateFormatter.getDateFormatOrder(getActivity());
         int index = 0;
         for (char field : dateFormat) {
             TextView component = (TextView) inflater.inflate(R.layout.item_onboarding_birthday_field, fieldContainer, false);
