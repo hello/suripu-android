8bb9e9fb71f835016ebb5f701184a8ff0a0a5c55
km <km@sayhello.com>
2015-06-23 16:13:10 -0700

    add analytic event for unsupported device

--- app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingUnsupportedDeviceFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingUnsupportedDeviceFragment.java
@@ -6,16 +6,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.json.JSONObject;
+
 import is.hello.sense.R;
+import is.hello.sense.bluetooth.stacks.BluetoothStack;
 import is.hello.sense.ui.fragments.HardwareFragment;
+import is.hello.sense.util.Analytics;
 
 public class OnboardingUnsupportedDeviceFragment extends HardwareFragment {
+    private BluetoothStack.SupportLevel supportLevel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.supportLevel = hardwarePresenter.getDeviceSupportLevel();
+
+        JSONObject properties = Analytics.createProperties(
+            Analytics.Onboarding.PROP_DEVICE_SUPPORT_LEVEL, supportLevel.toString()
+        );
+        Analytics.trackEvent(Analytics.Onboarding.EVENT_UNSUPPORTED_DEVICE, properties);
+
+        setRetainInstance(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         OnboardingSimpleStepViewBuilder builder = new OnboardingSimpleStepViewBuilder(this, inflater, container);
 
-        switch (hardwarePresenter.getDeviceSupportLevel()) {
+        switch (supportLevel) {
             case UNSUPPORTED: {
                 builder.setHeadingText(R.string.onboarding_title_unsupported_device);
                 builder.setSubheadingText(R.string.onboarding_message_unsupported_device);
