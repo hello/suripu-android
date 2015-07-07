d496b247a78fc85b0dcc6f6f640495764c740f65
km <km@sayhello.com>
2015-07-07 10:51:26 -0700

    support for inputting api host during on-boarding

--- app/src/debug/java/is/hello/sense/debug/EnvironmentActivity.java
+++ app/src/debug/java/is/hello/sense/debug/EnvironmentActivity.java
@@ -3,10 +3,13 @@ package is.hello.sense.debug;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -16,8 +19,12 @@ import is.hello.sense.R;
 import is.hello.sense.api.ApiEndpoint;
 import is.hello.sense.api.DynamicApiEndpoint;
 import is.hello.sense.ui.activities.SenseActivity;
+import is.hello.sense.ui.widget.SenseAlertDialog;
+import is.hello.sense.ui.widget.util.ListViews;
 
 public class EnvironmentActivity extends SenseActivity implements AdapterView.OnItemClickListener {
+    private static final String OTHER_ITEM = "other";
+
     private SharedPreferences preferences;
 
     @Override
@@ -31,6 +38,11 @@ public class EnvironmentActivity extends SenseActivity implements AdapterView.On
         ListView listView = (ListView) findViewById(android.R.id.list);
         listView.setOnItemClickListener(this);
 
+        TextView other = (TextView) getLayoutInflater().inflate(R.layout.item_simple_text, listView, false);
+        other.setBackgroundResource(R.drawable.selectable_dark_bounded);
+        other.setText(R.string.label_environment_other);
+        ListViews.addFooterView(listView, other, OTHER_ITEM, true);
+
         Adapter adapter = new Adapter(this,
             new NamedApiEndpoint("android_dev", "99999secret", "https://dev-api.hello.is", "Dev"),
             new NamedApiEndpoint("8d3c1664-05ae-47e4-bcdb-477489590aa4", "4f771f6f-5c10-4104-bbc6-3333f5b11bf9", "https://canary-api.hello.is", "Canary"),
@@ -41,15 +53,63 @@ public class EnvironmentActivity extends SenseActivity implements AdapterView.On
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        ApiEndpoint endpoint = (ApiEndpoint) parent.getItemAtPosition(position);
+        Object item = parent.getItemAtPosition(position);
+        if (item == OTHER_ITEM) {
+            createCustomApiEndpoint();
+        } else {
+            ApiEndpoint endpoint = (ApiEndpoint) item;
+            selectEndpoint(endpoint);
+            finish();
+        }
+    }
 
+    private void selectEndpoint(@NonNull ApiEndpoint endpoint) {
         preferences.edit()
                 .putString(DynamicApiEndpoint.PREF_CLIENT_ID_OVERRIDE, endpoint.getClientId())
                 .putString(DynamicApiEndpoint.PREF_CLIENT_SECRET_OVERRIDE, endpoint.getClientSecret())
                 .putString(DynamicApiEndpoint.PREF_API_ENDPOINT_OVERRIDE, endpoint.getUrl())
                 .apply();
+    }
 
-        finish();
+    private void createCustomApiEndpoint() {
+        SenseAlertDialog endpointDialog = new SenseAlertDialog(this);
+        endpointDialog.setView(R.layout.dialog_custom_endpoint);
+
+        TextView urlText = (TextView) endpointDialog.findViewById(R.id.dialog_custom_endpoint_url);
+        urlText.setText("https://canary-api.hello.is");
+
+        TextView idText = (TextView) endpointDialog.findViewById(R.id.dialog_custom_endpoint_client_id);
+        idText.setText("8d3c1664-05ae-47e4-bcdb-477489590aa4");
+
+        TextView secretText = (TextView) endpointDialog.findViewById(R.id.dialog_custom_endpoint_client_secret);
+        secretText.setText("4f771f6f-5c10-4104-bbc6-3333f5b11bf9");
+
+        endpointDialog.setOnDismissListener((ignored) -> {
+            InputMethodManager imm = (InputMethodManager) endpointDialog.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+            if (imm.isActive()) {
+                View decorView = endpointDialog.getWindow().getDecorView();
+                imm.hideSoftInputFromWindow(decorView.getWindowToken(), 0);
+            }
+        });
+        endpointDialog.setNegativeButton(android.R.string.cancel, null);
+        endpointDialog.setPositiveButton(android.R.string.ok, (ignored, which) -> {
+            String url = urlText.getText().toString(),
+                    id = idText.getText().toString(),
+                    secret = secretText.getText().toString();
+
+            if (TextUtils.isEmpty(url) ||
+                    TextUtils.isEmpty(id) ||
+                    TextUtils.isEmpty(secret)) {
+                return;
+            }
+
+            ApiEndpoint endpoint = new ApiEndpoint(id, secret, url);
+            selectEndpoint(endpoint);
+
+            // Soft keyboard won't dismiss otherwise.
+            new Handler().post(this::finish);
+        });
+        endpointDialog.show();
     }
 
 
