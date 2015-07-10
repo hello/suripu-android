2be97f454962a168eec062ace53ed64fe49a4846
km <km@sayhello.com>
2015-07-09 17:29:03 -0700

    Update support URLs [ref #98784328]

--- app/src/main/java/is/hello/sense/ui/common/UserSupport.java
+++ app/src/main/java/is/hello/sense/ui/common/UserSupport.java
@@ -21,7 +21,6 @@ import is.hello.sense.util.SessionLogger;
 import is.hello.sense.util.Share;
 
 public class UserSupport {
-    public static final String COMPANY_URL = "https://hello.is";
     public static final String ORDER_URL = "https://order.hello.is";
     public static final String FORGOT_PASSWORD_URL = "https://account.hello.is";
     public static final String SUPPORT_EMAIL = "support@hello.is";
@@ -42,7 +41,7 @@ public class UserSupport {
     public static void showSupport(@NonNull Context from) {
         Analytics.trackEvent(Analytics.TopView.EVENT_HELP, null);
 
-        Uri supportUrl = Uri.fromParts("http", BuildConfig.SUPPORT_AUTHORITY, null);
+        Uri supportUrl = Uri.parse("https://support.hello.is");
         from.startActivity(SupportActivity.getIntent(from, supportUrl));
     }
 
@@ -85,67 +84,36 @@ public class UserSupport {
         from.startActivity(SupportActivity.getIntent(from, issue.getUri()));
     }
 
-    public static void showEnhancedAudio(@NonNull Context from) {
-        Analytics.trackEvent(Analytics.TopView.EVENT_TROUBLESHOOTING_LINK, Analytics.createProperties(Analytics.TopView.PROP_TROUBLESHOOTING_ISSUE, "enhanced-audio"));
-
-        Uri issueUri = new Uri.Builder()
-                .scheme("http")
-                .authority(BuildConfig.SUPPORT_AUTHORITY)
-                .appendPath("app")
-                .appendPath("enhanced-audio")
-                .build();
-        from.startActivity(SupportActivity.getIntent(from, issueUri));
-    }
-
     public static void showReplaceBattery(@NonNull Context from) {
         Analytics.trackEvent(Analytics.TopView.EVENT_TROUBLESHOOTING_LINK, Analytics.createProperties(Analytics.TopView.PROP_TROUBLESHOOTING_ISSUE, "enhanced-audio"));
 
-        Uri issueUri = new Uri.Builder()
-                .scheme("http")
-                .authority(BuildConfig.SUPPORT_AUTHORITY)
-                .appendPath("sleep-pill")
-                .appendPath("battery-change")
-                .build();
+        Uri issueUri = Uri.parse("https://support.hello.is/hc/en-us/articles/204496999");
         from.startActivity(SupportActivity.getIntent(from, issueUri));
     }
 
     public static void showSupportedDevices(@NonNull Context from) {
         Analytics.trackEvent(Analytics.TopView.EVENT_TROUBLESHOOTING_LINK, Analytics.createProperties(Analytics.TopView.PROP_TROUBLESHOOTING_ISSUE, "enhanced-audio"));
 
-        Uri issueUri = new Uri.Builder()
-                .scheme("http")
-                .authority(BuildConfig.SUPPORT_AUTHORITY)
-                .appendPath("app")
-                .appendPath("supported-devices")
-                .build();
+        Uri issueUri = Uri.parse("https://support.hello.is/hc/en-us/articles/205152535");
         from.startActivity(SupportActivity.getIntent(from, issueUri));
     }
 
 
-    private static Uri buildSupportUrl(@NonNull String slug) {
-        return new Uri.Builder()
-                .scheme("http")
-                .authority(BuildConfig.SUPPORT_AUTHORITY)
-                .appendPath("troubleshoot")
-                .appendPath(slug)
-                .build();
-    }
-
     public enum DeviceIssue {
-        UNSTABLE_BLUETOOTH("android-bluetooth-problems"),
-        SENSE_MISSING("sense-not-seen-in-days"),
-        CANNOT_CONNECT_TO_SENSE("cannot-connect-sense-ble"),
-        SENSE_NO_WIFI("sense-cannot-connect-internet"),
-        SLEEP_PILL_MISSING("pill-not-seen-in-days"),
-        PAIRING_2ND_PILL("setting-up-second-sleep-pill");
+        UNSTABLE_BLUETOOTH("https://support.hello.is/hc/en-us/articles/204796429"),
+        SENSE_MISSING("https://support.hello.is/hc/en-us/articles/204797259"),
+        CANNOT_CONNECT_TO_SENSE("https://support.hello.is/hc/en-us/articles/205493075"),
+        SENSE_NO_WIFI("https://support.hello.is/hc/en-us/articles/205493285"),
+        SLEEP_PILL_MISSING("https://support.hello.is/hc/en-us/articles/204797159"),
+        PAIRING_2ND_PILL("https://support.hello.is/hc/en-us/articles/204797289");
 
-        private final String slug;
-        DeviceIssue(@NonNull String slug) {
-            this.slug = slug;
+        private final String url;
+        DeviceIssue(@NonNull String url) {
+            this.url = url;
         }
 
         public Uri getUri() {
-            return buildSupportUrl(slug);
+            return Uri.parse(url);
         }
 
         public String toProperty() {
@@ -155,25 +123,25 @@ public class UserSupport {
 
     public enum OnboardingStep {
         INFO(""),
-        DEMOGRAPHIC_QUESTIONS("demographic-questions"),
-        BLUETOOTH("turning-on-bluetooth"),
-        ENHANCED_AUDIO("enhanced-audio"),
-        SETTING_UP_SENSE("setting-up-sense"),
-        PAIRING_MODE("pairing-mode"),
-        PAIRING_SENSE_BLE("pairing-sense-ble"),
-        WIFI_SCAN("connecting-sense-wifi"),
-        SIGN_INTO_WIFI("connecting-sense-wifi"),
-        PILL_PAIRING("pairing-your-sleep-pill"),
-        PILL_PLACEMENT("attaching-sleep-pill"),
-        ADD_2ND_PILL("setting-up-second-sleep-pill");
+        DEMOGRAPHIC_QUESTIONS("https://support.hello.is/hc/en-us/articles/204796959"),
+        BLUETOOTH("https://support.hello.is/hc/en-us/articles/205493335"),
+        ENHANCED_AUDIO("https://support.hello.is/hc/en-us/articles/204812619"),
+        SETTING_UP_SENSE("https://support.hello.is/hc/en-us/articles/204797299"),
+        PAIRING_MODE("https://support.hello.is/hc/en-us/articles/204797179"),
+        PAIRING_SENSE_BLE("https://support.hello.is/hc/en-us/articles/205493235"),
+        WIFI_SCAN("https://support.hello.is/hc/en-us/articles/205493095"),
+        SIGN_INTO_WIFI("https://support.hello.is/hc/en-us/articles/205493095"),
+        PILL_PAIRING("https://support.hello.is/hc/en-us/articles/204797129"),
+        PILL_PLACEMENT("https://support.hello.is/hc/en-us/articles/205493045"),
+        ADD_2ND_PILL("https://support.hello.is/hc/en-us/articles/204797289");
 
-        private final String slug;
-        OnboardingStep(@NonNull String slug) {
-            this.slug = slug;
+        private final String url;
+        OnboardingStep(@NonNull String url) {
+            this.url = url;
         }
 
         public Uri getUri() {
-            return buildSupportUrl(slug);
+            return Uri.parse(url);
         }
 
         public String toProperty() {
