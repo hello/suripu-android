c85cb6313520bad380b9c4ff3e25bca8c4d0cecd
km <km@sayhello.com>
2015-08-28 14:39:19 -0700

    update unit settings interface [ref #102088836]

--- app/src/main/java/is/hello/sense/ui/fragments/settings/UnitSettingsFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/settings/UnitSettingsFragment.java
@@ -1,6 +1,7 @@
 package is.hello.sense.ui.fragments.settings;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,22 +9,27 @@ import android.os.Looper;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-import android.widget.ProgressBar;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import javax.inject.Inject;
 
 import is.hello.sense.R;
 import is.hello.sense.functional.Functions;
 import is.hello.sense.graph.presenters.PreferencesPresenter;
-import is.hello.sense.ui.adapter.StaticItemAdapter;
 import is.hello.sense.ui.common.InjectionFragment;
 import is.hello.sense.ui.dialogs.ErrorDialogFragment;
+import is.hello.sense.ui.widget.util.Views;
 import is.hello.sense.units.UnitFormatter;
 import is.hello.sense.util.Analytics;
+import rx.functions.Action1;
 
 public class UnitSettingsFragment extends InjectionFragment implements Handler.Callback {
     private static final int REQUEST_CODE_ERROR = 0xE3;
@@ -34,13 +40,13 @@ public class UnitSettingsFragment extends InjectionFragment implements Handler.C
     @Inject PreferencesPresenter preferencesPresenter;
 
     private final Handler handler = new Handler(Looper.getMainLooper(), this);
-    private StaticItemAdapter.CheckItem use24TimeItem;
-    private StaticItemAdapter.CheckItem useCelsius;
-    private StaticItemAdapter.CheckItem useGrams;
-    private StaticItemAdapter.CheckItem useCentimeters;
+    private CheckBox use24TimeToggle;
+    private Spinner temperatureSpinner;
+    private Spinner weightSpinner;
+    private Spinner heightSpinner;
 
-    private ProgressBar loadingIndicator;
-    private ListView listView;
+
+    //region Lifecycle
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,41 +60,34 @@ public class UnitSettingsFragment extends InjectionFragment implements Handler.C
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.list_view_static, container, false);
+        View view = inflater.inflate(R.layout.fragment_unit_settings, container, false);
 
-        this.loadingIndicator = (ProgressBar) view.findViewById(R.id.list_view_static_loading);
+        this.use24TimeToggle = (CheckBox) view.findViewById(R.id.fragment_unit_settings_use_24_time);
+        Views.setSafeOnClickListener(use24TimeToggle, this::updateUse24Time);
 
-        this.listView = (ListView) view.findViewById(android.R.id.list);
+        this.temperatureSpinner = (Spinner) view.findViewById(R.id.fragment_unit_settings_temperature);
+        temperatureSpinner.setAdapter(new SwitchAdapter(getActivity(), new int[]{
+                R.string.setting_option_temperature_us,
+                R.string.setting_option_temperature_metric,
+        }));
+        final TextView temperatureTitle = (TextView) view.findViewById(R.id.fragment_unit_settings_temperature_title);
+        temperatureTitle.setOnClickListener(ignored -> temperatureSpinner.performClick());
 
-        StaticItemAdapter adapter = new StaticItemAdapter(getActivity());
+        this.weightSpinner = (Spinner) view.findViewById(R.id.fragment_unit_settings_weight);
+        weightSpinner.setAdapter(new SwitchAdapter(getActivity(), new int[]{
+                R.string.setting_option_weight_us,
+                R.string.setting_option_weight_metric,
+        }));
+        final TextView weightTitle = (TextView) view.findViewById(R.id.fragment_unit_settings_weight_title);
+        weightTitle.setOnClickListener(ignored -> weightSpinner.performClick());
 
-        boolean defaultIsMetric = UnitFormatter.isDefaultLocaleMetric();
-
-        boolean use24Time = preferencesPresenter.getUse24Time();
-        this.use24TimeItem = adapter.addCheckItem(R.string.setting_title_use_24_time, use24Time, item -> {
-            updatePreference(PreferencesPresenter.USE_24_TIME, item);
-        });
-
-        boolean useCelsius = preferencesPresenter.getBoolean(PreferencesPresenter.USE_CELSIUS,
-                                                             defaultIsMetric);
-        this.useCelsius = adapter.addCheckItem(R.string.setting_title_use_celsius, useCelsius, item -> {
-            updatePreference(PreferencesPresenter.USE_CELSIUS, item);
-        });
-
-        boolean useGrams = preferencesPresenter.getBoolean(PreferencesPresenter.USE_GRAMS,
-                                                           defaultIsMetric);
-        this.useGrams = adapter.addCheckItem(R.string.setting_title_use_grams, useGrams, item -> {
-            updatePreference(PreferencesPresenter.USE_GRAMS, item);
-        });
-
-        boolean useCentimeters = preferencesPresenter.getBoolean(PreferencesPresenter.USE_CENTIMETERS,
-                                                                 defaultIsMetric);
-        this.useCentimeters = adapter.addCheckItem(R.string.setting_title_use_centimeters, useCentimeters, item -> {
-            updatePreference(PreferencesPresenter.USE_CENTIMETERS, item);
-        });
-
-        listView.setOnItemClickListener(adapter);
-        listView.setAdapter(adapter);
+        this.heightSpinner = (Spinner) view.findViewById(R.id.fragment_unit_settings_height);
+        heightSpinner.setAdapter(new SwitchAdapter(getActivity(), new int[]{
+                R.string.setting_option_height_us,
+                R.string.setting_option_height_metric,
+        }));
+        final TextView heightTitle = (TextView) view.findViewById(R.id.fragment_unit_settings_height_title);
+        heightTitle.setOnClickListener(ignored -> heightSpinner.performClick());
 
         return view;
     }
@@ -97,28 +96,27 @@ public class UnitSettingsFragment extends InjectionFragment implements Handler.C
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        showLoading();
         bindAndSubscribe(preferencesPresenter.pullAccountPreferences(),
-                         ignored -> hideLoading(),
+                         Functions.NO_OP,
                          this::pullingPreferencesFailed);
 
 
         bindAndSubscribe(preferencesPresenter.observableUse24Time(),
-                         use24TimeItem::setChecked,
+                         use24TimeToggle::setChecked,
                          Functions.LOG_ERROR);
 
         boolean defaultIsMetric = UnitFormatter.isDefaultLocaleMetric();
         bindAndSubscribe(preferencesPresenter.observableBoolean(PreferencesPresenter.USE_CELSIUS,
                                                                 defaultIsMetric),
-                         useCelsius::setChecked,
+                         createSpinnerValueBinder(temperatureSpinner, PreferencesPresenter.USE_CELSIUS),
                          Functions.LOG_ERROR);
         bindAndSubscribe(preferencesPresenter.observableBoolean(PreferencesPresenter.USE_GRAMS,
                                                                 defaultIsMetric),
-                         useGrams::setChecked,
+                         createSpinnerValueBinder(weightSpinner, PreferencesPresenter.USE_GRAMS),
                          Functions.LOG_ERROR);
         bindAndSubscribe(preferencesPresenter.observableBoolean(PreferencesPresenter.USE_CENTIMETERS,
                                                                 defaultIsMetric),
-                         useCentimeters::setChecked,
+                         createSpinnerValueBinder(heightSpinner, PreferencesPresenter.USE_CENTIMETERS),
                          Functions.LOG_ERROR);
     }
 
@@ -126,10 +124,10 @@ public class UnitSettingsFragment extends InjectionFragment implements Handler.C
     public void onDestroyView() {
         super.onDestroyView();
 
-        this.use24TimeItem = null;
-
-        this.loadingIndicator = null;
-        this.listView = null;
+        this.use24TimeToggle = null;
+        this.temperatureSpinner = null;
+        this.weightSpinner = null;
+        this.heightSpinner = null;
     }
 
     @Override
@@ -151,31 +149,64 @@ public class UnitSettingsFragment extends InjectionFragment implements Handler.C
         }
     }
 
+    //endregion
 
-    private void showLoading() {
-        loadingIndicator.setVisibility(View.VISIBLE);
-        listView.setVisibility(View.GONE);
+
+    //region Spinners
+
+    private Action1<Boolean> createSpinnerValueBinder(@NonNull Spinner spinner, @NonNull String key) {
+        return value -> {
+            spinner.setOnItemSelectedListener(null);
+
+            final int position = value
+                    ? SwitchAdapter.POSITION_TRUE
+                    : SwitchAdapter.POSITION_FALSE;
+            spinner.setSelection(position);
+
+            spinner.post(() -> {
+                spinner.setOnItemSelectedListener(createSpinnerSelectionListener(key));
+            });
+        };
     }
 
-    private void hideLoading() {
-        loadingIndicator.setVisibility(View.GONE);
-        listView.setVisibility(View.VISIBLE);
+    private Spinner.OnItemSelectedListener createSpinnerSelectionListener(@NonNull String key) {
+        return new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case SwitchAdapter.POSITION_TRUE:
+                        updatePreference(key, true);
+                        break;
+                    case SwitchAdapter.POSITION_FALSE:
+                        updatePreference(key, false);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        };
     }
 
+    //endregion
 
-    public void updatePreference(@NonNull String key, @NonNull StaticItemAdapter.CheckItem item) {
-        boolean update = !item.isChecked();
+
+    public void updatePreference(@NonNull String key, boolean newValue) {
         preferencesPresenter.edit()
-                            .putBoolean(key, update)
+                            .putBoolean(key, newValue)
                             .apply();
 
         handler.removeMessages(MSG_PUSH_PREFERENCES);
         handler.sendEmptyMessageDelayed(MSG_PUSH_PREFERENCES, DELAY_PUSH_PREFERENCES);
     }
 
-    public void pullingPreferencesFailed(Throwable e) {
-        loadingIndicator.setVisibility(View.GONE);
+    public void updateUse24Time(@NonNull View ignored) {
+        updatePreference(PreferencesPresenter.USE_24_TIME, use24TimeToggle.isChecked());
+    }
 
+    public void pullingPreferencesFailed(Throwable e) {
         ErrorDialogFragment errorDialogFragment = new ErrorDialogFragment.Builder(e).build();
         errorDialogFragment.setTargetFragment(this, REQUEST_CODE_ERROR);
         errorDialogFragment.showAllowingStateLoss(getFragmentManager(), ErrorDialogFragment.TAG);
@@ -188,5 +219,40 @@ public class UnitSettingsFragment extends InjectionFragment implements Handler.C
             return true;
         }
         return false;
+    }
+
+
+    static class SwitchAdapter extends ArrayAdapter<Boolean> {
+        static final int POSITION_FALSE = 0;
+        static final int POSITION_TRUE = 1;
+
+        private final LayoutInflater inflater;
+        private final @StringRes int[] titles;
+
+        SwitchAdapter(@NonNull Context context,
+                      @NonNull @StringRes int[] titles) {
+            super(context, R.layout.item_simple_text, new Boolean[] { false, true });
+
+            this.inflater = LayoutInflater.from(context);
+            this.titles = titles;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView text = (TextView) convertView;
+            if (text == null) {
+                text = (TextView) inflater.inflate(R.layout.item_unit_spinner, parent, false);
+            }
+
+            final int title = titles[position];
+            text.setText(title);
+
+            return text;
+        }
+
+        @Override
+        public View getDropDownView(int position, View convertView, ViewGroup parent) {
+            return getView(position, convertView, parent);
+        }
     }
 }
