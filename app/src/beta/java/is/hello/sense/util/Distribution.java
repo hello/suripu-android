e26ac67e8d74ba0c226162808a89d11168425328
km <km@sayhello.com>
2015-09-16 11:48:14 -0700

    remove hockey app code from betas [ref #103533796]

--- app/src/beta/java/is/hello/sense/util/Distribution.java
+++ app/src/beta/java/is/hello/sense/util/Distribution.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 
-import net.hockeyapp.android.UpdateManager;
-
 import is.hello.sense.BuildConfig;
 import is.hello.sense.ui.activities.DebugActivity;
 
+@SuppressWarnings("UnusedParameters")
 public class Distribution {
     public static void checkForUpdates(@NonNull Activity activity) {
-        UpdateManager.register(activity, BuildConfig.HOCKEY_API_KEY);
     }
 
     public static void startDebugActivity(@NonNull Activity from) {
