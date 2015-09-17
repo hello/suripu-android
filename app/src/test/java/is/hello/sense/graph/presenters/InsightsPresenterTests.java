5fb1656efb5fa63f88a449dc00f078009bb7f1da
km <km@sayhello.com>
2015-09-17 15:58:53 -0700

    add tests

--- app/src/test/java/is/hello/sense/graph/presenters/InsightsPresenterTests.java
+++ app/src/test/java/is/hello/sense/graph/presenters/InsightsPresenterTests.java
@@ -1,19 +1,48 @@
 package is.hello.sense.graph.presenters;
 
+import android.annotation.SuppressLint;
+
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
+import is.hello.sense.api.model.Insight;
 import is.hello.sense.graph.InjectionTestCase;
 import is.hello.sense.util.Sync;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 public class InsightsPresenterTests extends InjectionTestCase {
     @Inject InsightsPresenter insightsPresenter;
+    @Inject PreferencesPresenter preferences;
 
     @Test
     public void update() throws Exception {
-        Sync.wrapAfter(insightsPresenter::update, insightsPresenter.insights)
-            .assertTrue(insights -> insights.size() == 3);
+        final ArrayList<Insight> insights = Sync.wrapAfter(insightsPresenter::update,
+                                                           insightsPresenter.insights).last();
+        assertThat(insights.size(), is(equalTo(3)));
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    @Test
+    public void updateClearsUnreadStatus() throws Exception {
+        preferences.edit()
+                   .putBoolean(PreferencesPresenter.HAS_UNREAD_INSIGHT_ITEMS, true)
+                   .commit();
+
+        final ArrayList<Insight> insights = Sync.wrapAfter(insightsPresenter::update,
+                                                           insightsPresenter.insights).last();
+        assertThat(insights.size(), is(equalTo(3)));
+
+        // The `doOnCompleted` handler requires a little bit of a wait to fire.
+        Thread.sleep(2);
+
+        assertThat(preferences.getBoolean(PreferencesPresenter.HAS_UNREAD_INSIGHT_ITEMS, false),
+                   is(false));
     }
 
 }
