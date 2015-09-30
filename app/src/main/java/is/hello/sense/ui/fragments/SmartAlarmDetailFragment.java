4cc16d5882cccee5fba42c34062689a763dc161e
km <km@sayhello.com>
2015-09-30 16:57:48 -0700

    whoops

--- app/src/main/java/is/hello/sense/ui/fragments/SmartAlarmDetailFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/SmartAlarmDetailFragment.java
@@ -357,7 +357,6 @@ public class SmartAlarmDetailFragment extends InjectionFragment {
         LoadingDialogFragment.close(getFragmentManager());
 
         final ErrorDialogFragment.Builder errorDialogBuilder = new ErrorDialogFragment.Builder(e, getResources());
-        final ErrorDialogFragment.Builder errorDialogBuilder = new ErrorDialogFragment.Builder(e);
         if (e instanceof SmartAlarmPresenter.DayOverlapError) {
             errorDialogBuilder.withMessage(StringRef.from(R.string.error_smart_alarm_day_overlap));
         } else if (ApiException.statusEquals(e, 400)) {
