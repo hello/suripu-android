a756bee1348591144cd09a660ffb8b8f9c56d1a7
km <km@sayhello.com>
2015-10-08 15:27:25 -0700

    make time zone not completely horrible

--- app/src/main/java/is/hello/sense/ui/fragments/settings/DeviceTimeZoneFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/settings/DeviceTimeZoneFragment.java
@@ -6,7 +6,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -27,7 +26,6 @@ import is.hello.sense.ui.adapter.TimeZoneAdapter;
 import is.hello.sense.ui.common.InjectionFragment;
 import is.hello.sense.ui.dialogs.ErrorDialogFragment;
 import is.hello.sense.ui.dialogs.LoadingDialogFragment;
-import is.hello.sense.ui.widget.util.Styles;
 import is.hello.sense.util.Analytics;
 import is.hello.sense.util.Logger;
 
@@ -37,6 +35,7 @@ public class DeviceTimeZoneFragment extends InjectionFragment implements Adapter
     private ListView listView;
     private ProgressBar activityIndicator;
     private TextView headerDetail;
+
 
     //region Lifecycle
 
@@ -55,28 +54,20 @@ public class DeviceTimeZoneFragment extends InjectionFragment implements Adapter
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.list_view_static, container, false);
+        final View view = inflater.inflate(R.layout.list_view_static, container, false);
 
         this.activityIndicator = (ProgressBar) view.findViewById(R.id.list_view_static_loading);
 
         this.listView = (ListView) view.findViewById(android.R.id.list);
         listView.setOnItemClickListener(this);
 
-        View header = inflater.inflate(R.layout.item_settings_detail, listView, false);
-
-        TextView headerTitle = (TextView) header.findViewById(R.id.item_settings_detail_title);
-        headerTitle.setText(R.string.label_time_zone);
-
-        this.headerDetail = (TextView) header.findViewById(R.id.item_settings_detail_detail);
+        final View header = inflater.inflate(R.layout.sub_fragment_device_time_zone_header, listView, false);
+        this.headerDetail = (TextView) header.findViewById(R.id.item_static_choice_name);
         headerDetail.setText(R.string.missing_data_placeholder);
 
         listView.addHeaderView(header, null, false);
 
-        View headerDivider = Styles.createHorizontalDivider(getActivity(), ViewGroup.LayoutParams.MATCH_PARENT);
-        headerDivider.setLayoutParams(new AbsListView.LayoutParams(headerDivider.getLayoutParams()));
-        listView.addHeaderView(headerDivider, null, false);
-
-        TimeZoneAdapter adapter = new TimeZoneAdapter(getActivity());
+        final TimeZoneAdapter adapter = new TimeZoneAdapter(getActivity());
         listView.setAdapter(adapter);
 
         return view;
@@ -117,22 +108,22 @@ public class DeviceTimeZoneFragment extends InjectionFragment implements Adapter
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String timeZoneId = (String) parent.getItemAtPosition(position);
+        final String timeZoneId = (String) parent.getItemAtPosition(position);
         if (timeZoneId == null) {
             return;
         }
 
-        DateTimeZone timeZone = DateTimeZone.forID(timeZoneId);
-        SenseTimeZone senseTimeZone = SenseTimeZone.fromDateTimeZone(timeZone);
+        final DateTimeZone timeZone = DateTimeZone.forID(timeZoneId);
+        final SenseTimeZone senseTimeZone = SenseTimeZone.fromDateTimeZone(timeZone);
         LoadingDialogFragment.show(getFragmentManager(),
-                null, LoadingDialogFragment.OPAQUE_BACKGROUND);
+                                   null, LoadingDialogFragment.OPAQUE_BACKGROUND);
         bindAndSubscribe(accountPresenter.updateTimeZone(senseTimeZone),
                          ignored -> {
                              Logger.info(getClass().getSimpleName(), "Updated time zone");
 
-                             JSONObject properties = Analytics.createProperties(
-                                 Analytics.TopView.PROP_TIME_ZONE, senseTimeZone.timeZoneId
-                             );
+                             final JSONObject properties =
+                                     Analytics.createProperties(Analytics.TopView.PROP_TIME_ZONE,
+                                                                senseTimeZone.timeZoneId);
                              Analytics.trackEvent(Analytics.TopView.EVENT_TIME_ZONE_CHANGED, properties);
 
                              LoadingDialogFragment.closeWithDoneTransition(getFragmentManager(), null);
@@ -144,10 +135,10 @@ public class DeviceTimeZoneFragment extends InjectionFragment implements Adapter
 
 
     public void bindTimeZone(@NonNull SenseTimeZone senseTimeZone) {
-        DateTimeZone timeZone = senseTimeZone.toDateTimeZone();
-        TimeZone displayTimeZone = timeZone.toTimeZone();
-        boolean inDST = displayTimeZone.inDaylightTime(new Date());
-        String timeZoneName = displayTimeZone.getDisplayName(inDST, TimeZone.LONG);
+        final DateTimeZone timeZone = senseTimeZone.toDateTimeZone();
+        final TimeZone displayTimeZone = timeZone.toTimeZone();
+        final boolean inDST = displayTimeZone.inDaylightTime(new Date());
+        final String timeZoneName = displayTimeZone.getDisplayName(inDST, TimeZone.LONG);
         headerDetail.setText(timeZoneName);
 
         endActivity(true);
