ea9736573988b517c0371252d1772c6a6a77eb3a
km <km@sayhello.com>
2015-11-06 14:53:25 -0800

    more roll over wtf [ref #105704018]

--- app/src/main/java/is/hello/sense/ui/widget/RotaryPickerView.java
+++ app/src/main/java/is/hello/sense/ui/widget/RotaryPickerView.java
@@ -339,14 +339,6 @@ public class RotaryPickerView extends RecyclerView implements View.OnClickListen
 
         smoothScrollBy(0, itemHeight);
         this.value = newValue;
-
-        if (wrapAroundEventsEnabled && onSelectionListener != null
-                && wrapsAround && newValue == rollForwardPosition) {
-            onSelectionListener.onSelectionRolledOver(this, RolloverDirection.FORWARD);
-
-            final View centerView = findChildViewUnder(0, recyclerMidY);
-            this.lastWrapAroundPosition = getChildAdapterPosition(centerView);
-        }
     }
 
     public void decrement() {
@@ -359,13 +351,6 @@ public class RotaryPickerView extends RecyclerView implements View.OnClickListen
 
         smoothScrollBy(0, -itemHeight);
         this.value = newValue;
-        if (wrapAroundEventsEnabled && onSelectionListener != null
-                && wrapsAround && newValue == rollBackwardPosition) {
-            onSelectionListener.onSelectionRolledOver(this, RolloverDirection.FORWARD);
-
-            final View centerView = findChildViewUnder(0, recyclerMidY);
-            this.lastWrapAroundPosition = getChildAdapterPosition(centerView);
-        }
     }
 
     public int getValue() {
