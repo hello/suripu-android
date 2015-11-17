1e67ab2d8b07295395dcab9ac2cbae74e348a056
David Zafrani <d.zafrani21@gmail.com>
2015-11-16 17:09:07 -0800

    Hiding alarm button. [ref #108041108]

--- app/src/main/java/is/hello/sense/ui/activities/HomeActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/HomeActivity.java
@@ -14,6 +14,7 @@ import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
@@ -439,7 +440,6 @@ public class HomeActivity extends ScopedInjectionActivity
 
     @Override
     public void onPageSelected(int position) {
-        showAlarmShortcut();
     }
 
     @Override
