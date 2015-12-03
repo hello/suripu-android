72f72ccec6ce98949a23781f74da4c52b5a5e82f
km <km@sayhello.com>
2015-12-02 16:16:44 -0800

    implement status bar coloring as intended [ref #108662434]

--- app/src/main/java/is/hello/sense/ui/dialogs/InsightInfoDialogFragment.java
+++ app/src/main/java/is/hello/sense/ui/dialogs/InsightInfoDialogFragment.java
@@ -1,28 +1,38 @@
 package is.hello.sense.ui.dialogs;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
 import android.app.Dialog;
 import android.content.DialogInterface;
-import android.os.Build;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
-import android.view.View;
 import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import javax.inject.Inject;
 
+import is.hello.go99.Anime;
+import is.hello.go99.animators.AnimatorTemplate;
 import is.hello.sense.R;
 import is.hello.sense.ui.common.InjectionDialogFragment;
+import is.hello.sense.ui.widget.util.Drawing;
 import is.hello.sense.ui.widget.util.Views;
+import is.hello.sense.ui.widget.util.Windows;
 import is.hello.sense.util.markup.text.MarkupString;
 
-public class InsightInfoDialogFragment extends InjectionDialogFragment {
+public class InsightInfoDialogFragment extends InjectionDialogFragment implements Target {
     public static final String TAG = InsightInfoDialogFragment.class.getSimpleName();
 
     private static final String ARG_TITLE = InsightInfoDialogFragment.class.getName() + ".ARG_TITLE";
@@ -38,6 +48,7 @@ public class InsightInfoDialogFragment extends InjectionDialogFragment {
     private MarkupString info;
 
     private ImageView illustrationImage;
+    private ValueAnimator loadedAnimator;
 
     //region Lifecycle
 
@@ -70,14 +81,8 @@ public class InsightInfoDialogFragment extends InjectionDialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final Dialog dialog = new Dialog(getActivity(), R.style.AppTheme_Dialog_InsightInfo);
+        final Dialog dialog = new Dialog(getActivity(), R.style.AppTheme_Dialog_FullScreen_Insight);
         dialog.setContentView(R.layout.fragment_dialog_insight_info);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            final Window window = dialog.getWindow();
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
-                                                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        }
 
         this.illustrationImage =
                 (ImageView) dialog.findViewById(R.id.fragment_dialog_insight_info_illustration);
@@ -87,10 +92,6 @@ public class InsightInfoDialogFragment extends InjectionDialogFragment {
             illustrationImage.getLayoutParams().height = Math.round(width * 0.5f /* 2:1 */);
             illustrationImage.requestLayout();
         });
-
-        if (!TextUtils.isEmpty(imageUrl)) {
-            picasso.load(imageUrl).into(illustrationImage);
-        }
 
         final TextView titleText =
                 (TextView) dialog.findViewById(R.id.fragment_dialog_insight_info_title);
@@ -110,6 +111,11 @@ public class InsightInfoDialogFragment extends InjectionDialogFragment {
             dismissAllowingStateLoss();
         });
 
+        if (!TextUtils.isEmpty(imageUrl)) {
+            picasso.load(imageUrl)
+                   .into(this);
+        }
+
         return dialog;
     }
 
@@ -117,8 +123,76 @@ public class InsightInfoDialogFragment extends InjectionDialogFragment {
     public void onDismiss(DialogInterface dialog) {
         super.onDismiss(dialog);
 
-        picasso.cancelRequest(illustrationImage);
+        if (loadedAnimator != null) {
+            loadedAnimator.cancel();
+        }
+
+        picasso.cancelRequest(this);
     }
 
     //endregion
+
+
+    private static @ColorInt int getStatusBarColor(@NonNull Bitmap bitmap) {
+        return Drawing.darkenColorBy(bitmap.getPixel(0, 0), 0.2f);
+    }
+
+    @Override
+    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+        if (!isAdded()) {
+            return;
+        }
+
+        if (getDialog() == null) {
+            stateSafeExecutor.execute(() -> {
+                final Window window = getDialog().getWindow();
+                final @ColorInt int newStatusBar = getStatusBarColor(bitmap);
+                Windows.setStatusBarColor(window, newStatusBar);
+                illustrationImage.setImageBitmap(bitmap);
+            });
+
+            return;
+        }
+
+        final Window window = getDialog().getWindow();
+        final BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
+        drawable.setAlpha(0);
+        illustrationImage.setImageDrawable(drawable);
+
+        final @ColorInt int oldStatusBar = Windows.getStatusBarColor(window);
+        final @ColorInt int newStatusBar = getStatusBarColor(bitmap);
+        this.loadedAnimator = AnimatorTemplate.DEFAULT.apply(ValueAnimator.ofFloat(0f, 1f));
+        loadedAnimator.addUpdateListener(a -> {
+            final float fraction = a.getAnimatedFraction();
+            drawable.setAlpha(Math.round(255f * fraction));
+            Windows.setStatusBarColor(window, Anime.interpolateColors(fraction,
+                                                                      oldStatusBar,
+                                                                      newStatusBar));
+        });
+        loadedAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                if (loadedAnimator == animation) {
+                    InsightInfoDialogFragment.this.loadedAnimator = null;
+                }
+            }
+        });
+        loadedAnimator.start();
+    }
+
+    @Override
+    public void onBitmapFailed(Drawable errorDrawable) {
+        if (!isAdded()) {
+            return;
+        }
+
+        final Window window = getDialog().getWindow();
+        final @ColorInt int statusBar = getResources().getColor(R.color.status_bar_illustration);
+        Windows.setStatusBarColor(window, statusBar);
+        illustrationImage.setImageDrawable(errorDrawable);
+    }
+
+    @Override
+    public void onPrepareLoad(Drawable placeHolderDrawable) {
+    }
 }
