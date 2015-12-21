028a0fea28d2a6a261a30ae2cf6977aa24a64e09
km <km@sayhello.com>
2015-12-21 14:34:08 -0800

    remove unnecessary log call

--- app/src/main/java/is/hello/sense/ui/widget/ParallaxImageView.java
+++ app/src/main/java/is/hello/sense/ui/widget/ParallaxImageView.java
@@ -16,7 +16,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
@@ -217,8 +216,6 @@ public class ParallaxImageView extends FrameLayout implements Target {
         drawableFadeIn.start();
 
         if (loadingView != null) {
-            Log.d(getClass().getSimpleName(), hashCode() + " fading out loading view");
-
             animatorFor(loadingView, animatorContext)
                     .fadeOut(INVISIBLE)
                     .start();
