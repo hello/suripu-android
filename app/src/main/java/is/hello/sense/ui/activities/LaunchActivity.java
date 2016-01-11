38a14b0e624c1d5c37df5431e918c9a2f2e33cb1
km <km@sayhello.com>
2016-01-11 15:27:32 -0800

    prevent unwanted log out events on launch

--- app/src/main/java/is/hello/sense/ui/activities/LaunchActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/LaunchActivity.java
@@ -42,16 +42,19 @@ public class LaunchActivity extends InjectionActivity {
 
         if (savedInstanceState == null) {
             localUsageTracker.incrementAsync(LocalUsageTracker.Identifier.APP_LAUNCHED);
-            apiService.getAccount().subscribe(account -> {
-                final com.segment.analytics.Properties properties = new Properties();
-                properties.put(Analytics.Global.GLOBAL_PROP_ACCOUNT_EMAIL, account.getEmail());
-                properties.put(Analytics.Global.GLOBAL_PROP_ACCOUNT_NAME, account.getName());
-                Analytics.trackEvent(Analytics.Global.APP_LAUNCHED, properties);
-            }, e -> {
-                Logger.error(getClass().getSimpleName(), "Could not load user info", e);
+            if (sessionManager.hasSession()) {
+                apiService.getAccount().subscribe(account -> {
+                    final com.segment.analytics.Properties properties = new Properties();
+                    properties.put(Analytics.Global.GLOBAL_PROP_ACCOUNT_EMAIL, account.getEmail());
+                    properties.put(Analytics.Global.GLOBAL_PROP_ACCOUNT_NAME, account.getName());
+                    Analytics.trackEvent(Analytics.Global.APP_LAUNCHED, properties);
+                }, e -> {
+                    Logger.error(getClass().getSimpleName(), "Could not load user info", e);
+                    Analytics.trackEvent(Analytics.Global.APP_LAUNCHED, null);
+                });
+            } else {
                 Analytics.trackEvent(Analytics.Global.APP_LAUNCHED, null);
-            });
-
+            }
         }
 
         if (sessionManager.getSession() != null) {
