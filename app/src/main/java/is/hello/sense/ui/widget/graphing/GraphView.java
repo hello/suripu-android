fae96dcbc03b82b8457bf1c16e3da19b3b5d5d01
David Zafrani <d.zafrani21@gmail.com>
2016-01-12 16:57:20 -0800

    PR fix.

--- app/src/main/java/is/hello/sense/ui/widget/graphing/GraphView.java
+++ app/src/main/java/is/hello/sense/ui/widget/graphing/GraphView.java
@@ -63,7 +63,7 @@ public class GraphView extends View implements GraphAdapter.ChangeObserver {
     private int touchSlop;
     private float startEventX = 0f, startEventY = 0f;
     private boolean trackingTouchEvents = false;
-    int offscreenInset = getResources().getDimensionPixelSize(R.dimen.series_graph_offscreen_inset); // Extra spacing so graph doesn't fall below visible portion of the screen.
+    private int offscreenInset = getResources().getDimensionPixelSize(R.dimen.series_graph_offscreen_inset); // Extra spacing so graph doesn't fall below visible portion of the screen.
 
     private final Drawable.Callback DRAWABLE_CALLBACK = new Drawable.Callback() {
         @Override
