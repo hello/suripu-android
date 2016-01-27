777242658e0dcc76cf26ff87a8df8521d7223878
km <km@sayhello.com>
2016-01-27 10:50:06 -0800

    support for automatically applying insets on backside tabs

--- app/src/main/java/is/hello/sense/ui/fragments/BacksideTabFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/BacksideTabFragment.java
@@ -1,5 +1,11 @@
 package is.hello.sense.ui.fragments;
 
+import android.content.res.Resources;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.View;
+
+import is.hello.sense.R;
 import is.hello.sense.graph.Scope;
 import is.hello.sense.ui.activities.HomeActivity;
 import is.hello.sense.ui.activities.OnboardingActivity;
@@ -43,4 +49,54 @@ public abstract class BacksideTabFragment extends InjectionFragment {
      * updates when its appropriate to do so.
      */
     public abstract void onUpdate();
+
+
+    //region Content Insets
+
+    /**
+     * Calculates the insets that should be applied to fragment's
+     * content views to appear visually correct.
+     * @return  A {@code Rect} containing the insets.
+     */
+    protected Rect getContentInsets() {
+        final Resources resources = getResources();
+        final int topInset = resources.getDimensionPixelSize(R.dimen.action_bar_height);
+        final int bottomInset = resources.getDimensionPixelSize(R.dimen.sliding_layers_open_height);
+        return new Rect(0, topInset, 0, bottomInset);
+    }
+
+    /**
+     * Indicates whether or not the tab wants to have its content automatically inset.
+     * <p>
+     * The insets will be applied after the fragment creates its view.
+     * @return true if the tab wants to have its content inset automatically; false otherwise.
+     */
+    protected boolean automaticallyApplyContentInsets() {
+        return true;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (automaticallyApplyContentInsets()) {
+            final Rect contentInsets = getContentInsets();
+
+            final int newTopPadding = view.getPaddingTop() + contentInsets.top;
+            final int newBottomPadding = view.getPaddingBottom() + contentInsets.bottom;
+            if (view.isPaddingRelative()) {
+                view.setPaddingRelative(view.getPaddingStart(),
+                                        newTopPadding,
+                                        view.getPaddingEnd(),
+                                        newBottomPadding);
+            } else {
+                view.setPadding(view.getPaddingLeft(),
+                                newTopPadding,
+                                view.getPaddingRight(),
+                                newBottomPadding);
+            }
+        }
+    }
+
+    //endregion
 }
