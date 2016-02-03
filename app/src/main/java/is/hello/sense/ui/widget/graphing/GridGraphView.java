cc411644f5cce4a72ce24e75a627a43e53b757a8
km <km@sayhello.com>
2016-02-03 15:44:36 -0800

    preliminary support for recycling rows and cells

--- app/src/main/java/is/hello/sense/ui/widget/graphing/GridGraphView.java
+++ app/src/main/java/is/hello/sense/ui/widget/graphing/GridGraphView.java
@@ -9,6 +9,9 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.widget.LinearLayout;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 import is.hello.sense.R;
 
 public class GridGraphView extends LinearLayout {
@@ -16,7 +19,10 @@ public class GridGraphView extends LinearLayout {
     private final LayoutParams cellLayoutParams;
     private final int interRowPadding;
 
-    private @Nullable Adapter adapter;
+    private final Deque<LinearLayout> rowScrap = new ArrayDeque<>();
+    private final Deque<GridDataPointView> cellScrap = new ArrayDeque<>();
+
+    private Adapter adapter;
 
     public GridGraphView(@NonNull Context context) {
         this(context, null);
@@ -39,42 +45,94 @@ public class GridGraphView extends LinearLayout {
         this.interRowPadding = resources.getDimensionPixelSize(R.dimen.gap_small);
     }
 
+    //region Vending Views
+
     private LinearLayout createRow(@NonNull Context context) {
-        final LinearLayout rowLayout = new LinearLayout(context);
-        rowLayout.setOrientation(HORIZONTAL);
-        rowLayout.setGravity(Gravity.CENTER);
-        return rowLayout;
+        final LinearLayout row = new LinearLayout(context);
+        row.setOrientation(HORIZONTAL);
+        row.setGravity(Gravity.CENTER);
+        row.setLayoutParams(rowLayoutParams);
+        return row;
     }
 
-    private void updateViews() {
-        removeAllViews();
+    private GridDataPointView createCell(@NonNull Context context) {
+        final GridDataPointView cell = new GridDataPointView(context);
+        cell.setLayoutParams(cellLayoutParams);
+        return cell;
+    }
 
-        final Context context = getContext();
-        final int sectionCount = adapter.getSectionCount();
-        final int lastSection = sectionCount - 1;
-        for (int section = 0; section < sectionCount; section++) {
-            final LinearLayout row = createRow(context);
-            final int itemCount = adapter.getSectionItemCount(section);
-            for (int item = 0; item < itemCount; item++) {
-                final GridDataPointView pointView = new GridDataPointView(context);
-                pointView.setValue(adapter.getValueReading(section, item));
-                pointView.setFillColor(adapter.getValueColor(section, item));
-                row.addView(pointView, cellLayoutParams);
-            }
-
-            if (section != lastSection) {
-                row.setPadding(0, 0, 0, interRowPadding);
-            } else {
-                row.setPadding(0, 0, 0, 0);
-            }
-
-            addView(row, rowLayoutParams);
+    private LinearLayout dequeueRow(@NonNull Context context) {
+        final LinearLayout scrap = rowScrap.poll();
+        if (scrap != null) {
+            return scrap;
+        } else {
+            return createRow(context);
         }
     }
 
+    private GridDataPointView dequeueCell(@NonNull Context context) {
+        final GridDataPointView scrap = cellScrap.poll();
+        if (scrap != null) {
+            return scrap;
+        } else {
+            return createCell(context);
+        }
+    }
+
+    private void recycleRow(@NonNull LinearLayout row) {
+        for (int i = 0, count = row.getChildCount(); i < count; i++) {
+            cellScrap.offer((GridDataPointView) row.getChildAt(i));
+        }
+        removeView(row);
+        rowScrap.offer(row);
+    }
+
+    //endregion
+
+    //region Populating
+
+    private void displayDataPoint(GridDataPointView itemView, int row, int item) {
+        itemView.setValue(adapter.getValueReading(row, item));
+        itemView.setFillColor(adapter.getValueColor(row, item));
+    }
+
+    private void populate(int oldRowCount, int newRowCount) {
+        if (newRowCount < oldRowCount) {
+            final int toRemove = oldRowCount - newRowCount;
+            for (int i = 0; i < toRemove; i++) {
+                recycleRow((LinearLayout) getChildAt(0));
+            }
+        }
+
+        final Context context = getContext();
+        final int lastRow = newRowCount - 1;
+        for (int row = 0; row < newRowCount; row++) {
+            final LinearLayout rowView = dequeueRow(context);
+            final int itemCount = adapter.getSectionItemCount(row);
+            for (int item = 0; item < itemCount; item++) {
+                final GridDataPointView itemView = dequeueCell(context);
+                displayDataPoint(itemView, row, item);
+                rowView.addView(itemView, cellLayoutParams);
+            }
+
+            if (row != lastRow) {
+                rowView.setPadding(0, 0, 0, interRowPadding);
+            } else {
+                rowView.setPadding(0, 0, 0, 0);
+            }
+
+            addView(rowView, rowLayoutParams);
+        }
+    }
+
+    //endregion
+
     public void setAdapter(@Nullable Adapter adapter) {
+        final int oldRowCount = this.adapter != null ? this.adapter.getSectionCount() : 0;
+        final int newRowCount = adapter != null ? adapter.getSectionCount() : 0;
+
         this.adapter = adapter;
-        updateViews();
+        populate(oldRowCount, newRowCount);
     }
 
     public interface Adapter {
