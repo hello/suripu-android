6a6c77dd6360321cc190e6a5f04e453a910dcc87
km <km@sayhello.com>
2016-02-05 14:06:27 -0800

    coordinate parent layout transitions + refactoring + add logging

--- app/src/main/java/is/hello/sense/ui/widget/graphing/GridGraphView.java
+++ app/src/main/java/is/hello/sense/ui/widget/graphing/GridGraphView.java
@@ -12,23 +12,26 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AnticipateInterpolator;
 import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.OvershootInterpolator;
 import android.widget.LinearLayout;
 
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 import is.hello.go99.Anime;
 import is.hello.go99.animators.MultiAnimator;
 import is.hello.sense.R;
+import is.hello.sense.util.Logger;
 
 public class GridGraphView extends LinearLayout {
-    private static final int MSG_POPULATE = 0x606;
+    //region Constants
 
-    private static final float TENSION = 0.5f;
+    private static final boolean DEBUG = true;
+
+    private static final float TENSION = 0.9f;
 
     private static final float SCALE_MIN = 0.9f;
     private static final float SCALE_MAX = 1.0f;
@@ -37,16 +40,37 @@ public class GridGraphView extends LinearLayout {
 
     private static final long ROW_STAGGER = 50L;
 
+    //endregion
+
+    //region Render Attributes
+
     private final LayoutParams rowLayoutParams;
     private final LayoutParams cellLayoutParams;
     private final int interRowPadding;
 
-    private final Deque<LinearLayout> rowScrap = new ArrayDeque<>();
-    private final Deque<GridGraphCellView> cellScrap = new ArrayDeque<>();
-    private final List<LinearLayout> rows = new ArrayList<>();
+    //endregion
 
+    //region Contents
+
+    private final List<LinearLayout> rowScrap = new ArrayList<>();
+    private final List<GridGraphCellView> cellScrap = new ArrayList<>();
+
+    private final List<LinearLayout> rowViews = new ArrayList<>();
     private final Runnable populateCallback = this::populate;
+
+    //endregion
+
+    //region Attribute Backing
+
+    private @Nullable LayoutTransition parentLayoutTransition;
+    private int maxRowScrapCount = 3;
+    private int maxCellScrapCount = 7;
     private Adapter adapter;
+
+    //endregion
+
+
+    //region Lifecycle
 
     public GridGraphView(@NonNull Context context) {
         this(context, null);
@@ -62,21 +86,21 @@ public class GridGraphView extends LinearLayout {
         final Resources resources = getResources();
 
         setOrientation(VERTICAL);
-        setShowDividers(SHOW_DIVIDER_MIDDLE);
 
         final LayoutTransition layoutTransition = new LayoutTransition();
 
         layoutTransition.setAnimator(LayoutTransition.APPEARING, createAppearingAnimator());
         layoutTransition.setAnimator(LayoutTransition.DISAPPEARING, createDisappearingAnimator());
 
-        final AnticipateInterpolator anticipate = new AnticipateInterpolator(TENSION);
-        final AnticipateOvershootInterpolator anticipateOvershoot =
+        final AnticipateOvershootInterpolator rowInterpolator =
                 new AnticipateOvershootInterpolator(TENSION);
-        layoutTransition.setInterpolator(LayoutTransition.APPEARING, anticipate);
-        layoutTransition.setInterpolator(LayoutTransition.DISAPPEARING, anticipateOvershoot);
+        layoutTransition.setInterpolator(LayoutTransition.APPEARING, rowInterpolator);
+        layoutTransition.setInterpolator(LayoutTransition.DISAPPEARING, rowInterpolator);
 
-        layoutTransition.setInterpolator(LayoutTransition.CHANGE_APPEARING, anticipateOvershoot);
-        layoutTransition.setInterpolator(LayoutTransition.CHANGE_DISAPPEARING, anticipate);
+        layoutTransition.setInterpolator(LayoutTransition.CHANGE_APPEARING,
+                                         new OvershootInterpolator(TENSION));
+        layoutTransition.setInterpolator(LayoutTransition.CHANGE_DISAPPEARING,
+                                         new AnticipateInterpolator(TENSION));
 
         layoutTransition.setDuration(Anime.DURATION_SLOW);
 
@@ -91,11 +115,30 @@ public class GridGraphView extends LinearLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
 
-        for (final LinearLayout row : rows) {
-            row.setPivotX(row.getMeasuredWidth() / 2f);
-            row.setPivotY(0f);
+        for (final LinearLayout rowView : rowViews) {
+            rowView.setPivotX(rowView.getMeasuredWidth() / 2f);
+            rowView.setPivotY(0f);
         }
     }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        // When a GridGraphView is contained in an outer ViewGroup which is animated
+        // independently of its own LayoutAnimator, both its parent and itself must
+        // disable child clipping for the animated layout changes to render correctly.
+        final boolean clipChildren = (parentLayoutTransition == null);
+        final ViewGroup parent = (ViewGroup) getParent();
+
+        setClipChildren(clipChildren);
+        parent.setClipChildren(clipChildren);
+
+        setClipToPadding(clipChildren);
+        parent.setClipToPadding(clipChildren);
+    }
+
+    //endregion
 
 
     //region Animations
@@ -145,12 +188,45 @@ public class GridGraphView extends LinearLayout {
                             .scale(SCALE_MIN);
     }
 
+    public void bindParentLayoutTransition(@NonNull LayoutTransition parentLayoutTransition) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(),
+                         "bindParentLayoutTransition(" + parentLayoutTransition + ")");
+        }
+
+        final LayoutTransition myLayoutTransition = getLayoutTransition();
+        myLayoutTransition.setAnimateParentHierarchy(false);
+
+        this.parentLayoutTransition = parentLayoutTransition;
+
+        parentLayoutTransition.enableTransitionType(LayoutTransition.CHANGING);
+        parentLayoutTransition.setInterpolator(LayoutTransition.CHANGE_APPEARING,
+                                               myLayoutTransition.getInterpolator(LayoutTransition.CHANGE_APPEARING));
+        parentLayoutTransition.setInterpolator(LayoutTransition.CHANGE_DISAPPEARING,
+                                               myLayoutTransition.getInterpolator(LayoutTransition.CHANGE_DISAPPEARING));
+
+        parentLayoutTransition.setDuration(parentLayoutTransition.getDuration(LayoutTransition.CHANGING));
+
+        // See #onAttachedToWindow() for explanation on these lines
+        setClipChildren(false);
+        setClipToPadding(false);
+        if (getParent() != null) {
+            final ViewGroup parent = (ViewGroup) getParent();
+            parent.setClipChildren(false);
+            parent.setClipToPadding(false);
+        }
+    }
+
     //endregion
 
 
     //region Vending Views
 
     private LinearLayout createRowView(@NonNull Context context) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "createRowView()");
+        }
+
         final LinearLayout row = new LinearLayout(context);
         row.setOrientation(HORIZONTAL);
         row.setGravity(Gravity.CENTER);
@@ -159,90 +235,140 @@ public class GridGraphView extends LinearLayout {
     }
 
     private GridGraphCellView createCellView(@NonNull Context context) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "createCellView()");
+        }
+
         final GridGraphCellView cell = new GridGraphCellView(context);
         cell.setLayoutParams(cellLayoutParams);
         return cell;
     }
 
-    private LinearLayout dequeueRowView(@NonNull Context context) {
-        LinearLayout row = rowScrap.poll();
-        if (row == null) {
-            row = createRowView(context);
+    private LinearLayout dequeueRowView() {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "dequeueRowView()");
         }
-        return row;
+
+        if (rowScrap.isEmpty()) {
+            return createRowView(getContext());
+        } else {
+            final LinearLayout rowView = rowScrap.get(0);
+            rowScrap.remove(0);
+            return rowView;
+        }
     }
 
-    private GridGraphCellView dequeueCellView(@NonNull Context context) {
-        GridGraphCellView cell = cellScrap.poll();
-        if (cell == null) {
-            cell = createCellView(context);
+    private GridGraphCellView dequeueCellView() {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "dequeueCellView()");
         }
-        return cell;
+
+        if (cellScrap.isEmpty()) {
+            return createCellView(getContext());
+        } else {
+            final GridGraphCellView cellView = cellScrap.get(0);
+            cellScrap.remove(0);
+            return cellView;
+        }
     }
 
     private void recycleUnusedRowCells(@NonNull LinearLayout rowView,
                                        int targetCount) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "recycleUnusedRowCells(" + rowView +
+                    ", " + targetCount + ")");
+        }
+
         while (rowView.getChildCount() > targetCount) {
             final int lastChildIndex = rowView.getChildCount() - 1;
             final GridGraphCellView cellView = (GridGraphCellView) rowView.getChildAt(lastChildIndex);
-            cellScrap.offer(cellView);
+            if (cellScrap.size() < maxCellScrapCount) {
+                cellScrap.add(cellView);
+            }
             rowView.removeViewAt(lastChildIndex);
         }
     }
 
-    private void recycleRow(@NonNull LinearLayout row) {
-        removeView(row);
-        rows.remove(row);
-        rowScrap.offer(row);
+    private void recycleRow(@NonNull LinearLayout rowView) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "recycleRow(" + rowView + ")");
+        }
+
+        removeView(rowView);
+        rowViews.remove(rowView);
+
+        if (rowScrap.size() < maxRowScrapCount) {
+            rowScrap.add(rowView);
+        }
     }
 
     //endregion
 
+
     //region Populating
 
-    private void displayDataPoint(GridGraphCellView itemView, int row, int item) {
-        itemView.setValue(adapter.getCellReading(row, item));
-        itemView.setFillColor(adapter.getCellColor(row, item));
-        itemView.setBorder(adapter.getCellBorder(row, item));
+    private void displayDataPoint(GridGraphCellView itemView, int row, int cell) {
+        itemView.setValue(adapter.getCellReading(row, cell));
+        itemView.setFillColor(adapter.getCellColor(row, cell));
+        itemView.setBorder(adapter.getCellBorder(row, cell));
     }
 
-    private void populate() {
-        final Context context = getContext();
-        final int oldRowCount = rows.size();
-        final int newRowCount = adapter != null ? adapter.getRowCount() : 0;
-        if (newRowCount < oldRowCount) {
-            final int toRemove = (oldRowCount - newRowCount);
-
-            getLayoutTransition().setStartDelay(LayoutTransition.CHANGE_DISAPPEARING,
-                                                (ROW_STAGGER * toRemove) / 2L);
-
-            for (int i = 0; i < toRemove; i++) {
-                final LinearLayout rowView = rows.get(0);
-                setAnimationIndex(rowView, toRemove - i);
-                recycleRow(rowView);
-            }
-        } else if (newRowCount > oldRowCount) {
-            final int toAdd = (newRowCount - oldRowCount);
-
-            getLayoutTransition().setStartDelay(LayoutTransition.CHANGE_APPEARING,
-                                                (ROW_STAGGER * toAdd) / 4L);
-
-            for (int i = 0; i < toAdd; i++) {
-                final LinearLayout rowView = dequeueRowView(context);
-                setAnimationIndex(rowView, toAdd - i);
-                rows.add(0, rowView);
-                addView(rowView, 0);
-            }
+    private void shrinkRowCount(int delta) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "shrinkRowCount(" + delta + ")");
         }
 
-        final int lastRow = newRowCount - 1;
-        for (int row = 0; row < newRowCount; row++) {
-            final LinearLayout rowView = rows.get(row);
+        final long startDelay = (ROW_STAGGER * delta) / 2L;
+        getLayoutTransition().setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, startDelay);
+        if (parentLayoutTransition != null) {
+            parentLayoutTransition.setInterpolator(LayoutTransition.CHANGING,
+                                                   getLayoutTransition().getInterpolator(LayoutTransition.CHANGE_DISAPPEARING));
+            parentLayoutTransition.setStartDelay(LayoutTransition.CHANGING, startDelay);
+            parentLayoutTransition.setStartDelay(LayoutTransition.CHANGE_DISAPPEARING, startDelay);
+        }
+
+        for (int i = 0; i < delta; i++) {
+            final LinearLayout rowView = rowViews.get(0);
+            setAnimationIndex(rowView, delta - i);
+            recycleRow(rowView);
+        }
+    }
+
+    private void growRowCount(int delta) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "growRowCount(" + delta + ")");
+        }
+
+        final long startDelay = (ROW_STAGGER * delta) / 4L;
+        getLayoutTransition().setStartDelay(LayoutTransition.CHANGE_APPEARING, startDelay);
+        if (parentLayoutTransition != null) {
+            parentLayoutTransition.setInterpolator(LayoutTransition.CHANGING,
+                                                   getLayoutTransition().getInterpolator(LayoutTransition.CHANGE_APPEARING));
+            parentLayoutTransition.setStartDelay(LayoutTransition.CHANGING, startDelay);
+            parentLayoutTransition.setStartDelay(LayoutTransition.CHANGE_APPEARING, startDelay);
+        }
+
+        for (int i = 0; i < delta; i++) {
+            final LinearLayout rowView = dequeueRowView();
+            setAnimationIndex(rowView, delta - i);
+            rowViews.add(0, rowView);
+            addView(rowView, 0);
+        }
+    }
+
+    private void populateRows(int rowCount) {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "populateRows(" + rowCount + ")");
+        }
+
+        final int lastRow = rowCount - 1;
+        for (int row = 0; row < rowCount; row++) {
+            final LinearLayout rowView = rowViews.get(row);
             final int cellCount = adapter.getRowCellCount(row);
             for (int cell = 0; cell < cellCount; cell++) {
                 GridGraphCellView cellView = (GridGraphCellView) rowView.getChildAt(cell);
                 if (cellView == null) {
-                    cellView = dequeueCellView(context);
+                    cellView = dequeueCellView();
                     rowView.addView(cellView);
                 }
 
@@ -259,12 +385,44 @@ public class GridGraphView extends LinearLayout {
         }
     }
 
-    private void postPopulate() {
+    private void populate() {
+        if (DEBUG) {
+            Logger.debug(getClass().getSimpleName(), "populate()");
+        }
+
+        final int oldCount = rowViews.size();
+        final int newCount = adapter != null ? adapter.getRowCount() : 0;
+        if (newCount < oldCount) {
+            shrinkRowCount(oldCount - newCount);
+        } else if (newCount > oldCount) {
+            growRowCount(newCount - oldCount);
+        }
+        populateRows(newCount);
+    }
+
+    private void requestPopulate() {
         removeCallbacks(populateCallback);
         post(populateCallback);
     }
 
     //endregion
+
+
+    //region Attributes
+
+    public void setMaxRowScrapCount(int maxRowScrapCount) {
+        this.maxRowScrapCount = maxRowScrapCount;
+        while (rowScrap.size() > maxRowScrapCount) {
+            rowScrap.remove(0);
+        }
+    }
+
+    public void setMaxCellScrapCount(int maxCellScrapCount) {
+        this.maxCellScrapCount = maxCellScrapCount;
+        while (cellScrap.size() > maxCellScrapCount) {
+            cellScrap.remove(0);
+        }
+    }
 
     public void setAdapter(@Nullable Adapter adapter) {
         if (this.adapter != null) {
@@ -277,13 +435,16 @@ public class GridGraphView extends LinearLayout {
             adapter.registerObserver(ADAPTER_OBSERVER);
         }
 
-        postPopulate();
+        requestPopulate();
     }
+
+    //endregion
+
 
     private final DataSetObserver ADAPTER_OBSERVER = new DataSetObserver() {
         @Override
         public void onChanged() {
-            postPopulate();
+            requestPopulate();
         }
 
         @Override
@@ -295,11 +456,11 @@ public class GridGraphView extends LinearLayout {
     public abstract static class Adapter {
         private final DataSetObservable observable = new DataSetObservable();
 
-        public void registerObserver(DataSetObserver observer) {
+        public void registerObserver(@NonNull DataSetObserver observer) {
             observable.registerObserver(observer);
         }
 
-        public void unregisterObserver(DataSetObserver observer) {
+        public void unregisterObserver(@NonNull DataSetObserver observer) {
             observable.unregisterObserver(observer);
         }
 
