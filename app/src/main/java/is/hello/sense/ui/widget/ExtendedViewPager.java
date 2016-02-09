bc1435962fddb545fcb401c427b081de7ab709a2
km <km@sayhello.com>
2016-02-09 14:58:01 -0800

    another attempt at preventing ViewPager IndexOutOfBoundsExceptions [ref #113011517]

--- app/src/main/java/is/hello/sense/ui/widget/ExtendedViewPager.java
+++ app/src/main/java/is/hello/sense/ui/widget/ExtendedViewPager.java
@@ -7,6 +7,10 @@ import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import is.hello.sense.functional.Lists;
 import is.hello.sense.util.Logger;
 
 /**
@@ -17,6 +21,7 @@ import is.hello.sense.util.Logger;
  * </ol>
  */
 public class ExtendedViewPager extends ViewPager {
+    private @Nullable Field mItemsField;
     private boolean scrollingEnabled = true;
 
     public ExtendedViewPager(@NonNull Context context) {
@@ -25,13 +30,44 @@ public class ExtendedViewPager extends ViewPager {
 
     public ExtendedViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        // See #mItemsIsEmpty()
+        try {
+            this.mItemsField = ViewPager.class.getDeclaredField("mItems");
+            mItemsField.setAccessible(true);
+        } catch (NoSuchFieldException e) {
+            Logger.error(getClass().getSimpleName(), "Could not get `mItems` field", e);
+        }
+    }
+
+    /**
+     * There appears to be a race condition inside of {@code ViewPager} where its adapter can
+     * have items, and before the {@code ViewPager} is able to populate its internal items list,
+     * the user gets in some touch events. This will result in {@code IndexOutOfBoundsException}s.
+     */
+    private boolean mItemsIsEmpty() {
+        if (mItemsField != null) {
+            try {
+                final ArrayList<?> mItems = (ArrayList<?>) mItemsField.get(this);
+                return Lists.isEmpty(mItems);
+            } catch (IllegalAccessException e) {
+                Logger.error(getClass().getSimpleName(), "Could not access `mItems` field", e);
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
 
     @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_MOVE && mItemsIsEmpty()) {
+            return false;
+        }
+
         try {
-            return scrollingEnabled && super.onInterceptTouchEvent(ev);
+            return scrollingEnabled && super.onInterceptTouchEvent(event);
         } catch (IllegalArgumentException e) {
             Logger.warn(getClass().getSimpleName(), "Swallowing illegal argument exception", e);
             return false;
@@ -40,6 +76,10 @@ public class ExtendedViewPager extends ViewPager {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_MOVE && mItemsIsEmpty()) {
+            return false;
+        }
+
         try {
             return scrollingEnabled && super.onTouchEvent(event);
         } catch (IllegalArgumentException e) {
