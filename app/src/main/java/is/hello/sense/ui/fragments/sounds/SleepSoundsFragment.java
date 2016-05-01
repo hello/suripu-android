965fd41abfb63625ce109172446b8d1623811f39
David Zafrani <d.zafrani21@gmail.com>
2016-05-01 00:21:38 -0700

    Switched to spinner button

--- app/src/main/java/is/hello/sense/ui/fragments/sounds/SleepSoundsFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/sounds/SleepSoundsFragment.java
@@ -42,6 +42,7 @@ import is.hello.sense.ui.common.SubFragment;
 import is.hello.sense.ui.dialogs.ErrorDialogFragment;
 import is.hello.sense.ui.handholding.WelcomeDialogFragment;
 import is.hello.sense.ui.recycler.InsetItemDecoration;
+import is.hello.sense.ui.widget.SpinnerImageView;
 import is.hello.sense.util.Analytics;
 import is.hello.sense.util.Constants;
 import rx.Observable;
@@ -49,8 +50,6 @@ import rx.Observable;
 import static is.hello.sense.ui.adapter.SleepSoundsAdapter.*;
 
 public class SleepSoundsFragment extends SubFragment implements InteractionListener, Retry {
-    private final static int deltaRotation = 5; // degrees
-    private final static int spinnerInterval = 1; // ms
     private final static int pollingInterval = 500; // ms
     private final static int initialBackOff = 0; // ms
     private final static int backOffIncrements = 1000; // ms
@@ -68,7 +67,7 @@ public class SleepSoundsFragment extends SubFragment implements InteractionListe
     SleepSoundsPresenter sleepSoundsPresenter;
 
 
-    private ImageButton playButton;
+    private SpinnerImageView playButton;
     private FrameLayout buttonLayout;
     private ProgressBar progressBar;
     private RecyclerView recyclerView;
@@ -90,18 +89,6 @@ public class SleepSoundsFragment extends SubFragment implements InteractionListe
         STOP,
         NONE
     }
-
-    // todo create a custom component that has this logic
-    final Runnable spinningRunnable = new Runnable() {
-        @Override
-        public void run() {
-            if (playButton != null) {
-                playButton.setRotation(playButton.getRotation() + deltaRotation);
-                playButton.postDelayed(this, spinnerInterval);
-            }
-        }
-    };
-
 
     final View.OnClickListener playClickListener = new View.OnClickListener() {
         @Override
@@ -183,7 +170,7 @@ public class SleepSoundsFragment extends SubFragment implements InteractionListe
     public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_sleep_sounds, container, false);
         progressBar = (ProgressBar) view.findViewById(R.id.fragment_sleep_sounds_progressbar);
-        playButton = (ImageButton) view.findViewById(R.id.fragment_sleep_sounds_playbutton);
+        playButton = (SpinnerImageView) view.findViewById(R.id.fragment_sleep_sounds_playbutton);
         buttonLayout = (FrameLayout) view.findViewById(R.id.fragment_sleep_sounds_buttonLayout);
         preferences = getActivity().getSharedPreferences(Constants.SLEEP_SOUNDS_PREFS, 0);
         this.recyclerView = (RecyclerView) view.findViewById(R.id.fragment_sleep_sounds_recycler);
@@ -366,9 +353,9 @@ public class SleepSoundsFragment extends SubFragment implements InteractionListe
         playButton.setOnClickListener(listener);
         playButton.setEnabled(enabled);
         if (enabled) {
-            playButton.removeCallbacks(spinningRunnable);
+            playButton.stopSpinning();
         } else {
-            playButton.post(spinningRunnable);
+            playButton.startSpinning();
         }
     }
 
