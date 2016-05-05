49ddd35c4bb33913fe1511ee5d5d4993faa657d5
David Zafrani <d.zafrani21@gmail.com>
2016-05-05 13:22:01 -0700

    clean up

--- app/src/main/java/is/hello/sense/ui/activities/ListActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/ListActivity.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
