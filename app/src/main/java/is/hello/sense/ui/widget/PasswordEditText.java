6aeeb549ffb3b50d285d5bd7ac5b0248476d854c
Simon Chen <simon.c312@gmail.com>
2016-05-18 11:49:58 -0700

    moved the input types and icons as finals and made the class implement onTouchListener to avoid extra objection creation.

--- app/src/main/java/is/hello/sense/ui/widget/PasswordEditText.java
+++ app/src/main/java/is/hello/sense/ui/widget/PasswordEditText.java
@@ -9,8 +9,12 @@ import android.widget.EditText;
 
 import is.hello.sense.R;
 
-public class PasswordEditText extends EditText {
+public class PasswordEditText extends EditText implements View.OnTouchListener{
     private boolean isPasswordMasked = true;
+    private final int HIDDEN_INPUT_TYPE = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS | InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+    private final int VISIBLE_INPUT_TYPE = InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS;
+    private final int HIDDEN_ICON = R.drawable.secreteye;
+    private final int VISIBLE_ICON = R.drawable.secreteyehighlighted;
 
     public PasswordEditText(Context context) {
         super(context);
@@ -27,31 +31,41 @@ public class PasswordEditText extends EditText {
         init();
     }
 
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (event.getAction() != MotionEvent.ACTION_DOWN) {
+            return false;
+        }
+        else if (event.getRawX() > getWidth() - getCompoundDrawables()[2].getIntrinsicWidth()) {
+                togglePasswordShowing();
+                return true;
+        } else{
+            return false;
+        }
+    }
+
     private void init() {
-        setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.secreteye, 0);
-        setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    if (event.getRawX() > getWidth() - getCompoundDrawables()[2].getIntrinsicWidth()) {
-                        togglePasswordShowing();
-                        return true;
-                    }
-                }
-                return false;
-            }
-        });
+        setInputType(isPasswordMasked);
+        setDrawableIcon(isPasswordMasked);
+        setOnTouchListener(this);
     }
 
     private void togglePasswordShowing() {
-        if (isPasswordMasked) {
-            setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS);
-            setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.secreteyehighlighted, 0);
-        } else {
-            setInputType(InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS | InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-            setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, R.drawable.secreteye, 0);
-        }
-        setSelection(getText().length());
         isPasswordMasked = !isPasswordMasked;
+        setInputType(isPasswordMasked);
+        setDrawableIcon(isPasswordMasked);
+        setSelection(getText().length());
+    }
+
+    private void setInputType(boolean isPasswordMasked){
+        if (isPasswordMasked) {
+            setInputType(HIDDEN_INPUT_TYPE);
+        } else {
+            setInputType(VISIBLE_INPUT_TYPE);
+        }
+    }
+    private void setDrawableIcon(boolean isPasswordMasked){
+        final int drawableIcon = isPasswordMasked ? HIDDEN_ICON : VISIBLE_ICON;
+        setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, drawableIcon, 0);
     }
 }
