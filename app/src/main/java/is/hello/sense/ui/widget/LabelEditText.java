985472b9f8eb4e24518ce741a60446c1caa8dffa
David Zafrani <d.zafrani21@gmail.com>
2016-05-27 14:25:53 -0700

    Using new component in onboarding

--- app/src/main/java/is/hello/sense/ui/widget/LabelEditText.java
+++ app/src/main/java/is/hello/sense/ui/widget/LabelEditText.java
@@ -5,7 +5,7 @@ import android.content.res.TypedArray;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
-import android.support.annotation.StyleRes;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.EditText;
@@ -13,6 +13,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import is.hello.sense.R;
+import is.hello.sense.ui.fragments.onboarding.RegisterFragment;
 import is.hello.sense.ui.widget.util.Styles;
 
 public class LabelEditText extends RelativeLayout {
@@ -110,5 +111,13 @@ public class LabelEditText extends RelativeLayout {
         }
         Styles.setTextAppearance(label, R.style.Label_Error);
         input.setBackgroundResource(R.drawable.edit_text_background_error);
+    }
+
+    public void addTextChangedListener(TextWatcher tw) {
+        input.addTextChangedListener(tw);
+    }
+
+    public void removeTextChangedListener(TextWatcher tw) {
+        input.removeTextChangedListener(tw);
     }
 }
