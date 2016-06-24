950c84953c3c9164ac9df9188063e00ae470c2e0
David Zafrani <d.zafrani21@gmail.com>
2016-06-24 12:51:15 -0700

    updated build tools

--- circle_dep.sh
+++ circle_dep.sh
@@ -6,7 +6,7 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  echo y | android update sdk -u -a -t build-tools-20.0.0 &&
+  echo y | android update sdk -u -a -t build-tools-24.0.0 &&
   echo y | android update sdk -u -a -t android-24 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
