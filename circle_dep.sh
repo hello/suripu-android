283471fa840bc641750df36743a6cde6b46bd0ed
David Zafrani <d.zafrani21@gmail.com>
2016-06-24 12:40:10 -0700

    removed copy

--- circle_dep.sh
+++ circle_dep.sh
@@ -6,7 +6,6 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  cp -r /user/local/android-sdk-linux $ANDROID_HOME &&
   echo y | android update sdk -u -a -t build-tools-20.0.0 &&
   echo y | android update sdk -u -a -t android-19 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
