7838a981b6c273f40ceaee8b0d604f3fc14ee923
David Zafrani <d.zafrani21@gmail.com>
2016-06-24 13:21:39 -0700

    changed environment

--- circle_dep.sh
+++ circle_dep.sh
@@ -3,12 +3,13 @@
 # Fix the CircleCI path
 export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-  echo y | android update sdk -u -a -t build-tools-24.0.0 &&
+  cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
   echo y | android update sdk -u -a -t android-24 &&
+  echo y | android update sdk -u -a -t platform-tools &&
+  echo y | android update sdk -u -a -t build-tools-24.0.0 &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
   touch $DEPS
