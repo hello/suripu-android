9d1b214f51329de187dd3245b4c587de55fdb62c
David Zafrani <d.zafrani21@gmail.com>
2016-07-01 11:11:59 -0700

    Clean up

--- app/src/main/java/is/hello/sense/ui/adapter/InsightsAdapter.java
+++ app/src/main/java/is/hello/sense/ui/adapter/InsightsAdapter.java
@@ -5,7 +5,6 @@ import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.VisibleForTesting;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
