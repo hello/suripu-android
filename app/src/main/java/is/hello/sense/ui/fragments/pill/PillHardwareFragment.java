27360dd0c42c45f7cb3fbc38d744957e7724e4f9
David Zafrani <d.zafrani21@gmail.com>
2016-07-20 12:58:07 -0400

    Handling network problems correctly

--- app/src/main/java/is/hello/sense/ui/fragments/pill/PillHardwareFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/pill/PillHardwareFragment.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 
 import is.hello.commonsense.util.StringRef;
 import is.hello.sense.R;
+import is.hello.sense.api.model.ApiException;
 import is.hello.sense.bluetooth.exceptions.PillNotFoundException;
 import is.hello.sense.bluetooth.exceptions.RssiException;
 import is.hello.sense.graph.presenters.DevicesPresenter;
@@ -38,14 +39,14 @@ public abstract class PillHardwareFragment extends InjectionFragment {
         return new BatteryUtil.Operation(0.20, false);
     }
 
-    public static BatteryUtil.Operation pillUpdateOperationWithCharge(){
+    public static BatteryUtil.Operation pillUpdateOperationWithCharge() {
         return new BatteryUtil.Operation(0, true);
     }
 
     @Override
-    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull  final int[] grantResults) {
+    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(locationPermission.isGrantedFromResult(requestCode, permissions, grantResults)){
+        if (locationPermission.isGrantedFromResult(requestCode, permissions, grantResults)) {
             onLocationPermissionGranted(true);
         } else {
             locationPermission.showEnableInstructionsDialog();
@@ -56,7 +57,7 @@ public abstract class PillHardwareFragment extends InjectionFragment {
         locationPermission.requestPermissionWithDialog();
     }
 
-    protected boolean isLocationPermissionGranted(){
+    protected boolean isLocationPermissionGranted() {
         return locationPermission.isGranted();
     }
 
@@ -65,11 +66,11 @@ public abstract class PillHardwareFragment extends InjectionFragment {
     protected void showBlockingActivity(@StringRes final int titleRes) {
         if (loadingDialogFragment == null) {
             stateSafeExecutor.execute(() -> this.loadingDialogFragment = LoadingDialogFragment.show(getFragmentManager(),
-                                                                                                getString(titleRes),
-                                                                                                LoadingDialogFragment.OPAQUE_BACKGROUND));
+                                                                                                    getString(titleRes),
+                                                                                                    LoadingDialogFragment.OPAQUE_BACKGROUND));
         } else {
-            if(loadingDialogFragment.getDialog() == null){
-                loadingDialogFragment.showAllowingStateLoss(getFragmentManager(),LoadingDialogFragment.TAG);
+            if (loadingDialogFragment.getDialog() == null) {
+                loadingDialogFragment.showAllowingStateLoss(getFragmentManager(), LoadingDialogFragment.TAG);
             }
             loadingDialogFragment.setTitle(getString(titleRes));
         }
@@ -90,12 +91,12 @@ public abstract class PillHardwareFragment extends InjectionFragment {
         });
     }
 
-    protected void hideBlockingActivity(){
+    protected void hideBlockingActivity() {
         //for delay of 1000 millisecond
         LoadingDialogFragment.closeWithOnComplete(getFragmentManager(), null);
     }
 
-    protected void presentPhoneBatteryError(){
+    protected void presentPhoneBatteryError() {
         final ErrorDialogFragment errorDialogFragment = new ErrorDialogFragment.Builder()
                 .withOperation("Check Phone Battery")
                 .withTitle(R.string.error_phone_battery_low_title)
@@ -112,6 +113,7 @@ public abstract class PillHardwareFragment extends InjectionFragment {
     protected void presentError(final Throwable e) {
         //todo confirm all error states.
         @StringRes int title = R.string.error_sleep_pill_title_update_missing;
+        @StringRes int message = R.string.error_sleep_pill_message_update_missing;
         final String helpUriString = UserSupport.DeviceIssue.SLEEP_PILL_WEAK_RSSI.getUri().toString();
         final ErrorDialogFragment.Builder errorDialogBuilder = new ErrorDialogFragment.Builder(e, getActivity());
         errorDialogBuilder.withOperation(operationString());
@@ -120,12 +122,15 @@ public abstract class PillHardwareFragment extends InjectionFragment {
             title = R.string.error_pill_too_far;
         } else if (e instanceof PillNotFoundException) {
             title = R.string.error_pill_not_found;
+        } else if (e instanceof ApiException) {
+            title = R.string.network_activity_no_connectivity;
+            message = R.string.error_network_failure_pair_pill;
         } else {
             errorDialogBuilder.withUnstableBluetoothHelp(getActivity());
         }
         errorDialogBuilder
                 .withTitle(title)
-                .withMessage(StringRef.from(R.string.error_sleep_pill_message_update_missing))
+                .withMessage(StringRef.from(message))
                 .withAction(helpUriString, R.string.label_having_trouble)
                 .build()
                 .showAllowingStateLoss(getFragmentManager(), ErrorDialogFragment.TAG);
