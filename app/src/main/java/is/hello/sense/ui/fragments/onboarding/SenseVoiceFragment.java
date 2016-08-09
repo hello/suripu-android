71a6a89e6dd01875215c48208cef500c7b1b8688
Simon Chen <simon.c312@gmail.com>
2016-08-09 13:20:37 -0700

    add voice tutorial analytics events

--- app/src/main/java/is/hello/sense/ui/fragments/onboarding/SenseVoiceFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/onboarding/SenseVoiceFragment.java
@@ -38,6 +38,7 @@ import is.hello.sense.ui.common.VoiceHelpDialogFragment;
 import is.hello.sense.ui.dialogs.ErrorDialogFragment;
 import is.hello.sense.ui.dialogs.LoadingDialogFragment;
 import is.hello.sense.ui.widget.util.Views;
+import is.hello.sense.util.Analytics;
 import is.hello.sense.util.AnimatorSetHandler;
 import rx.Observable;
 import rx.Subscription;
@@ -82,6 +83,10 @@ public class SenseVoiceFragment extends InjectionFragment {
     public void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPresenter(senseVoicePresenter);
+
+        if(savedInstanceState == null){
+            Analytics.trackEvent(Analytics.Onboarding.EVENT_VOICE_TUTORIAL, null);
+        }
     }
 
     @Nullable
@@ -320,6 +325,8 @@ public class SenseVoiceFragment extends InjectionFragment {
     }
 
     private void handleVoiceResponse(@Nullable final VoiceResponse voiceResponse) {
+        sendAnalyticsEvent(voiceResponse);
+
         if(SenseVoicePresenter.hasSuccessful(voiceResponse)){
             updateState(R.string.sense_voice_question_temperature,
                         R.color.primary,
@@ -418,5 +425,11 @@ public class SenseVoiceFragment extends InjectionFragment {
         animSet.setStartDelay(200);
 
         return animSet;
+    }
+
+    private void sendAnalyticsEvent(@NonNull final VoiceResponse voiceResponse){
+        Analytics.trackEvent(Analytics.Onboarding.EVENT_VOICE_COMMAND,
+                             Analytics.createProperties(Analytics.Onboarding.PROP_VOICE_COMMAND_STATUS,
+                                                        voiceResponse.result));
     }
 }
