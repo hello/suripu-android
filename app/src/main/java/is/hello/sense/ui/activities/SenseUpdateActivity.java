0d6e5dce5c74d0767e40eb6a08682108a72d3f79
Simon Chen <simon.c312@gmail.com>
2016-08-17 10:25:07 -0700

    use finishFlow()
    Preparing Pair new sense session
    
    - Move TroubleshootSenseDialogFragment to its own class.
    - finalize parameters.
    - add method to specify message string on success pairing
    - added pair new 1.5 sense strings
    - add onDestroyView to release views and listeners for ConnectToWifiFragment
    
    clean up after rebase
    
    don't use intent argument to determine if pairNewSenseSession and instead if the fragment's activity is SenseUpdateActivity.
    
    - create SkippableFlow interface to allow skipping to end for ease of testing.
    - add onDestroyView calls to release views in SelectWifiNetworkFragment
    - make ConnectToWifiFragment extend HardwareFragment again and finish after making connection to let activity push pair sense fragment
    
    - Revert ConnectToWifiFragment extends BasePairSenseFragment until Presenter can be extracted.

--- app/src/main/java/is/hello/sense/ui/activities/SenseUpdateActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/SenseUpdateActivity.java
@@ -24,16 +24,17 @@ import is.hello.sense.ui.fragments.onboarding.ConnectToWiFiFragment;
 import is.hello.sense.ui.fragments.onboarding.PairPillFragment;
 import is.hello.sense.ui.fragments.onboarding.PairSenseFragment;
 import is.hello.sense.ui.fragments.onboarding.SelectWiFiNetworkFragment;
-import is.hello.sense.ui.fragments.sense.SenseResetOriginalFragment;
 import is.hello.sense.ui.fragments.onboarding.SenseVoiceFragment;
 import is.hello.sense.ui.fragments.onboarding.VoiceCompleteFragment;
 import is.hello.sense.ui.fragments.onboarding.sense.SenseOTAFragment;
 import is.hello.sense.ui.fragments.onboarding.sense.SenseOTAIntroFragment;
+import is.hello.sense.ui.fragments.sense.SenseResetOriginalFragment;
 import is.hello.sense.ui.fragments.sense.SenseUpdateIntroFragment;
 import is.hello.sense.ui.fragments.sense.SenseUpdateReadyFragment;
+import is.hello.sense.util.SkippableFlow;
 
 public class SenseUpdateActivity extends InjectionActivity
-        implements FragmentNavigation {
+        implements FragmentNavigation, SkippableFlow {
     public static final String ARG_NEEDS_BLUETOOTH = SenseUpdateActivity.class.getName() + ".ARG_NEEDS_BLUETOOTH";
     public static final String EXTRA_DEVICE_ID = SenseUpdateActivity.class.getName() + ".EXTRA_DEVICE_ID";
     public static final int REQUEST_CODE = 0xbeef;
@@ -92,6 +93,15 @@ public class SenseUpdateActivity extends InjectionActivity
         super.onDestroy();
         navigationDelegate.onDestroy();
     }
+
+    //region SkippableFlow interface
+
+    @Override
+    public void skipToEnd() {
+        //todo point to showHomeActivity()
+    }
+
+    //endregion
 
     @Override
     public void pushFragment(@NonNull final Fragment fragment, @Nullable final String title, final boolean wantsBackStackEntry) {
@@ -176,7 +186,7 @@ public class SenseUpdateActivity extends InjectionActivity
 
     public void showSenseUpdate() {
         if (bluetoothStack.isEnabled()) {
-            pushFragment(PairSenseFragment.newUpdateInstance(), null, false);
+            pushFragment(new PairSenseFragment(), null, false);
         } else {
             showBluetoothFragment();
         }
@@ -201,7 +211,7 @@ public class SenseUpdateActivity extends InjectionActivity
     }
 
     public void showSelectWifiNetwork() {
-        pushFragment(SelectWiFiNetworkFragment.newOnboardingInstance(false), null, true);
+        pushFragment(SelectWiFiNetworkFragment.newOnboardingInstance(true), null, true);
     }
 
 
