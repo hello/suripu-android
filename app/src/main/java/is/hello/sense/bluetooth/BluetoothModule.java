328d65d120dd76a4c2cc4fdc503040d0b08e429a
Simon Chen <simon.c312@gmail.com>
2016-08-30 16:13:30 -0700

    - filter set of device ids after discovering peripherals to prevent connecting with current sense in update pair sense screen. - added inject bluetooth fragment to bluetooth module to prevent crash because its not present in SenseAppModule for now

--- app/src/main/java/is/hello/sense/bluetooth/BluetoothModule.java
+++ app/src/main/java/is/hello/sense/bluetooth/BluetoothModule.java
@@ -11,6 +11,7 @@ import is.hello.buruberi.bluetooth.Buruberi;
 import is.hello.buruberi.bluetooth.errors.BuruberiException;
 import is.hello.buruberi.bluetooth.stacks.BluetoothStack;
 import is.hello.buruberi.bluetooth.stacks.util.ErrorListener;
+import is.hello.sense.ui.fragments.onboarding.BluetoothFragment;
 import is.hello.sense.util.Analytics;
 
 /**
@@ -19,25 +20,26 @@ import is.hello.sense.util.Analytics;
  * Requires a containing module to provide an unqualified
  * application Context in order to compile.
  */
-@Module(library = true, complete = false)
+@Module(library = true,
+        complete = false,
+        injects = {
+                BluetoothFragment.class //todo remove when fragment uses presenters
+        })
 @SuppressWarnings("UnusedDeclaration")
 public class BluetoothModule {
     @Provides
     ErrorListener provideErrorListener() {
-        return new ErrorListener() {
-            @Override
-            public void call(Throwable e) {
-                if (e != null && !(e instanceof BuruberiException)) {
-                    Analytics.trackUnexpectedError(e);
-                }
+        return e -> {
+            if (e != null && !(e instanceof BuruberiException)) {
+                Analytics.trackUnexpectedError(e);
             }
         };
     }
 
     @Provides
     @Singleton
-    BluetoothStack provideDeviceCenter(@NonNull Context applicationContext,
-                                       @NonNull ErrorListener errorListener) {
+    BluetoothStack provideDeviceCenter(@NonNull final Context applicationContext,
+                                       @NonNull final ErrorListener errorListener) {
         return new Buruberi()
                 .setApplicationContext(applicationContext)
                 .setErrorListener(errorListener)
