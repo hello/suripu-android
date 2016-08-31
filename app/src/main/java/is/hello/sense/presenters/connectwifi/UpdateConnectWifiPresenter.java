527c9509d20c0bce8a29acc542a9c1bde64eea3e
Simon Chen <simon.c312@gmail.com>
2016-08-31 15:36:03 -0700

    add proper upgrade analytics events for wifi screens

--- app/src/main/java/is/hello/sense/presenters/connectwifi/UpdateConnectWifiPresenter.java
+++ app/src/main/java/is/hello/sense/presenters/connectwifi/UpdateConnectWifiPresenter.java
@@ -48,16 +48,16 @@ public class UpdateConnectWifiPresenter extends BaseConnectWifiPresenter {
 
     @Override
     public String getOnCreateAnalyticsEvent() {
-        return Analytics.Onboarding.EVENT_WIFI_PASSWORD_IN_APP;
+        return Analytics.SenseUpgrade.EVENT_WIFI_PASSWORD;
     }
 
     @Override
     public String getOnSubmitWifiCredentialsAnalyticsEvent() {
-        return Analytics.Onboarding.EVENT_WIFI_CREDENTIALS_SUBMITTED_IN_APP;
+        return Analytics.SenseUpgrade.EVENT_WIFI_CREDENTIALS_SUBMITTED;
     }
 
     @Override
     public String getWifiAnalyticsEvent() {
-        return Analytics.Onboarding.EVENT_SENSE_WIFI_UPDATE_IN_APP;
+        return Analytics.SenseUpgrade.EVENT_SENSE_WIFI_UPDATE;
     }
 }
