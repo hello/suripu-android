db341868e57148c0d744c526d35dcd27835a60ce
David Zafrani <d.zafrani21@gmail.com>
2016-08-31 14:16:07 -0700

    Clean up PairPillPresenter/Fragment

--- app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingRegisterAudioFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingRegisterAudioFragment.java
@@ -18,7 +18,6 @@ import is.hello.sense.ui.common.UserSupport;
 import is.hello.sense.ui.dialogs.ErrorDialogFragment;
 import is.hello.sense.ui.dialogs.LoadingDialogFragment;
 import is.hello.sense.ui.fragments.BaseHardwareFragment;
-import is.hello.sense.ui.fragments.ScopedInjectionFragment;
 import is.hello.sense.util.Analytics;
 import rx.Observable;
 
