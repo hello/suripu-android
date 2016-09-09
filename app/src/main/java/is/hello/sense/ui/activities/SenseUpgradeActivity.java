cdf1d60eb55be0aa6b05b566e538d3e552e970f7
David Zafrani <d.zafrani21@gmail.com>
2016-09-08 17:45:20 -0700

    Reset SenseUpgradeActivity

--- app/src/main/java/is/hello/sense/ui/activities/SenseUpgradeActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/SenseUpgradeActivity.java
@@ -70,8 +70,7 @@ public class SenseUpgradeActivity extends ScopedInjectionActivity
         if (savedInstanceState != null) {
             navigationDelegate.onRestoreInstanceState(savedInstanceState);
         } else if (navigationDelegate.getTopFragment() == null) {
-          //  showSenseUpdateIntro();
-            showUnpairPillFragment();
+            showSenseUpdateIntro();
             storeCurrentSenseDevice();
         }
     }
