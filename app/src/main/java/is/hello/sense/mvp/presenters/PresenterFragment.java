a463090a36ffeef368eb13c9f623dc4eab8677f7
David Zafrani <d.zafrani21@gmail.com>
2016-09-09 19:44:59 -0700

    huge change to our viper design

---
+++ app/src/main/java/is/hello/sense/mvp/presenters/PresenterFragment.java
@@ -0,0 +1,172 @@
+package is.hello.sense.mvp.presenters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.CallSuper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import is.hello.go99.animators.AnimatorContext;
+import is.hello.sense.mvp.view.PresenterView;
+import is.hello.sense.ui.common.UserSupport;
+import is.hello.sense.ui.dialogs.ErrorDialogFragment;
+import is.hello.sense.ui.dialogs.LoadingDialogFragment;
+import is.hello.sense.ui.widget.SenseAlertDialog;
+import is.hello.sense.util.Logger;
+
+public abstract class PresenterFragment<T extends PresenterView> extends ObserverFragment {
+
+    protected boolean animatorContextFromActivity = false;
+    protected LoadingDialogFragment loadingDialogFragment;
+    protected T presenterView;
+
+    abstract T getPresenterView();
+
+    @Nullable
+    protected AnimatorContext animatorContext;
+
+    @CallSuper
+    @Override
+    public void onAttach(final Context context) {
+        super.onAttach(context);
+        if (animatorContext == null && context instanceof AnimatorContext.Scene) {
+            this.animatorContext = ((AnimatorContext.Scene) context).getAnimatorContext();
+            this.animatorContextFromActivity = true;
+        }
+        presenterView = getPresenterView();
+    }
+
+    @CallSuper
+    @Override
+    public void onAttach(final Activity activity) {
+        super.onAttach(activity);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return;
+        }
+        if (animatorContext == null && activity instanceof AnimatorContext.Scene) {
+            this.animatorContext = ((AnimatorContext.Scene) activity).getAnimatorContext();
+            this.animatorContextFromActivity = true;
+        }
+        presenterView = getPresenterView();
+    }
+
+    @CallSuper
+    @Override
+    public void onResume() {
+        super.onResume();
+        presenterView.resume();
+    }
+
+    @CallSuper
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        presenterView.create();
+    }
+
+    @CallSuper
+    @NonNull
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
+        return presenterView.createView(inflater, container, savedInstanceState);
+    }
+
+    @CallSuper
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        this.presenterView.destroyView();
+    }
+
+    @CallSuper
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.animatorContext = null;
+        this.animatorContextFromActivity = false;
+        this.presenterView = null;
+        //todo make instance of presenter null?
+    }
+
+    @NonNull
+    public AnimatorContext getAnimatorContext() {
+        if (animatorContext == null) {
+            this.animatorContext = new AnimatorContext(getClass().getSimpleName());
+            Logger.debug(getClass().getSimpleName(), "Creating animator context");
+        }
+
+        return animatorContext;
+    }
+    //region BaseOutput
+
+    public void hideBlockingActivity(@StringRes final int text, @Nullable final Runnable onCompletion) {
+        LoadingDialogFragment.closeWithMessageTransition(getFragmentManager(),
+                                                         () -> {
+                                                             loadingDialogFragment = null;
+                                                             if (onCompletion != null) {
+                                                                 onCompletion.run();
+                                                             }
+                                                         },
+                                                         text);
+    }
+
+    public void hideBlockingActivity(final boolean success, @NonNull final Runnable onCompletion) {
+        if (success) {
+            LoadingDialogFragment.closeWithDoneTransition(getFragmentManager(), () -> {
+                this.loadingDialogFragment = null;
+                onCompletion.run();
+            });
+        } else {
+            LoadingDialogFragment.close(getFragmentManager());
+            this.loadingDialogFragment = null;
+            onCompletion.run();
+        }
+    }
+
+    public void showBlockingActivity(@StringRes final int titleRes) {
+        if (loadingDialogFragment == null) {
+            this.loadingDialogFragment = LoadingDialogFragment.show(getFragmentManager(),
+                                                                    getString(titleRes),
+                                                                    LoadingDialogFragment.OPAQUE_BACKGROUND);
+        } else {
+            loadingDialogFragment.setTitle(getString(titleRes));
+        }
+    }
+
+    public void showErrorDialog(@NonNull final ErrorDialogFragment.PresenterBuilder builder) {
+        builder.build().showAllowingStateLoss(getFragmentManager(), ErrorDialogFragment.TAG);
+    }
+
+    public void showAlertDialog(@NonNull final SenseAlertDialog.Builder builder) {
+        builder.build(getActivity()).show();
+    }
+
+    public void showHelpUri(@NonNull final Uri uri) {
+        UserSupport.openUri(getActivity(), uri);
+    }
+
+    public void showHelpUri(@NonNull final String uri) {
+        showHelpUri(Uri.parse(uri));
+    }
+
+    public void showHelpUri(@NonNull final UserSupport.HelpStep helpStep) {
+        UserSupport.showForHelpStep(getActivity(), helpStep);
+    }
+
+    public void showHelpUri(@NonNull final UserSupport.DeviceIssue deviceIssue) {
+        UserSupport.showForDeviceIssue(getActivity(), deviceIssue);
+    }
+
+    public void finishActivity() {
+        getActivity().finish();
+    }
+
+    //endregion
+}
---
+++ app/src/main/java/is/hello/sense/mvp/view/BacksideView.java
@@ -0,0 +1,193 @@
+package is.hello.sense.mvp.view;
+
+import android.app.Activity;
+import android.app.FragmentManager;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ToggleButton;
+
+import is.hello.go99.Anime;
+import is.hello.sense.R;
+import is.hello.sense.ui.adapter.StaticFragmentAdapter;
+import is.hello.sense.ui.fragments.BacksideTabFragment;
+import is.hello.sense.ui.fragments.InsightsFragment;
+import is.hello.sense.ui.fragments.RoomConditionsFragment;
+import is.hello.sense.ui.fragments.TrendsFragment;
+import is.hello.sense.ui.fragments.settings.AppSettingsFragment;
+import is.hello.sense.ui.fragments.sounds.SoundsFragment;
+import is.hello.sense.ui.widget.ExtendedViewPager;
+import is.hello.sense.ui.widget.SelectorView;
+
+public final class BacksideView extends PresenterView {
+    public static final int ITEM_ROOM_CONDITIONS = 0;
+    public static final int ITEM_TRENDS = 1;
+    public static final int ITEM_INSIGHTS = 2;
+    public static final int ITEM_SOUNDS = 3;
+    public static final int ITEM_APP_SETTINGS = 4;
+    public static final int OPTION_ANIMATE = (1 << 1);
+    public static final int DEFAULT_START_ITEM = BacksideView.ITEM_INSIGHTS;
+
+    private int tabSelectorHeight;
+    private SelectorView tabSelector;
+    private ExtendedViewPager pager;
+    private StaticFragmentAdapter adapter;
+
+
+    public BacksideView(@NonNull final Activity activity) {
+        super(activity);
+    }
+
+    @NonNull
+    @Override
+    public final View createView(@NonNull final LayoutInflater inflater, @NonNull final ViewGroup container, @Nullable final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.fragment_backside, container, false);
+        final Resources resources = context.getResources();
+        this.pager = (ExtendedViewPager) view.findViewById(R.id.fragment_backside_pager);
+
+        this.tabSelectorHeight = resources.getDimensionPixelSize(R.dimen.action_bar_height);
+        this.tabSelector = (SelectorView) view.findViewById(R.id.fragment_backside_tabs);
+        tabSelector.setButtonLayoutParams(new SelectorView.LayoutParams(0, SelectorView.LayoutParams.MATCH_PARENT, 1));
+        final @DrawableRes int[] inactiveIcons = {
+                R.drawable.backside_icon_currently,
+                R.drawable.backside_icon_trends,
+                R.drawable.backside_icon_insights,
+                R.drawable.backside_icon_sounds,
+                R.drawable.backside_icon_settings,
+        };
+        final @DrawableRes int[] activeIcons = {
+                R.drawable.backside_icon_currently_active,
+                R.drawable.backside_icon_trends_active,
+                R.drawable.backside_icon_insights_active,
+                R.drawable.backside_icon_sounds_active,
+                R.drawable.backside_icon_settings_active,
+        };
+        for (int i = 0, count = adapter.getCount(); i < count; i++) {
+            final SpannableString inactiveContent = createIconSpan(adapter.getPageTitle(i),
+                                                                   inactiveIcons[i]);
+            final SpannableString activeContent = createIconSpan(adapter.getPageTitle(i),
+                                                                 activeIcons[i]);
+            final ToggleButton button = tabSelector.addOption(activeContent, inactiveContent, false);
+            button.setPadding(0, 0, 0, 0);
+        }
+        tabSelector.setSelectedIndex(pager.getCurrentItem());
+        return view;
+    }
+
+    @Override
+    public final void destroyView() {
+        pager.clearOnPageChangeListeners();
+    }
+
+    public final void addOnPageChangeListener(@NonNull final ViewPager.OnPageChangeListener listener) {
+        pager.addOnPageChangeListener(listener);
+    }
+
+    public final void setOnSelectionChangedListener(@NonNull final SelectorView.OnSelectionChangedListener listener) {
+        tabSelector.setOnSelectionChangedListener(listener);
+    }
+
+    public final void setAdapter(@NonNull final FragmentManager fragmentManager) {
+        this.adapter = new StaticFragmentAdapter(fragmentManager,
+                                                 new StaticFragmentAdapter.Item(RoomConditionsFragment.class, getString(R.string.title_current_conditions)),
+                                                 new StaticFragmentAdapter.Item(TrendsFragment.class, getString(R.string.title_trends)),
+                                                 new StaticFragmentAdapter.Item(InsightsFragment.class, getString(R.string.action_insights)),
+                                                 new StaticFragmentAdapter.Item(SoundsFragment.class, getString(R.string.action_alarm)),
+                                                 new StaticFragmentAdapter.Item(AppSettingsFragment.class, getString(R.string.action_settings)));
+        pager.setAdapter(adapter);
+    }
+
+    public final void setCurrentItem(final int currentItem, final int options) {
+        final boolean animate = ((options & OPTION_ANIMATE) == OPTION_ANIMATE);
+        pager.setCurrentItem(currentItem, animate);
+    }
+
+    public final boolean isShowingAppSettings() {
+        return pager.getCurrentItem() == BacksideView.ITEM_APP_SETTINGS;
+    }
+
+    public final boolean isShowingDefaultStartItem() {
+        return pager.getCurrentItem() != DEFAULT_START_ITEM;
+    }
+
+    public final void setSelectedIndex(final int position) {
+        tabSelector.setSelectedIndex(position);
+    }
+
+    @Nullable
+    public final BacksideTabFragment getCurrentTabFragment(@NonNull final FragmentManager fragmentManager) {
+        if (adapter != null) {
+            // This depends on semi-undefined behavior. It may break in a future update
+            // of the Android support library, but won't break if the host OS changes.
+            return (BacksideTabFragment) fragmentManager.findFragmentByTag(getItemTag(pager.getCurrentItem()));
+        } else {
+            return null;
+        }
+    }
+
+    public final void setChromeTranslationAmount(final float amount) {
+        final float tabsTranslationY = Anime.interpolateFloats(amount, 0f, -tabSelectorHeight);
+        tabSelector.setTranslationY(tabsTranslationY);
+
+        final float buttonAlpha = Anime.interpolateFloats(amount, 1f, 0f);
+        for (int i = 0, count = tabSelector.getButtonCount(); i < count; i++) {
+            tabSelector.getButtonAt(i).setAlpha(buttonAlpha);
+        }
+    }
+
+    public final float getChromeTranslationAmount() {
+        return (-tabSelector.getTranslationY() / tabSelectorHeight);
+    }
+
+    public final void setHasUnreadInsightItems(final boolean hasUnreadInsightItems) {
+        final @DrawableRes int iconRes = hasUnreadInsightItems
+                ? R.drawable.backside_icon_insights_unread
+                : R.drawable.backside_icon_insights;
+
+        final ToggleButton button = tabSelector.getButtonAt(ITEM_INSIGHTS);
+        final SpannableString inactiveContent = createIconSpan(adapter.getPageTitle(ITEM_INSIGHTS),
+                                                               iconRes);
+        button.setTextOff(inactiveContent);
+        if (!button.isChecked()) {
+            button.setText(inactiveContent);
+        }
+    }
+
+    public final void setHasUnreadAccountItems(final boolean hasUnreadAccountItems) {
+        final @DrawableRes int iconRes = hasUnreadAccountItems
+                ? R.drawable.backside_icon_settings_unread
+                : R.drawable.backside_icon_settings;
+
+        final ToggleButton button = tabSelector.getButtonAt(ITEM_APP_SETTINGS);
+        final SpannableString inactiveContent = createIconSpan(adapter.getPageTitle(ITEM_APP_SETTINGS),
+                                                               iconRes);
+        button.setTextOff(inactiveContent);
+        if (!button.isChecked()) {
+            button.setText(inactiveContent);
+        }
+    }
+
+    private String getItemTag(int position) {
+        if (position < 0 || position > adapter.getCount() - 1) {
+            position = pager.getCurrentItem();
+        }
+        return "android:switcher:" + pager.getId() + ":" + adapter.getItemId(position);
+    }
+
+    private SpannableString createIconSpan(@NonNull final CharSequence title, @DrawableRes final int iconRes) {
+        final ImageSpan image = new ImageSpan(context, iconRes);
+        final SpannableString span = new SpannableString(title);
+        span.setSpan(image, 0, span.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        return span;
+    }
+
+}
