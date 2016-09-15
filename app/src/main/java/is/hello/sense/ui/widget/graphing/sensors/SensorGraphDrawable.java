00ecec4a69c0ff43bd418c8788b10148190b3e2f
David Zafrani <d.zafrani21@gmail.com>
2016-09-15 11:54:19 -0700

    Sensor graphs working. Need cleaning

--- app/src/main/java/is/hello/sense/ui/widget/graphing/sensors/SensorGraphDrawable.java
+++ app/src/main/java/is/hello/sense/ui/widget/graphing/sensors/SensorGraphDrawable.java
@@ -10,6 +10,7 @@ import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.util.TypedValue;
 
 import java.util.Random;
@@ -19,6 +20,7 @@ import is.hello.sense.api.model.v2.sensors.Sensor;
 public class SensorGraphDrawable extends Drawable {
     private final Sensor sensor;
     private final int height;
+    private final int minHeight;
     private final float scaleRatio;
     private final ValueLimits valueLimits;
 
@@ -26,15 +28,17 @@ public class SensorGraphDrawable extends Drawable {
     private final Paint edgePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public SensorGraphDrawable(@NonNull final Context context, @NonNull final Sensor sensor) {
-        this.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 150, context.getResources().getDisplayMetrics());
+        this.height = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 120, context.getResources().getDisplayMetrics());
         this.sensor = sensor;
         final int color = sensor.getColor(context);
         this.paint.setColor(color);
-        this.paint.setAlpha(200);
+        this.paint.setAlpha(100);
+        this.paint.setStyle(Paint.Style.FILL);
         this.edgePaint.setColor(color);
         this.edgePaint.setAlpha(255);
         this.valueLimits = new ValueLimits(sensor.getSensorValues());
-        this.scaleRatio = height / valueLimits.max;
+        this.minHeight = (int) (height * .99f);
+        this.scaleRatio = minHeight / valueLimits.max;
 
 
     }
@@ -47,21 +51,30 @@ public class SensorGraphDrawable extends Drawable {
     @Override
     public void draw(final Canvas canvas) {
         final float[] values = sensor.getSensorValues();
-        final float xIncrement = values.length / canvas.getWidth();
-
+        final Path path = new Path();
+        final Path edgePath = new Path();
+        edgePath.reset();
+        final double xIncrement = (double) canvas.getWidth() / (double) values.length + 1;
+        float lastEnd = 0;
         for (int i = 0; i < values.length; i++) {
             final float value = values[i];
             final RectF rectF = new RectF();
-            rectF.left = xIncrement * i;
-            rectF.right = rectF.left + xIncrement;
-            rectF.top = new Random().nextInt(50) + 30;
+            rectF.left = (int) (lastEnd);
+            rectF.right = (int) (rectF.left + xIncrement);
             rectF.bottom = canvas.getHeight();
+            rectF.top = minHeight - value * scaleRatio;
             final RectF edgeRectF = new RectF(rectF);
-            edgeRectF.bottom = edgeRectF.top;
-            edgeRectF.top -= 10;
-            canvas.drawRect(rectF, paint);
-            canvas.drawRect(edgeRectF, edgePaint);
+            edgeRectF.bottom = edgeRectF.top + 4;
+            path.addRect(rectF, Path.Direction.CW);
+            //edgePath.addRect(edgeRectF, Path.Direction.CW);
+            edgePath.moveTo((float) (rectF.left + xIncrement / 2), edgeRectF.top);
+
+
+            lastEnd = rectF.right;
         }
+
+        canvas.drawPath(path, paint);
+        canvas.drawPath(edgePath, edgePaint);
 
 
     }
