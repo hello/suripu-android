5db3d1dde9975a5418814e06d98366647bbed07c
David Zafrani <d.zafrani21@gmail.com>
2016-09-22 11:46:40 -0700

    Showing value with correct color and size. Showing graph at the bottom

--- app/src/main/java/is/hello/sense/mvp/presenters/SensorDetailFragment.java
+++ app/src/main/java/is/hello/sense/mvp/presenters/SensorDetailFragment.java
@@ -3,6 +3,7 @@ package is.hello.sense.mvp.presenters;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.view.View;
 
 import is.hello.sense.api.model.v2.sensors.Sensor;
 import is.hello.sense.mvp.view.SensorDetailView;
@@ -24,7 +25,7 @@ public final class SensorDetailFragment extends PresenterFragment<SensorDetailVi
     @Override
     public final void initializePresenterView() {
         if (presenterView == null) {
-            this.presenterView = new SensorDetailView(getActivity(), sensor.getColor(getActivity()));
+            this.presenterView = new SensorDetailView(getActivity());
         }
     }
 
@@ -41,7 +42,12 @@ public final class SensorDetailFragment extends PresenterFragment<SensorDetailVi
             return;
         }
 
-        sensor = (Sensor) args.getSerializable(ARG_SENSOR);
+        this.sensor = (Sensor) args.getSerializable(ARG_SENSOR);
+    }
 
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.presenterView.showSensor(this.sensor);
     }
 }
