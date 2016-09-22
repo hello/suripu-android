250cddeb049b66f32f9fa1f76e9adabb5bc55373
David Zafrani <d.zafrani21@gmail.com>
2016-09-22 11:46:40 -0700

    Showing value with correct color and size. Showing graph at the bottom

--- app/src/main/java/is/hello/sense/mvp/view/SensorDetailView.java
+++ app/src/main/java/is/hello/sense/mvp/view/SensorDetailView.java
@@ -3,26 +3,29 @@ package is.hello.sense.mvp.view;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.widget.TextView;
 
 import is.hello.sense.R;
+import is.hello.sense.api.model.v2.sensors.Sensor;
 import is.hello.sense.ui.widget.SelectorView;
+import is.hello.sense.ui.widget.graphing.sensors.SensorGraphDrawable;
+import is.hello.sense.ui.widget.graphing.sensors.SensorGraphView;
 
 @SuppressLint("ViewConstructor")
 public final class SensorDetailView extends PresenterView {
     private final SelectorView subNavSelector;
+    private final TextView value;
+    private final TextView message;
+    private final SensorGraphView sensorGraphView;
 
-    public SensorDetailView(@NonNull final Activity activity,
-                            final int color) {
+    public SensorDetailView(@NonNull final Activity activity) {
         super(activity);
         this.subNavSelector = (SelectorView) findViewById(R.id.fragment_sensor_detail_selector);
-        this.subNavSelector.setToggleButtonColor(R.color.white);
-        this.subNavSelector.addOption(R.string.sensor_detail_last_day, false)
-                           .setBackgroundColor(color);
-        this.subNavSelector.addOption(R.string.sensor_detail_past_week, false)
-                           .setBackgroundColor(color);
-        this.subNavSelector.setBackgroundColor(color);
-    }
+        this.value = (TextView) findViewById(R.id.fragment_sensor_detail_value);
+        this.message = (TextView) findViewById(R.id.fragment_sensor_detail_message);
+        this.sensorGraphView = (SensorGraphView) findViewById(R.id.fragment_sensor_detail_graph_view);
 
+      }
 
     @Override
     protected final int getLayoutRes() {
@@ -31,6 +34,23 @@ public final class SensorDetailView extends PresenterView {
 
     @Override
     public final void releaseViews() {
+
+    }
+
+    public final void showSensor(@NonNull final Sensor sensor){
+        this.subNavSelector.removeAllButtons();
+        final int color = sensor.getColor(context);
+        this.subNavSelector.setToggleButtonColor(R.color.white);
+        this.subNavSelector.addOption(R.string.sensor_detail_last_day, false)
+                           .setBackgroundColor(color);
+        this.subNavSelector.addOption(R.string.sensor_detail_past_week, false)
+                           .setBackgroundColor(color);
+        this.subNavSelector.setBackgroundColor(color);
+        this.value.setText(sensor.getFormattedValue(true));
+        this.message.setText(sensor.getMessage());
+        this.sensorGraphView.setSensorGraphDrawable(new SensorGraphDrawable(context, sensor));
+        this.value.setTextColor(color);
+
 
     }
 }
