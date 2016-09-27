31ff9a4553387358edf2a49a6c6e889eb3a834f1
David Zafrani <d.zafrani21@gmail.com>
2016-09-27 13:07:44 -0700

    Context is final to reduce checks and npe on destroy/detach

--- app/src/main/java/is/hello/sense/api/model/v2/sensors/Sensor.java
+++ app/src/main/java/is/hello/sense/api/model/v2/sensors/Sensor.java
@@ -1,6 +1,7 @@
 package is.hello.sense.api.model.v2.sensors;
 
 import android.content.Context;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
