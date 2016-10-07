542b1d5ed7ede104ad98bd81d0800360a5f54399
David Zafrani <d.zafrani21@gmail.com>
2016-10-07 11:29:56 -0700

    Added expansion models

---
+++ app/src/main/java/is/hello/sense/api/model/v2/expansions/Configuration.java
@@ -0,0 +1,28 @@
+package is.hello.sense.api.model.v2.expansions;
+
+import com.google.gson.annotations.SerializedName;
+
+
+import is.hello.sense.api.model.ApiResponse;
+
+public class Configuration extends ApiResponse {
+    @SerializedName("id")
+    private String id;
+
+    @SerializedName("name")
+    private String name;
+
+    @SerializedName("selected")
+    private boolean selected;
+
+
+    @Override
+    public String toString() {
+        return "Configuration{" +
+                "id=" + id +
+                ", name=" + name +
+                ", selected=" + selected +
+                "}";
+
+    }
+}
