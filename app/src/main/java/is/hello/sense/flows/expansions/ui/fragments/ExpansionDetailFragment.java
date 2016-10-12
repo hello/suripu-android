2395ad858c386cdf2fe72a3ffd33185e75591cd9
Simon Chen <simon.c312@gmail.com>
2016-10-11 17:32:34 -0700

    set up action click listener, picasso load icon

--- app/src/main/java/is/hello/sense/flows/expansions/ui/fragments/ExpansionDetailFragment.java
+++ app/src/main/java/is/hello/sense/flows/expansions/ui/fragments/ExpansionDetailFragment.java
@@ -1,13 +1,38 @@
 package is.hello.sense.flows.expansions.ui.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+
+import com.squareup.picasso.Picasso;
+
+import javax.inject.Inject;
+
 import is.hello.sense.flows.expansions.ui.views.ExpansionDetailView;
 import is.hello.sense.mvp.presenters.PresenterFragment;
 
 public class ExpansionDetailFragment extends PresenterFragment<ExpansionDetailView> {
+
+    @Inject
+    Picasso picasso;
+
     @Override
     public void initializePresenterView() {
         if(presenterView == null){
             presenterView = new ExpansionDetailView(getActivity());
+            presenterView.setActionButtonClickListener(this::handleActionButtonClicked);
         }
+    }
+
+    @Override
+    public void onViewCreated(final View view, final Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if(savedInstanceState != null) {
+            presenterView.loadExpansionIcon(picasso, "");
+        }
+    }
+
+    private void handleActionButtonClicked(final View ignored) {
+
     }
 }
--- app/src/main/java/is/hello/sense/flows/expansions/ui/views/ExpansionDetailView.java
+++ app/src/main/java/is/hello/sense/flows/expansions/ui/views/ExpansionDetailView.java
@@ -2,14 +2,32 @@ package is.hello.sense.flows.expansions.ui.views;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import is.hello.sense.R;
 import is.hello.sense.mvp.view.PresenterView;
+import is.hello.sense.ui.widget.util.Views;
 
 public class ExpansionDetailView extends PresenterView {
 
-    public ExpansionDetailView(@NonNull Activity activity) {
+    final TextView deviceNameTextView;
+    final TextView serviceNameTextView;
+    final ImageView expansionIconImageView;
+    final TextView expansionDescriptionTextView;
+    final Button actionButton;
+
+    public ExpansionDetailView(@NonNull final Activity activity) {
         super(activity);
+        this.deviceNameTextView = (TextView) findViewById(R.id.view_expansion_detail_device_name);
+        this.serviceNameTextView = (TextView) findViewById(R.id.view_expansion_detail_device_service_name);
+        this.expansionIconImageView = (ImageView) findViewById(R.id.view_expansion_detail_icon);
+        this.expansionDescriptionTextView = (TextView) findViewById(R.id.view_expansion_detail_description);
+        this.actionButton = (Button) findViewById(R.id.view_expansion_detail_action_button);
     }
 
     @Override
@@ -19,6 +37,16 @@ public class ExpansionDetailView extends PresenterView {
 
     @Override
     public void releaseViews() {
+        this.actionButton.setOnClickListener(null);
+    }
 
+    public void loadExpansionIcon(@NonNull final Picasso picasso,
+                                  @NonNull final String url){
+        picasso.load(url)
+               .into(expansionIconImageView);
+    }
+
+    public void setActionButtonClickListener(@Nullable final OnClickListener onClickListener){
+        Views.setSafeOnClickListener(this.actionButton, onClickListener);
     }
 }
