e01e9133366837490b19ef6db2710b3a77e82d9c
David Zafrani <d.zafrani21@gmail.com>
2016-10-25 17:50:18 -0700

    in progress

---
+++ app/src/debug/java/is/hello/sense/adapter/CustomAdapter.java
@@ -0,0 +1,120 @@
+package is.hello.sense.adapter;
+
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import is.hello.go99.Anime;
+import is.hello.sense.R;
+import is.hello.sense.ui.adapter.ArrayRecyclerAdapter;
+
+
+/**
+ * todo rename this file
+ */
+public class CustomAdapter extends ArrayRecyclerAdapter<Integer, CustomAdapter.BaseViewHolder> {
+    private final LayoutInflater inflater;
+    final int min;
+    final int max;
+    int centerItemPosition = 0;
+    final int textSizeNormal;
+    final int textSizeLarge;
+    boolean animateAbove = false;
+    private TextView currentCenter;
+
+    public CustomAdapter(@NonNull final LayoutInflater inflater,
+                         final int min,
+                         final int max) {
+        super(new ArrayList<>());
+        this.inflater = inflater;
+        this.min = min;
+        this.max = max;
+        this.textSizeNormal = inflater.getContext().getResources().getDimensionPixelSize(R.dimen.text_body);
+        this.textSizeLarge = inflater.getContext().getResources().getDimensionPixelSize(R.dimen.text_h4);
+
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public Integer getItem(final int position) {
+        return position % (max - min + 1);
+        //return min + position + 1;
+    }
+
+    @Override
+    public BaseViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        return new BaseViewHolder(inflater.inflate(android.R.layout.simple_list_item_1, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(final BaseViewHolder holder, final int position) {
+        holder.bind(position);
+    }
+
+    public void setCenterItemPosition(final int position) {
+        if (this.centerItemPosition == position) {
+            return;
+        }
+        Log.e("SetCenter", "Position: " + position);
+        if (currentCenter != null) {
+            animateTextSize(currentCenter, false);
+        }
+        animateAbove = centerItemPosition > position;
+        this.centerItemPosition = position;
+        notifyDataSetChanged();
+    }
+
+
+    public class BaseViewHolder extends ArrayRecyclerAdapter.ViewHolder {
+        private final TextView textView;
+
+        public BaseViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            this.textView = (TextView) itemView.findViewById(android.R.id.text1);
+            textView.setTextSize(textSizeNormal);
+        }
+
+        @Override
+        public void bind(final int position) {
+            super.bind(position);
+            if (position == centerItemPosition) {
+                Log.e("Animating", "Position: " + position);
+                currentCenter = textView;
+                textView.setTextColor(Color.BLUE);
+                animateTextSize(textView, true);
+            } else if (animateAbove && position == centerItemPosition - 1) {
+
+            } else if (!animateAbove && position == centerItemPosition + 1) {
+
+            } else {
+                //    textView.setTextSize(textSizeNormal);
+            }
+            textView.setText(getItem(position) + "%");
+        }
+    }
+
+    private void animateTextSize(@NonNull final TextView textView, boolean increase) {
+        final ValueAnimator animator;
+        if (increase) {
+            animator = ValueAnimator.ofFloat(textSizeNormal, textSizeLarge);
+        } else {
+            animator = ValueAnimator.ofFloat(textSizeLarge, textSizeNormal);
+        }
+        animator.setDuration(Anime.DURATION_FAST);
+        animator.setInterpolator(Anime.INTERPOLATOR_DEFAULT);
+        animator.addUpdateListener(animation -> textView.setTextSize((float) animation.getAnimatedValue()));
+        animator.start();
+    }
+}
---
+++ app/src/debug/java/is/hello/sense/debug/DeleteMeActivity.java
@@ -0,0 +1,47 @@
+package is.hello.sense.debug;
+
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import is.hello.sense.R;
+import is.hello.sense.adapter.CustomAdapter;
+import is.hello.sense.ui.activities.SenseActivity;
+
+/**
+ * Temporary activity to test the view.
+ * <p>
+ * todo Delete when alarms support the view
+ */
+public class DeleteMeActivity extends SenseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_me);
+
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.activity_delete_me_recycler);
+        final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setItemAnimator(null);
+        final CustomAdapter adapter = new CustomAdapter(getLayoutInflater(), 0, 100);
+        recyclerView.setAdapter(adapter);
+        layoutManager.scrollToPosition(Integer.MAX_VALUE / 2);
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                final int firstPosition = layoutManager.findFirstVisibleItemPosition();
+                final int lastPosition = layoutManager.findLastVisibleItemPosition();
+                final int center = firstPosition + (lastPosition - firstPosition) / 2;
+                adapter.setCenterItemPosition(center);
+            }
+        });
+    }
+}
