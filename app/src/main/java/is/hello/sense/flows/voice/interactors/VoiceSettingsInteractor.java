b3c6ed054b00c9e39a1c9853e8000cf74c113956
David Zafrani <d.zafrani21@gmail.com>
2016-10-28 12:15:47 -0700

    PR fixes

--- app/src/main/java/is/hello/sense/flows/voice/interactors/VoiceSettingsInteractor.java
+++ app/src/main/java/is/hello/sense/flows/voice/interactors/VoiceSettingsInteractor.java
@@ -22,11 +22,10 @@ public class VoiceSettingsInteractor extends ValueInteractor<SenseVoiceSettings>
     private static final String KEY_SENSE_ID = VoiceSettingsInteractor.class.getName() + "KEY_SENSE_ID";
     private final ApiService apiService;
     private String senseId = EMPTY_ID;
-    private boolean changingVolume = false;
 
     public InteractorSubject<SenseVoiceSettings> settingsSubject = this.subject;
 
-    public VoiceSettingsInteractor(@NonNull final ApiService apiService){
+    public VoiceSettingsInteractor(@NonNull final ApiService apiService) {
         this.apiService = apiService;
     }
 
@@ -42,7 +41,7 @@ public class VoiceSettingsInteractor extends ValueInteractor<SenseVoiceSettings>
 
     @Override
     protected Observable<SenseVoiceSettings> provideUpdateObservable() {
-        if(EMPTY_ID.equals(senseId)){
+        if (EMPTY_ID.equals(senseId)) {
             return Observable.error(new SenseNotFoundError()); //todo handle better
         }
         return apiService.getVoiceSettings(senseId);
@@ -58,75 +57,51 @@ public class VoiceSettingsInteractor extends ValueInteractor<SenseVoiceSettings>
     @Override
     public Bundle onSaveState() {
         Bundle bundle = super.onSaveState();
-        if(bundle == null){
+        if (bundle == null) {
             bundle = new Bundle();
         }
         bundle.putString(KEY_SENSE_ID, senseId);
         return bundle;
     }
 
-    public Observable<SenseVoiceSettings> setMuted(final boolean muted){
-        changingVolume = false;
-        return getNewInstanceOfLatestSettings()
-                .flatMap( newSettings -> {
-                    newSettings.setMuted(muted);
-                    return setAndPoll(newSettings);
-                });
+    public Observable<SenseVoiceSettings> setMuted(final boolean muted) {
+        return setAndPoll(new SenseVoiceSettings(null, muted, null));
     }
 
-    public Observable<SenseVoiceSettings> setPrimaryUser(final boolean isPrimaryUser){
-        changingVolume = false;
-        return getNewInstanceOfLatestSettings()
-                .flatMap( newSettings -> {
-                    newSettings.setPrimaryUser(isPrimaryUser);
-                    return setAndPoll(newSettings);
-                });
+    public Observable<SenseVoiceSettings> setPrimaryUser(final boolean isPrimaryUser) {
+        return setAndPoll(new SenseVoiceSettings(null, null, isPrimaryUser));
     }
 
-    public Observable<SenseVoiceSettings> setVolume(final int volume){
-        changingVolume = true;
-        return getNewInstanceOfLatestSettings()
-                .flatMap(newSettings -> {
-                    newSettings.setVolume(volume);
-                    return setAndPoll(newSettings);
-                });
-    }
-
-    private Observable<SenseVoiceSettings> getNewInstanceOfLatestSettings() {
-        return latest().map(SenseVoiceSettings::newInstance);
+    public Observable<SenseVoiceSettings> setVolume(final int volume) {
+        return setAndPoll(new SenseVoiceSettings(volume, null, null));
     }
 
     /**
      * Attempts to patch newSettings and retry polling for current settings until updated or failed
+     *
      * @return Observable Boolean true if new settings has updated {@link this#settingsSubject}
      */
-    private Observable<SenseVoiceSettings> setAndPoll(@NonNull final SenseVoiceSettings newSettings){
+    private Observable<SenseVoiceSettings> setAndPoll(@NonNull final SenseVoiceSettings newSettings) {
         //todo handle error if update fails after repeat count max
         return apiService.setVoiceSettings(senseId, newSettings)
-                         .flatMap( ignore -> provideUpdateObservable()
-                                 .doOnNext( responseSettings -> {
-                                     //updateVolumeTextView subject with new settings
+                         .flatMap(ignore -> provideUpdateObservable()
+                                 .doOnNext(responseSettings -> {
                                      if (newSettings.equals(responseSettings)) {
-                                         if (!changingVolume) {
-                                             responseSettings.setVolume(settingsSubject.getValue().getVolume());
-                                         }
                                          settingsSubject.onNext(responseSettings);
                                      }
                                  })
-                                 .repeatWhen( onComplete -> onComplete.zipWith(
+                                 .repeatWhen(onComplete -> onComplete.zipWith(
                                          Observable.range(1, REPEAT_MAX_COUNT)
                                          , (ignore2, integer) -> integer)
-                                            .takeUntil( integer -> hasUpdatedTo(newSettings))
-                                            .flatMap( repeatCount -> Observable.timer(RESUBSCRIBE_DELAY_MILLIS, TimeUnit.MILLISECONDS))
-                                            )
-                                 );
+                                                                     .takeUntil(integer -> hasUpdatedTo(newSettings))
+                                                                     .flatMap(repeatCount -> Observable.timer(RESUBSCRIBE_DELAY_MILLIS, TimeUnit.MILLISECONDS))));
     }
 
     private Boolean hasUpdatedTo(@NonNull final SenseVoiceSettings newSettings) {
         return settingsSubject.hasValue() && settingsSubject.getValue().equals(newSettings);
     }
 
-    public void setSenseId(@NonNull final String id){
+    public void setSenseId(@NonNull final String id) {
         this.senseId = id;
     }
 }
