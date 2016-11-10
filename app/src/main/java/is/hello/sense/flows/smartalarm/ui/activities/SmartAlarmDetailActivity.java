d31f86f68b4933ebe36215aa3ce2518494d77741
David Zafrani <d.zafrani21@gmail.com>
2016-11-10 12:31:44 -0800

    Added fragment navigation and back options

--- app/src/main/java/is/hello/sense/flows/smartalarm/ui/activities/SmartAlarmDetailActivity.java
+++ app/src/main/java/is/hello/sense/flows/smartalarm/ui/activities/SmartAlarmDetailActivity.java
@@ -1,17 +1,27 @@
 package is.hello.sense.flows.smartalarm.ui.activities;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import is.hello.sense.R;
 import is.hello.sense.api.model.Alarm;
+import is.hello.sense.flows.expansions.ui.fragments.ExpansionsAuthFragment;
 import is.hello.sense.ui.activities.ScopedInjectionActivity;
+import is.hello.sense.ui.common.FragmentNavigation;
+import is.hello.sense.ui.common.FragmentNavigationDelegate;
+import is.hello.sense.ui.common.OnBackPressedInterceptor;
 
-public class SmartAlarmDetailActivity extends ScopedInjectionActivity {
-
+public class SmartAlarmDetailActivity extends ScopedInjectionActivity
+        implements FragmentNavigation {
+    //region static fields & functions
     public static final String EXTRA_ALARM = SmartAlarmDetailActivity.class.getName() + ".ARG_ALARM";
     public static final String EXTRA_INDEX = SmartAlarmDetailActivity.class.getName() + ".ARG_INDEX";
 
@@ -23,9 +33,78 @@ public class SmartAlarmDetailActivity extends ScopedInjectionActivity {
         intent.putExtra(EXTRA_INDEX, index);
         context.startActivity(intent);
     }
+    // endregion
+
+    //region fields
+    private FragmentNavigationDelegate navigationDelegate;
+    //endregion
+
+    //region ScopedInjectionActivity
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.navigationDelegate = new FragmentNavigationDelegate(this,
+                                                                 R.id.activity_navigation_container,
+                                                                 stateSafeExecutor);
+        if (savedInstanceState != null) {
+            navigationDelegate.onRestoreInstanceState(savedInstanceState);
+        }
+    }
 
     @Override
     protected List<Object> getModules() {
         return new ArrayList<>();
     }
+
+    @Override
+    public void onBackPressed() {
+        final Fragment fragment = getTopFragment();
+        if (!(fragment instanceof OnBackPressedInterceptor) ||
+                !((OnBackPressedInterceptor) fragment).onInterceptBackPressed(stateSafeExecutor.bind(super::onBackPressed))) {
+            stateSafeExecutor.execute(super::onBackPressed);
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final Fragment fragment = getTopFragment();
+        if (fragment instanceof ExpansionsAuthFragment && fragment.onOptionsItemSelected(item)) {
+            return true;
+        } else if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //endregion
+
+    //region FragmentNavigation
+    @Override
+    public final void pushFragment(@NonNull final Fragment fragment, @Nullable final String title, final boolean wantsBackStackEntry) {
+        navigationDelegate.pushFragment(fragment, title, wantsBackStackEntry);
+    }
+
+    @Override
+    public final void pushFragmentAllowingStateLoss(@NonNull final Fragment fragment, @Nullable final String title, final boolean wantsBackStackEntry) {
+        navigationDelegate.pushFragmentAllowingStateLoss(fragment, title, wantsBackStackEntry);
+    }
+
+    @Override
+    public final void popFragment(@NonNull final Fragment fragment, final boolean immediate) {
+        navigationDelegate.popFragment(fragment, immediate);
+    }
+
+    @Override
+    public final void flowFinished(@NonNull final Fragment fragment, final int responseCode, @Nullable final Intent result) {
+
+    }
+
+    @Nullable
+    @Override
+    public final Fragment getTopFragment() {
+        return navigationDelegate.getTopFragment();
+    }
+    //endregion
 }
