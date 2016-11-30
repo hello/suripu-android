6101db275b9ab14a65b7d50688ac9786ce122aa8
David Zafrani <d.zafrani21@gmail.com>
2016-11-30 12:43:05 -0800

    Using new ViewPagerFragment in NewHomeActivity for Sounds

--- app/src/main/java/is/hello/sense/mvp/view/PresenterView.java
+++ app/src/main/java/is/hello/sense/mvp/view/PresenterView.java
@@ -8,14 +8,14 @@ import android.support.annotation.CallSuper;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.FrameLayout;
-import android.widget.LinearLayout;
 
 import is.hello.sense.R;
+
 import is.hello.sense.ui.recycler.CardItemDecoration;
 import is.hello.sense.ui.recycler.FadingEdgesItemDecoration;
 
@@ -32,6 +32,10 @@ public abstract class PresenterView extends FrameLayout {
             activity.getLayoutInflater().inflate(getLayoutRes(), this);
         }
         this.context = activity;
+    }
+
+    protected boolean useAppCompat() {
+        return false;
     }
 
     /**
