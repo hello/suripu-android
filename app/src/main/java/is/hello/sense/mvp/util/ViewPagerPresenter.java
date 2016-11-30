3cacffc4e9bb3d06b5b4735d2638b1006e6e0a6a
David Zafrani <d.zafrani21@gmail.com>
2016-11-30 11:54:42 -0800

    Added a ViewPagerPresenterDelegate. Used in ViewPagerPresenterFragment

--- app/src/main/java/is/hello/sense/mvp/util/ViewPagerPresenter.java
+++ app/src/main/java/is/hello/sense/mvp/util/ViewPagerPresenter.java
@@ -5,6 +5,16 @@ import android.support.annotation.NonNull;
 import is.hello.sense.ui.adapter.StaticFragmentAdapter;
 
 public interface ViewPagerPresenter {
+    /**
+     * @return List of items to be shown
+     */
     @NonNull
     StaticFragmentAdapter.Item[] getViewPagerItems();
+
+    /**
+     * Initial Item that should be shown. Doesn't crash if out of bounds, instead will default to 0.
+     *
+     * @return index position in {@link #getViewPagerItems()}.
+     */
+    int getStartingItemPosition();
 }
