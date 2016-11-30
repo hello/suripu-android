eac74ee65570cd1a59a0ef3fc627748fc82e2ab6
David Zafrani <d.zafrani21@gmail.com>
2016-11-30 11:54:42 -0800

    Added a ViewPagerPresenterDelegate. Used in ViewPagerPresenterFragment

---
+++ app/src/main/java/is/hello/sense/flows/home/util/SoundsViewPagerPresenterDelegate.java
@@ -0,0 +1,31 @@
+package is.hello.sense.flows.home.util;
+
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+
+import is.hello.sense.R;
+import is.hello.sense.mvp.util.BaseViewPagerPresenterDelegate;
+import is.hello.sense.ui.adapter.StaticFragmentAdapter;
+import is.hello.sense.ui.fragments.sounds.SleepSoundsFragment;
+import is.hello.sense.ui.fragments.sounds.SmartAlarmListFragment;
+import is.hello.sense.util.NotTested;
+
+@NotTested
+public class SoundsViewPagerPresenterDelegate extends BaseViewPagerPresenterDelegate {
+    private final Resources resources;
+
+    public SoundsViewPagerPresenterDelegate(@NonNull final Resources resources) {
+        this.resources = resources;
+    }
+
+    @NonNull
+    @Override
+    public StaticFragmentAdapter.Item[] getViewPagerItems() {
+        return new StaticFragmentAdapter.Item[]{
+                new StaticFragmentAdapter.Item(SmartAlarmListFragment.class,
+                                               resources.getString(R.string.alarm_subnavbar_alarm_list)),
+                new StaticFragmentAdapter.Item(SleepSoundsFragment.class,
+                                               resources.getString(R.string.alarm_subnavbar_sounds_list))
+        };
+    }
+}
