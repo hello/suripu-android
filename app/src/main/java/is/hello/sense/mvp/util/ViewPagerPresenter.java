49a1d3167aae14fffa8fee63346757b5baa039aa
David Zafrani <d.zafrani21@gmail.com>
2016-11-29 16:39:25 -0800

    Using TabLayout successfully

--- app/src/main/java/is/hello/sense/mvp/util/ViewPagerPresenter.java
+++ app/src/main/java/is/hello/sense/mvp/util/ViewPagerPresenter.java
@@ -1,18 +1,10 @@
 package is.hello.sense.mvp.util;
 
-import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 
 import is.hello.sense.ui.adapter.StaticFragmentAdapter;
-import is.hello.sense.ui.widget.SelectorView;
 
 public interface ViewPagerPresenter {
     @NonNull
     StaticFragmentAdapter.Item[] getViewPagerItems();
-
-    @NonNull
-    SelectorView.Option[] getSelectorViewOptions();
-
-    @NonNull
-    Drawable getSelectorViewBackground();
 }
