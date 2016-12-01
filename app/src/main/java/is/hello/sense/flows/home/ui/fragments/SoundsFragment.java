089670fd9181fe104ed0629267ffa85b133fe881
David Zafrani <d.zafrani21@gmail.com>
2016-11-30 16:38:50 -0800

    Renamed xml view for SmartAlarmListView

--- app/src/main/java/is/hello/sense/flows/home/ui/fragments/SoundsFragment.java
+++ app/src/main/java/is/hello/sense/flows/home/ui/fragments/SoundsFragment.java
@@ -15,8 +15,10 @@ import is.hello.sense.ui.widget.SelectorView.OnSelectionChangedListener;
 import rx.Subscription;
 import rx.subscriptions.Subscriptions;
 
-
-public class SoundsFragment extends BacksideTabFragment<SoundsView> implements OnSelectionChangedListener, SwipeRefreshLayout.OnRefreshListener {
+@Deprecated
+public class SoundsFragment extends BacksideTabFragment<SoundsView>
+        implements OnSelectionChangedListener,
+        SwipeRefreshLayout.OnRefreshListener {
 
     @Inject
     PreferencesInteractor preferencesInteractor;
