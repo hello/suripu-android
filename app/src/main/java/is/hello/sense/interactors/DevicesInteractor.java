898b57935bd961f96e02e3f91ec9779472c7100a
Simon Chen <simon.c312@gmail.com>
2017-01-19 12:53:51 -0800

    move logic to retry fetching devices to device interactor method getDevicesWithRetry. Modeled similarly to how ConfigurationInteractor performs retries.

--- app/src/main/java/is/hello/sense/interactors/DevicesInteractor.java
+++ app/src/main/java/is/hello/sense/interactors/DevicesInteractor.java
@@ -1,6 +1,10 @@
 package is.hello.sense.interactors;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import is.hello.sense.api.ApiService;
 import is.hello.sense.api.model.BaseDevice;
@@ -12,6 +16,8 @@ import is.hello.sense.graph.InteractorSubject;
 import rx.Observable;
 
 public class DevicesInteractor extends ValueInteractor<Devices> {
+    private static final long RESUBSCRIBE_DELAY_SECONDS = 2;
+    private static final int RETRY_TIMES = 10;
     private final ApiService apiService;
 
     public final InteractorSubject<Devices> devices = this.subject;
@@ -35,7 +41,7 @@ public class DevicesInteractor extends ValueInteractor<Devices> {
         return apiService.registeredDevices();
     }
 
-    public Observable<VoidResponse> unregisterDevice(@NonNull BaseDevice device) {
+    public Observable<VoidResponse> unregisterDevice(@NonNull final BaseDevice device) {
         if (device instanceof SleepPillDevice) {
             return apiService.unregisterPill(device.deviceId);
         } else if (device instanceof SenseDevice) {
@@ -45,7 +51,43 @@ public class DevicesInteractor extends ValueInteractor<Devices> {
         }
     }
 
-    public Observable<VoidResponse> removeSenseAssociations(@NonNull SenseDevice senseDevice) {
+    public Observable<VoidResponse> removeSenseAssociations(@NonNull final SenseDevice senseDevice) {
         return apiService.removeSenseAssociations(senseDevice.deviceId);
+    }
+
+    /**
+     * @return {@link Observable<Devices>} after retrying if initial returned devices were missing
+     * due to bad sync with server. Update {@link DevicesInteractor#devices} if finished retrying and no other errors were thrown.
+     */
+    public Observable<Devices> getDevicesWithRetry() {
+        final AtomicInteger retryRemaining = new AtomicInteger(RETRY_TIMES);
+        return provideUpdateObservable().flatMap( deviceList -> {
+            if(shouldResubscribe(deviceList, retryRemaining.getAndDecrement())){
+                return Observable.error(new RetryRequiredException());
+            } else {
+                this.devices.onNext(deviceList);
+                return Observable.just(deviceList);
+            }
+        }).retryWhen( errorObservable -> errorObservable.flatMap(this::provideErrorNotificationHandler));
+    }
+
+    private boolean shouldResubscribe(@Nullable final Devices devices,
+                                      final int retryRemaining) {
+        return retryRemaining > 0
+                && (devices == null || devices.getSense() == null);
+    }
+
+    private Observable provideErrorNotificationHandler(final Throwable throwable) {
+        if (throwable instanceof RetryRequiredException) {
+            return Observable.just(null).delay(RESUBSCRIBE_DELAY_SECONDS, TimeUnit.SECONDS);
+        } else {
+            return Observable.error(throwable); //do not resubscribe
+        }
+    }
+
+    private class RetryRequiredException extends Throwable {
+        RetryRequiredException() {
+            super("should only be visible in logs.");
+        }
     }
 }
