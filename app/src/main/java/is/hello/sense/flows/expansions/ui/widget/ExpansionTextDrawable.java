a1b85c5f60388da76b7e0f682719c95603d9a4c1
Simon Chen <simon.c312@gmail.com>
2017-01-24 19:31:02 -0800

    apply a rounded corner picasso transformation with same border width, radius, and color as placeholder drawable.

--- app/src/main/java/is/hello/sense/flows/expansions/ui/widget/ExpansionTextDrawable.java
+++ app/src/main/java/is/hello/sense/flows/expansions/ui/widget/ExpansionTextDrawable.java
@@ -9,6 +9,7 @@ import android.graphics.Path;
 import android.graphics.PixelFormat;
 import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -113,4 +114,16 @@ public class ExpansionTextDrawable extends Drawable {
         this.text = text;
     }
 
+    public int getBorderWidth() {
+        return borderWidth;
+    }
+
+    @ColorInt
+    public int getBorderColor() {
+        return borderPaint.getColor();
+    }
+
+    public int getRadius() {
+        return radius;
+    }
 }
