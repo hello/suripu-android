5c4baca2bf05154f0ca76adda42854f26b852d05
Simon Chen <simon.c312@gmail.com>
2017-01-30 11:58:18 -0800

    return to device settings after upgrade not home activity. Need to make sure views refresh when exiting settings to home activity.

--- app/src/main/java/is/hello/sense/ui/activities/SenseUpgradeActivity.java
+++ app/src/main/java/is/hello/sense/ui/activities/SenseUpgradeActivity.java
@@ -14,7 +14,6 @@ import javax.inject.Inject;
 import is.hello.buruberi.bluetooth.stacks.BluetoothStack;
 import is.hello.sense.R;
 import is.hello.sense.SenseUpgradeModule;
-import is.hello.sense.flows.home.ui.activities.HomeActivity;
 import is.hello.sense.functional.Functions;
 import is.hello.sense.interactors.CurrentSenseInteractor;
 import is.hello.sense.interactors.DevicesInteractor;
@@ -25,9 +24,9 @@ import is.hello.sense.ui.common.FragmentNavigation;
 import is.hello.sense.ui.common.FragmentNavigationDelegate;
 import is.hello.sense.ui.common.OnBackPressedInterceptor;
 import is.hello.sense.ui.fragments.onboarding.BluetoothFragment;
-import is.hello.sense.ui.fragments.onboarding.SetLocationFragment;
 import is.hello.sense.ui.fragments.onboarding.PairSenseFragment;
 import is.hello.sense.ui.fragments.onboarding.SenseVoiceFragment;
+import is.hello.sense.ui.fragments.onboarding.SetLocationFragment;
 import is.hello.sense.ui.fragments.onboarding.VoiceCompleteFragment;
 import is.hello.sense.ui.fragments.onboarding.sense.SenseOTAFragment;
 import is.hello.sense.ui.fragments.onboarding.sense.SenseOTAIntroFragment;
@@ -76,8 +75,9 @@ public class SenseUpgradeActivity extends ScopedInjectionActivity
         if (savedInstanceState != null) {
             navigationDelegate.onRestoreInstanceState(savedInstanceState);
         } else if (navigationDelegate.getTopFragment() == null) {
-            showSenseUpdateIntro();
-            storeCurrentSenseDevice();
+            finishUpgrade();
+            //showSenseUpdateIntro(); todo uncomment
+            //storeCurrentSenseDevice();
         }
     }
 
@@ -106,7 +106,7 @@ public class SenseUpgradeActivity extends ScopedInjectionActivity
 
     @Override
     public void skipToEnd() {
-        showHomeActivity();
+        finishUpgrade();
     }
 
     //endregion
@@ -185,7 +185,7 @@ public class SenseUpgradeActivity extends ScopedInjectionActivity
         } else if (fragment instanceof VoiceCompleteFragment) {
             showResetOriginalSense();
         } else if (fragment instanceof SenseResetOriginalFragment) {
-            showHomeActivity();
+            finishUpgrade();
         }
 
     }
@@ -301,11 +301,11 @@ public class SenseUpgradeActivity extends ScopedInjectionActivity
         pushFragment(new SenseResetOriginalFragment(), null, false);
     }
 
-    public void showHomeActivity() {
+    public void finishUpgrade() {
         setResult(RESULT_OK);
-        final Intent intent = new Intent(this, HomeActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+        //final Intent intent = new Intent(this, AppSettingsActivity.class);
+        //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        //startActivity(intent);
         finish();
     }
 
