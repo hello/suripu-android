29070d69216c0cf415da6e70ee65512267b4fc49
David Zafrani <d.zafrani21@gmail.com>
2017-02-28 16:40:29 -0800

    Added genderOther. Starting with no gender. Continue will act as skip if none selected. Clearing gender on skip. Supporting back and forward actions in onboarding

--- app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingRegisterGenderFragment.java
+++ app/src/main/java/is/hello/sense/ui/fragments/onboarding/OnboardingRegisterGenderFragment.java
@@ -19,6 +19,7 @@ import is.hello.sense.ui.common.AccountEditor;
 import is.hello.sense.ui.common.SenseFragment;
 import is.hello.sense.ui.widget.util.Views;
 import is.hello.sense.util.Analytics;
+import is.hello.sense.util.Constants;
 
 public class OnboardingRegisterGenderFragment extends SenseFragment {
 
@@ -30,7 +31,7 @@ public class OnboardingRegisterGenderFragment extends SenseFragment {
     @DrawableRes
     private static final int OFF_IMAGE_RES = R.drawable.radio_off;
 
-    private Gender currentGender = Gender.MALE;
+    private Gender currentGender = null;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -47,22 +48,8 @@ public class OnboardingRegisterGenderFragment extends SenseFragment {
                              final ViewGroup container,
                              final Bundle savedInstanceState) {
         account = AccountEditor.getContainer(this).getAccount();
-        final View view = inflater.inflate(R.layout.fragment_onboarding_register_gender, container, false);
-        final Button nextButton = (Button) view.findViewById(R.id.fragment_onboarding_next);
-        final Button skipButton = (Button) view.findViewById(R.id.fragment_onboarding_skip);
-        Views.setSafeOnClickListener(nextButton,
-                                     this::onNextClick);
-        Views.setSafeOnClickListener(skipButton,
-                                     this::onSkipClick);
 
-        if (AccountEditor.getWantsSkipButton(this)) {
-            Views.setSafeOnClickListener(skipButton,
-                                         this::onSkipClick);
-        } else {
-            skipButton.setVisibility(View.GONE);
-            nextButton.setText(R.string.action_done);
-        }
-        return view;
+        return inflater.inflate(R.layout.fragment_onboarding_register_gender, container, false);
 
     }
 
@@ -70,6 +57,12 @@ public class OnboardingRegisterGenderFragment extends SenseFragment {
     public void onViewCreated(final View view,
                               final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        final Button nextButton = (Button) view.findViewById(R.id.fragment_onboarding_next);
+        final Button skipButton = (Button) view.findViewById(R.id.fragment_onboarding_skip);
+        Views.setSafeOnClickListener(nextButton,
+                                     this::onNextClick);
+        Views.setSafeOnClickListener(skipButton,
+                                     this::onSkipClick);
         this.binding = DataBindingUtil.bind(view);
         Views.setSafeOnClickListener(this.binding.fragmentOnboardingGenderMaleImagetextview,
                                      this::onMaleClick);
@@ -79,29 +72,47 @@ public class OnboardingRegisterGenderFragment extends SenseFragment {
                                      this::onOtherClick);
 
         final Account account = AccountEditor.getContainer(this).getAccount();
+
+        if (AccountEditor.getWantsSkipButton(this)) {
+            Views.setSafeOnClickListener(skipButton,
+                                         this::onSkipClick);
+        } else {
+            skipButton.setVisibility(View.GONE);
+            nextButton.setText(R.string.action_done);
+
+        }
         if (account.getGender() != null) {
             this.currentGender = account.getGender();
             if (account.getGender() == Gender.MALE) {
                 setImages(ON_IMAGE_RES, OFF_IMAGE_RES, OFF_IMAGE_RES);
             } else if (account.getGender() == Gender.FEMALE) {
                 setImages(OFF_IMAGE_RES, ON_IMAGE_RES, OFF_IMAGE_RES);
-            } else {
+            } else if (account.getGenderOther() != null && !account.getGenderOther().isEmpty()) {
                 setImages(OFF_IMAGE_RES, OFF_IMAGE_RES, ON_IMAGE_RES);
-
+            } else {
+                setImages(OFF_IMAGE_RES, OFF_IMAGE_RES, OFF_IMAGE_RES);
             }
         }
 
     }
 
     private void onNextClick(final View ignored) {
+        if (currentGender == null) {
+            onSkipClick(ignored);
+            return;
+        }
         final AccountEditor.Container container = AccountEditor.getContainer(this);
         container.getAccount().setGender(currentGender);
+        if (currentGender == Gender.OTHER){
+            container.getAccount().setGenderOther("Test");
+        }
         AccountEditor.getContainer(this).onAccountUpdated(this);
     }
 
     private void onSkipClick(final View ignored) {
         Analytics.trackEvent(Analytics.Onboarding.EVENT_SKIP, Analytics.createProperties(Analytics.Onboarding.PROP_SKIP_SCREEN, "gender"));
         account.setGender(Gender.OTHER);
+        account.setGenderOther(Constants.EMPTY_STRING);
         AccountEditor.getContainer(this).onAccountUpdated(this);
     }
 
