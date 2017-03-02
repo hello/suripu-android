c105008fde513e557d858aae9c86b20fda05145d
David Zafrani <d.zafrani21@gmail.com>
2017-03-02 15:51:06 -0800

    Removed state from ControllerPresenterFragment

--- app/src/main/java/is/hello/sense/mvp/presenters/ControllerPresenterFragment.java
+++ app/src/main/java/is/hello/sense/mvp/presenters/ControllerPresenterFragment.java
@@ -8,15 +8,13 @@ import is.hello.sense.flows.home.ui.adapters.StaticFragmentAdapter;
 
 public abstract class ControllerPresenterFragment<T extends PresenterView> extends PresenterFragment<T>
         implements StaticFragmentAdapter.Controller {
-    private static final String KEY_IS_VISIBLE = ControllerPresenterFragment.class.getSimpleName() + ".KEY_IS_VISIBLE";
     private boolean isVisible = false;
-    private boolean restoreStateOnStart = false;
 
 
     @CallSuper
     @Override
     public void setVisibleToUser(final boolean isVisible) {
-        //debugLog("setVisibleToUser: [ " + isVisible + " ]"); //useful for debugging
+        debugLog("setVisibleToUser: [ " + isVisible + " ]"); //useful for debugging
         this.isVisible = isVisible;
     }
 
@@ -26,30 +24,5 @@ public abstract class ControllerPresenterFragment<T extends PresenterView> exten
         return isVisible;
     }
 
-    @Override
-    public void onCreate(final Bundle savedInstanceState) {
-        // SetUserVisible should only be called when the presenterview is initialized.
-        super.onCreate(savedInstanceState);
-        if (savedInstanceState != null) {
-            this.restoreStateOnStart = savedInstanceState.getBoolean(KEY_IS_VISIBLE, false);
-        }
-    }
-
-    @CallSuper
-    @Override
-    public void onSaveInstanceState(final Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(KEY_IS_VISIBLE, isVisible);
-    }
-
-    @CallSuper
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (this.restoreStateOnStart) {
-            this.restoreStateOnStart = false;
-            setVisibleToUser(true);
-        }
-    }
 
 }
