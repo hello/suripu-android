c249488271d2e4173b826b4a75531fd324630f67
Simon Chen <simon.c312@gmail.com>
2017-03-17 13:11:53 -0700

    Allow not showing negative permission more info button by passing Constants.NONE in constructor
    Update night mode explanation dialog message
    
    remove enter exit fade animation from radio selectors. Interferes with rendering in adapters

--- app/src/main/java/is/hello/sense/flows/nightmode/ui/views/NightModeLocationPermission.java
+++ app/src/main/java/is/hello/sense/flows/nightmode/ui/views/NightModeLocationPermission.java
@@ -5,27 +5,27 @@ import android.support.annotation.NonNull;
 
 import is.hello.sense.R;
 import is.hello.sense.permissions.LocationPermission;
-import is.hello.sense.ui.common.UserSupport;
+import is.hello.sense.util.Constants;
 
 public class NightModeLocationPermission extends LocationPermission {
 
     public NightModeLocationPermission(@NonNull final Fragment fragment) {
-        super(fragment);
+        super(fragment,
+              Constants.NONE,
+              R.string.action_continue);
     }
 
     @Override
     public void requestPermissionWithDialog() {
         requestPermissionWithDialog(R.string.request_permission_location_title,
                                     R.string.night_mode_location_permission_message,
-                                    //todo redirect to night mode support page or hide more info
-                                    (dialog, which) -> UserSupport.showLocationPermissionMoreInfoPage(fragment.getActivity()));
+                                    null);
     }
 
     @Override
     public void showEnableInstructionsDialog() {
         showEnableInstructionsDialog(R.string.request_permission_location_required_title,
                                      R.string.night_mode_location_permission_required_message,
-                                     //todo redirect to night mode support page or hide more info
-                                     (dialog, which) -> UserSupport.showLocationPermissionMoreInfoPage(fragment.getActivity()));
+                                     null);
     }
 }
