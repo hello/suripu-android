a6f74e7a75fab70e8b15a4309dd02a4ea5f311ff
Simon Chen <simon.c312@gmail.com>
2017-03-28 16:19:24 -0700

    fix SenseAlertDialog appearance in lollipop
    - removed setting windowBackground to background_card fixed it
    
    - use Body1_Blue as focused text style for rotary picker ':' colon
    
    - removed Platform.Dialog.MinWidth

--- app/src/main/java/is/hello/sense/ui/widget/RotaryPickerView.java
+++ app/src/main/java/is/hello/sense/ui/widget/RotaryPickerView.java
@@ -12,7 +12,6 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -31,7 +30,7 @@ import is.hello.sense.ui.widget.util.Views;
 public class RotaryPickerView extends RecyclerView implements View.OnClickListener {
     public static final int DEFAULT_UNFOCUSED_ITEM_COUNT = 2;
     public static final @StyleRes int ITEM_TEXT_APPEARANCE = R.style.Display1;
-    public static final @StyleRes int ITEM_TEXT_APPEARANCE_FOCUSED = R.style.Body1;
+    public static final @StyleRes int ITEM_TEXT_APPEARANCE_FOCUSED = R.style.Body1_Blue;
 
     //region Internal
 
