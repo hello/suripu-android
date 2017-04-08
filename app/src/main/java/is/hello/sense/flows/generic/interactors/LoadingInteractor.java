d9891093539a2ad1219d0d335a01dbe5716651a8
David Zafrani <d.zafrani21@gmail.com>
2017-04-08 12:12:29 -0700

    Showing a cleaner transition to nightmode

---
+++ app/src/main/java/is/hello/sense/flows/generic/interactors/LoadingInteractor.java
@@ -0,0 +1,25 @@
+package is.hello.sense.flows.generic.interactors;
+
+import is.hello.sense.api.model.VoidResponse;
+import is.hello.sense.graph.InteractorSubject;
+import is.hello.sense.interactors.ValueInteractor;
+import rx.Observable;
+
+public class LoadingInteractor extends ValueInteractor<VoidResponse> {
+    public final InteractorSubject<VoidResponse> sub = subject;
+
+    @Override
+    protected boolean isDataDisposable() {
+        return true;
+    }
+
+    @Override
+    protected boolean canUpdate() {
+        return true;
+    }
+
+    @Override
+    protected Observable<VoidResponse> provideUpdateObservable() {
+        return Observable.create(subscriber -> subscriber.onNext(new VoidResponse()));
+    }
+}
