8b64dd03d753f470279b88669239e59d06b05748
Simon Chen <simon.c312@gmail.com>
2017-04-10 20:30:21 -0700

    only use fade in and out window animations when recreating night mode activity.

--- app/src/main/java/is/hello/sense/flows/nightmode/ui/activities/NightModeActivity.java
+++ app/src/main/java/is/hello/sense/flows/nightmode/ui/activities/NightModeActivity.java
@@ -1,5 +1,6 @@
 package is.hello.sense.flows.nightmode.ui.activities;
 
+import is.hello.sense.R;
 import is.hello.sense.flows.nightmode.ui.fragments.NightModeFragment;
 import is.hello.sense.ui.activities.appcompat.FragmentNavigationActivity;
 
@@ -10,5 +11,12 @@ public class NightModeActivity extends FragmentNavigationActivity {
         navigationDelegate.pushFragment(new NightModeFragment(),
                                         null,
                                         false);
+    }
+
+    @Override
+    public void recreate() {
+        getWindow().setWindowAnimations(R.style.WindowAnimations_Fade);
+        super.recreate();
+
     }
 }
